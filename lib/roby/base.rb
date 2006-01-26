1d8646e16c916a5290ab5c42c1a29ea5db7ee281
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2006-01-26 14:45:09 +0000

    Add a logger in the Roby namespace

---
+++ lib/roby/base.rb
@@ -0,0 +1,21 @@
+require 'logger'
+
+module Roby
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::DEBUG
+    @logger.progname = "Roby"
+    @logger.formatter = lambda { |severity, time, progname, msg| "#{progname}: #{msg}\n" }
+
+    class << self
+        # The logger used by Genom.rb 
+        # The default logger severity is set to WARN
+        attr_accessor :logger
+
+        [ :debug, :info, :warn, :error, :fatal, :unknown ].each do |level|
+            class_eval <<-EOF
+            def #{level}(*args, &proc); @logger.#{level}(*args, &proc) end
+            EOF
+        end
+    end
+end
+
