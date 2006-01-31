eef71dac94ac7146669ec79b8e5d1d5841636116
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2006-01-31 16:04:09 +0000

    Split the event loop between process_event and run so that one can put together their own event loop

--- lib/roby/event_loop.rb
+++ lib/roby/event_loop.rb
@@ -6,53 +6,57 @@ module Roby
         attr_reader :event_processing
     end
 
+    def self.drb(drb_uri = nil)
+        require 'roby/drb'
+        DRb.start_service(drb_uri, Server.new(Thread.current))
+        Roby.info "Started DRb server on #{drb_uri}"
+    end
+
+    def self.process_events
+        # Current time
+        cycle_start = Time.now
+
+        # Get the events received by the server and process them
+        cycle_server = Time.now
+        Thread.current.process_events
+        
+        # Call event processing registered by other modules
+        cycle_handlers = Time.now
+        event_processing.each { |prc| prc.call }
+        
+        cycle_end = Time.now
+        cycle_duration = cycle_end - cycle_start
+
+        Roby.debug { 
+            "Object allocation profile:" <<
+            ObjectStats.profile { GC.start }.collect do |klass, count|
+                "  #{klass}: #{count}"
+            end.join("\n")
+        }
+
+        Roby.debug { 
+            [
+                "Started cycle at #{cycle_start}",
+                cycle_server ? "  server events processing took #{cycle_handlers - cycle_server}" : nil,
+                "  event handlers took #{cycle_end - cycle_handlers}",
+                "end of cycle at #{cycle_end}. Event processing took #{cycle_duration}s" 
+            ].compact.join("\n")
+        }
+
+        cycle_duration
+    end
+
     # The main event loop
     def self.run(drb_uri = nil, cycle = 0.1)
-        # Start the DRb server if needed
-        if drb_uri
-            require 'roby/drb'
-            DRb.start_service(drb_uri, Server.new(Thread.current))
-            Roby.info "Started DRb server on #{drb_uri}"
-        end
-
+        drb(drb_uri) if drb_uri
         cycle_start, cycle_server, cycle_handlers = nil
         GC.disable
         GC.start
-        
+
         yield if block_given?
         loop do
-            # Current time
             cycle_start = Time.now
-
-            # Get the events received by the server and process them
-            cycle_server = nil
-            if drb_uri
-                cycle_server = Time.now
-                Thread.current.process_events
-            end
-            
-            # Call event processing registered by other modules
-            cycle_handlers = Time.now
-            event_processing.each { |prc| prc.call }
-            
-            cycle_end = Time.now
-            cycle_duration = cycle_end - cycle_start
-
-            Roby.debug { 
-                "Object allocation profile:" <<
-                ObjectStats.profile { GC.start }.collect do |klass, count|
-                    "  #{klass}: #{count}"
-                end.join("\n")
-            }
-
-            Roby.debug { 
-                [
-                    "Started cycle at #{cycle_start}",
-                    cycle_server ? "  server events processing took #{cycle_handlers - cycle_server}" : nil,
-                    "  event handlers took #{cycle_end - cycle_handlers}",
-                    "end of cycle at #{cycle_end}. Event processing took #{cycle_duration}s" 
-                ].compact.join("\n")
-            }
+            process_events
 
             GC.start
             cycle_duration = Time.now - cycle_start
