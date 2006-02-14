cd36da505a76efcc5a7e8e3361c1b5d91ff7cfd5
tailor <sylvain.joyeux@laas.fr>
2006-02-14 17:26:56 +0000

    test for the Runner task

--- test_suite/test_genom.rb
+++ test_suite/test_genom.rb
@@ -20,36 +20,50 @@ class TC_Genom < Test::Unit::TestCase
         ::Genom.connect do
             @env.stop_modules('mockup')
         end
+        sleep(1)
     end
+
+    def test_module_task
+        ::Genom.connect do
+            Genom::GenomModule('mockup')
+            start_task  = Genom::Mockup.start
+
+            Genom::Mockup::Runner.new.start!
+            start_activity
+        end
+    end
+            
 
     def test_event_handling
         ::Genom.connect do
             mod = Genom::GenomModule('mockup')
             @env.start_modules('mockup')
-
-            task = mod.start
-            task.start!
-
-            activity = task.activity
-            
-            assert_doesnt_timeout(2) { 
-                while !task.running?
-                    $stderr.puts "waiting for task to start"
-                    Roby.process_events
-                end
-            }
-            
-            task.activity.abort.wait
-            assert(!task.finished?)
-            assert_doesnt_timeout(2) {
-                while !task.finished?
-                    $stderr.puts "waiting for task to end"
-                    Roby.process_events
-                end
-            }
-
-            assert(task.finished?)
+            start_activity
         end
+    end
+
+    def start_activity
+        task = Genom::Mockup.start
+        task.start!
+        activity = task.activity
+        
+        assert_doesnt_timeout(2) { 
+            while !task.running?
+                $stderr.puts "waiting for task to start"
+                Roby.process_events
+            end
+        }
+        
+        activity.abort.wait
+        assert(!task.finished?)
+        assert_doesnt_timeout(2) {
+            while !task.finished?
+                $stderr.puts "waiting for task to end"
+                Roby.process_events
+            end
+        }
+
+        assert(task.finished?)
     end
 end
 
