7ca09d30d78b1e76d6d01b600efc752c81d9b7de
tailor <sylvain.joyeux@laas.fr>
2006-02-14 17:25:03 +0000

    Define the executed_by relation

---
+++ lib/roby/relations/executed_by.rb
@@ -0,0 +1,34 @@
+require 'roby/task'
+
+module Roby::TaskRelations
+    module ExecutedBy
+        def initialize
+            if self.class.execution_agent
+                executed_by self.class.execution_agent.new_task(self)
+            end
+            super
+        end
+
+        def self.included(klass)
+            class << klass
+                attr_reader :execution_agent
+                # Defines a model of execution agent
+                # model.new_task(task) shall return the task instance which
+                # will execute this task 
+                def executed_by(model)
+                    @execution_agent = agent
+                end
+            end
+            super
+        end
+        
+        attr_reader :execution_agent
+        def executed_by(agent)
+            raise "an agent is already defined for this task" if @execution_agent
+            @execution_agent = agent
+            realized_by agent, :fails_on => :stop
+        end
+    end
+end
+
+
