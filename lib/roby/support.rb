e56d2e931be31e9d0ede8369c69fee73469e7d0c
tailor <sylvain.joyeux@laas.fr>
2006-02-20 17:33:12 +0000

    Fix the exception message in check_arity

--- lib/roby/support.rb
+++ lib/roby/support.rb
@@ -220,7 +220,7 @@ module Kernel
 
     def check_arity(object, arity)
         unless object.arity == arity || (object.arity < 0 && object.arity > - arity - 2)
-            raise ArgumentError, "#{object} cannot take #{arity} argument(s)"
+            raise ArgumentError, "#{object} does not accept to be called with #{arity} argument(s)"
         end
     end
 end
