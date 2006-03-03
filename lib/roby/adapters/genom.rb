85676ec525948969fe9356f511b9f2a4354fd474
tailor <sylvain.joyeux@laas.fr>
2006-03-03 16:19:27 +0000

    there is only one init request, update the code to reflect that

--- lib/roby/adapters/genom.rb
+++ lib/roby/adapters/genom.rb
@@ -195,12 +195,9 @@ module ::Roby
 			# Make sure there is a init() method defined in the Roby module if there is one in the
 			# Genom module
 			if !roby_module.respond_to?(:init) && genom_module.respond_to?(:init)
-			    init_requests = genom_module.request_info.
-				find_all { |rq| rq.init? }.
-				map { |rq| rq.name }.
-				join(", ")
+			    init_request = genom_module.request_info.find { |rq| rq.init? }.name
 			    
-			    raise ArgumentError, "The Genom module '#{genom_module.name}' defines the following init requests: #{init_requests}. You must define an init method in '#{roby_module.name}' which calls one of these."
+			    raise ArgumentError, "The Genom module '#{genom_module.name}' defines the init request #{init_request}. You must define a singleton 'init' method in '#{roby_module.name}' which initializes the module"
 			end
 			super
 		    end
