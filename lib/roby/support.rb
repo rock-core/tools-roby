ac753694e32b981895f239e654f44d84c281cee6
tailor <sylvain.joyeux@laas.fr>
2006-03-06 13:40:02 +0000

    use the support.rb file from Genom.rb moved some of the functionalities of roby/support into genom/support:     string inflections, option validation

--- lib/roby/support.rb
+++ lib/roby/support.rb
@@ -1,11 +1,8 @@
-require 'active_support/core_ext/string/inflections'
 require 'enumerator'
 require 'thread'
 require 'set'
 
-class String
-    include ActiveSupport::CoreExtensions::String::Inflections
-end
+require 'genom/support'
 
 class BFSEnumerator
     def initialize(root, enum_with, args)
@@ -108,11 +105,6 @@ class UniqEnumerator
 end
 
 class Object
-    # Get the singleton class for this object
-    def singleton_class
-        class << self; self; end
-    end
-
     # Enumerates an iterator-based graph depth-first
     def enum_dfs(enum_with = :each, *args, &iterator) 
         enumerator = DFSEnumerator.new(self, enum_with, args) 
@@ -287,66 +279,6 @@ class Class
     end
 end
 
-
-module Kernel
-    # Validates an option hash, with default value support
-    # 
-    # :call-seq:
-    #   validate_options(option, hash)       -> options
-    #   validate_options(option, array)
-    #   validate_options(nil, known_options)
-    #
-    # In the first form, +option_hash+ should contain keys which are also 
-    # in known_hash. The non-nil values of +known_hash+ are used as default
-    # values
-    #
-    # In the second form, +known_array+ is an array of option
-    # keys. +option_hash+ keys shall be in +known_array+
-    #
-    # +nil+ is treated as an empty option hash
-    #
-    # All option keys are converted into symbols
-    #
-    def validate_options(options, known_options)
-        options = Hash.new unless options
-       
-        if Array === known_options
-            # Build a hash with all values to nil
-            known_options = known_options.inject({}) { |h, k| h[k.to_sym] = nil; h }
-        end
-
-        options        = options.inject({}) { |h,v| h[v[0].to_sym] = v[1]; h }
-        known_options  = known_options.inject({}) { |h,v| h[v[0].to_sym] = v[1]; h }
-
-        not_valid = options.keys - known_options.keys
-        not_valid = not_valid.map { |m| "'#{m}'" }.join(" ")
-        raise ArgumentError, "unknown options #{not_valid}" if !not_valid.empty?
-
-        # Set default values defined in 'known_options'
-        known_options.each_key do |k| 
-            value = known_options[k]
-            options[k] ||= value unless value.nil?
-        end
-
-        options
-    end
-
-    def validate_option(options, name, required, message = nil)
-        if required && !options.has_key?(name)
-            raise ArgumentError, "missing required option #{name}"
-        elsif options.has_key?(name) && block_given?
-            if !yield(options[name])
-                raise ArgumentError, (message || "invalid option value #{options[name]} for #{name}")
-            end
-        end
-    end
-
-    def check_arity(object, arity)
-        unless object.arity == arity || (object.arity < 0 && object.arity > - arity - 2)
-            raise ArgumentError, "#{object} does not accept to be called with #{arity} argument(s)"
-        end
-    end
-end
 
 class Thread
     def send_to(object, name, *args, &prc)
