0aebbaec8bb62495bf94ad517ccff53b2eff76e7
tailor <sylvain.joyeux@laas.fr>
2006-03-09 12:32:24 +0000

    update planning test suite

--- test_suite/test_planning.rb
+++ test_suite/test_planning.rb
@@ -52,40 +52,6 @@ class TC_Planner < Test::Unit::TestCase
 	assert_raises(Planning::NotFound) { planner.recursive(:recursive => false) }
     end
 
-    def test_method_model
-	# Some task models
-        base_task_model		= Class.new(Roby::Task) do
-            event :start
-            event :stop
-        end
-        derived_task_model	= Class.new(base_task_model)
-        not_a_task		= Class.new
-        another_kind_of_task	= Class.new(Roby::Task)
-
-	# The planning model
-        model = Class.new(Planner) do
-            method(:root, :returns => base_task_model)
-            method(:root, :id => 1) {}
-	    method(:root, :id => 10) {}
-	    method(:root) {}
-	end
-
-	assert( model.method_model(:root) )
-	assert( model.method_model(:root).returns == base_task_model, model.method_model(:root).inspect )
-
-	# Check that :returns is correctly validated
-	assert_raises(ArgumentError) { model.method(:root, :returns => not_a_task) }
-	assert_raises(ArgumentError) { model.method(:root, :returns => another_kind_of_task) }
-
-	# We cannot change the model since a method has been defined
-	assert_raises(ArgumentError) { model.method(:root, :returns => derived_task_model) }
-	assert_nothing_raised { model.method(:root, :returns => base_task_model) {} }
-	assert_nothing_raised { model.method(:root, :returns => derived_task_model) {} }
-
-	# Check that we can't override an already-defined method
-	assert_raises(ArgumentError) { model.method(:root, :id => 1) {} }
-    end
-
     def test_recursive
         model = Class.new(Planner) do
             method(:not_recursive) do
@@ -121,32 +87,69 @@ class TC_Planner < Test::Unit::TestCase
         assert_equal(['recursive', 'root'].to_set, methods.to_set)
     end
 
+    def test_method_model
+	# Some task models
+        base_task_model		= Class.new(Roby::Task) do
+            event :start
+            event :stop
+        end
+        derived_task_model	= Class.new(base_task_model)
+        not_a_task		= Class.new
+        another_kind_of_task	= Class.new(Roby::Task)
+
+	# The planning model
+        model = Class.new(Planner)
+	
+	# Check method model validation
+	assert_raises(ArgumentError) { model.method(:root, :returns => not_a_task) }
+	model.method(:root, :returns => base_task_model)
+	assert_equal( model.root_model, model.method_model(:root) )
+	assert( model.method_model(:root).returns == base_task_model, model.method_model(:root).inspect )
+	assert_raises(ArgumentError) { model.method(:root, :returns => another_kind_of_task) }
+	assert_nothing_raised { model.method(:root, :returns => base_task_model) }
+
+	# Define a method based on the model
+	model.method(:root, :id => 1) {}
+	assert_raises(ArgumentError) { model.method(:root, :returns => not_a_task) {} }
+	assert_nothing_raised { model.method(:root, :returns => base_task_model) {} }
+	assert_nothing_raised { model.method(:root, :returns => derived_task_model) {} }
+
+	# Check that we can't override an already-defined method
+	assert_raises(ArgumentError) { model.method(:root, :id => 1) {} }
+    end
+
     def test_model_inheritance
-        base_task_model			= Class.new(Roby::Task)
-        derived_task_model		= Class.new(base_task_model)
-        another_derived_task_model	= Class.new(base_task_model)
-        not_a_task			= Class.new
-        another_kind_of_task		= Class.new(Roby::Task)
+        tm_a	    = Class.new(Roby::Task)
+        tm_b	    = Class.new(Roby::Task)
+        tm_a_a	    = Class.new(tm_a)
+        tm_a_a_a    = Class.new(tm_a_a)
+        tm_b_a	    = Class.new(tm_a)
 
         base = Class.new(Planner) do
-            method(:root, :returns => base_task_model)
-            method(:root, :id => 1, :returns => derived_task_model) {}
+            method(:root, :returns => tm_a)
+            method(:root, :id => 1, :returns => tm_a_a) {}
 	end
+	base_root = base.enum_for(:each_root_method).to_a
 
 	# Test inheritance rules
-        derived = Class.new(base)
-	assert_raises(ArgumentError)	{ derived.method(:root, :returns => another_kind_of_task) }
-	assert_raises(ArgumentError)	{ derived.method(:root, :returns => another_kind_of_task) {} }
-	assert_nothing_raised		{ derived.method(:root, :returns => another_derived_task_model) {} }
-	assert_nothing_raised		{ derived.method(:root, :returns => derived_task_model) {} }
-	assert_nothing_raised		{ derived.method(:root, :returns => base_task_model) {} }
-	assert_raises(ArgumentError)    { derived.method(:root, :id => 1, :returns => another_derived_task_model) {} }
+        d1 = Class.new(base)
+	assert_raises(ArgumentError)	{ d1.method(:root, :returns => tm_a_a) }
+	assert_raises(ArgumentError)	{ d1.method(:root, :returns => tm_b) }
+	assert_raises(ArgumentError)	{ d1.method(:root, :returns => tm_b) {} }
 
-	derived = Class.new(base)
-	assert_raises(ArgumentError)	{ derived.method(:root, :returns => derived_task_model) }
-	assert_nothing_raised		{ derived.method(:root, :returns => derived_task_model) {} }
+	d1_root = []
+	assert_nothing_raised { d1_root << d1.method(:root, :returns => tm_a) {} }
+	assert_nothing_raised { d1_root << d1.method(:root, :returns => tm_a_a) {} }
+	assert_nothing_raised { d1_root << d1.method(:root, :returns => tm_b_a) {} }
+	d2_root = d1_root.dup
+	assert_nothing_raised { d1_root << d1.method(:root, :id => 1, :returns => tm_a_a) {} }
 
-	derived = Class.new(base)
+	d2 = Class.new(d1)
+	assert_nothing_raised { d2_root << d2.method(:root, :id => 1, :returns => tm_a_a_a) {} }
+
+	assert_equal(base_root.to_set, base.enum_for(:each_root_method).to_set)
+	assert_equal(d1_root.to_set, d1.enum_for(:each_root_method).map { |_, x| x }.to_set)
+	assert_equal(d2_root.to_set, d2.enum_for(:each_root_method).map { |_, x| x }.to_set)
     end
 
     def test_library
