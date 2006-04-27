3b80edcdde41469d551b669ed5d17b886b5944ba
tailor <sylvain.joyeux@laas.fr>
2006-04-27 14:22:44 +0000

    add a test about the 'super if defined? super' idiom

--- test_suite/test_support.rb
+++ test_suite/test_support.rb
@@ -28,6 +28,43 @@ module BaseClassExtension
 end
 
 class TC_Utils < Test::Unit::TestCase
+    def test_super_idiom
+	base = Class.new do
+	    attr_reader :base
+	    def initialize
+		super if defined? super
+		@base = true
+	    end
+	end
+	assert_nothing_raised { base.new }
+
+	derived = Class.new(base) do
+	    attr_reader :derived
+	    def initialize
+		super if defined? super
+		@derived = true
+	    end
+	end
+	obj = nil
+	assert_nothing_raised { obj = derived.new }
+	assert( obj.base )
+	assert( obj.derived )
+
+	mod = Module.new do
+	    attr_reader :module
+	    def initialize
+		super if defined? super
+		@module = true
+	    end
+	end
+	obj = nil
+	base.include mod
+
+	assert_nothing_raised { obj = derived.new }
+	assert( obj.base )
+	assert( obj.module )
+	assert( obj.derived )
+    end
     def test_attribute
 	a = Class.new do
 	    attribute(:array_attribute) { Array.new }
