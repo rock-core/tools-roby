abfe6e4d1017d24db92eb49ad01499f08185ed27
tailor <sylvain.joyeux@laas.fr>
2006-05-23 12:20:13 +0000

    [doc] document Object#address and Object::addres_from_id

--- lib/roby/support.rb
+++ lib/roby/support.rb
@@ -105,8 +105,13 @@ class Object
 	end
     end
 
-    def address
-	id = object_id
+    # Return the object address (for non immediate
+    # objects). 
+    def address; Object.address_from_id(object_id) end
+
+    # Converts the object_id of a non-immediate object
+    # to its 32 bits address
+    def self.address_from_id(id)
 	id = 0xFFFFFFFF - ~id if id < 0
 	(id * 2) & 0xFFFFFFFF
     end
