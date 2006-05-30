caa1aff94fd2e882710d3e42396079e5c3d3d6b8
tailor <sylvain.joyeux@laas.fr>
2006-05-30 16:22:24 +0000

    [test] move test_until back into test_event

--- test_suite/test_event.rb
+++ test_suite/test_event.rb
@@ -335,5 +335,16 @@ class TC_Event < Test::Unit::TestCase
 	    e1.call(nil)
 	end
     end
+
+    def test_until
+	e1, e2, e3, e4 = 4.enum_for(:times).map { Roby::EventGenerator.new(true) }
+	e1.on(e2)
+	e2.on(e3)
+	e3.until(e2).on(e4)
+
+	e1.call(nil)
+	assert( e3.happened? )
+	assert( !e4.happened? )
+    end
 end
 
