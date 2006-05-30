abd78d2aed20bc5f7d4e34db18b2345d3d6239e8
tailor <sylvain.joyeux@laas.fr>
2006-05-30 14:18:35 +0000

    [test] test for eql? and hash behavior

--- test_suite/test_drb.rb
+++ test_suite/test_drb.rb
@@ -24,5 +24,17 @@ class TC_Display < Test::Unit::TestCase
 	assert_nothing_raised { Marshal.dump(displayable) }
 	assert_nothing_raised { YAML.dump(displayable) }
     end
+
+    def test_hash_values
+	klass = Class.new(Task) do 
+	    event :start
+	    event :stop
+	end
+	t1, t2 = klass.new, klass.new
+	assert_not_equal(Display::Task[t1].hash, Display::Task[t2].hash)
+	assert(!Display::Task[t1].eql?(Display::Task[t2]))
+	assert_not_equal(Display::Event[t1.event(:start)].hash, Display::Event[t1.event(:stop)].hash)
+	assert(!Display::Event[t1.event(:start)].eql?(Display::Event[t1.event(:stop)]))
+    end
 end
 
