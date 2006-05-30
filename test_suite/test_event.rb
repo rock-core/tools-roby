1652bfb732dd73d3921bba1ee32a24f278072e15
tailor <sylvain.joyeux@laas.fr>
2006-05-30 16:13:28 +0000

    [test] refactor test_task_meta and test_event_propagation into test_task and test_event

---
+++ test_suite/test_event.rb
@@ -0,0 +1,339 @@
+require 'flexmock'
+require 'test_config'
+require 'roby/event_loop'
+require 'mockups/tasks'
+
+class TC_Event < Test::Unit::TestCase
+    def test_properties
+	event = Roby::EventGenerator.new
+	assert(! event.respond_to?(:call))
+	assert(! event.controlable?)
+
+	event = Roby::EventGenerator.new(true)
+	assert(event.respond_to?(:call))
+	assert(event.controlable?)
+
+	# Check command & emission behavior for controlable events
+	FlexMock.use do |mock|
+	    event = Roby::EventGenerator.new { |context| mock.call_handler(context); event.emit(context) }
+	    event.on { |event| mock.event_handler(event.context) }
+
+	    assert(event.respond_to?(:call))
+	    assert(event.controlable?)
+	    mock.should_receive(:call_handler).once.with(42)
+	    mock.should_receive(:event_handler).once.with(42)
+	    event.call(42)
+	end
+
+	# Check emission behavior for non-controlable events
+	FlexMock.use do |mock|
+	    event = Roby::EventGenerator.new
+	    event.on { |event| mock.event(event.context) }
+	    mock.should_receive(:event).once.with(42)
+	    event.emit(42)
+	end
+    end
+    def test_emit_failed
+	event = Roby::EventGenerator.new
+	assert_raises(Roby::EventModelViolation) { event.emit_failed }
+	assert_raises(Roby::EventModelViolation) { event.emit_failed("test") }
+
+	klass = Class.new(Roby::EventModelViolation)
+	assert_raises(klass) { event.emit_failed(klass) }
+	assert_raises(klass) { event.emit_failed(klass, "test") }
+	begin; event.emit_failed(klass, "test")
+	rescue klass => e
+	    assert( e.message =~ /: test$/ )
+	end
+
+	exception = klass.new(event)
+	assert_raises(klass) { event.emit_failed(exception, "test") }
+	begin; event.emit_failed(exception, "test")
+	rescue klass => e
+	    assert_equal(event, e.generator)
+	    assert( e.message =~ /: test$/ )
+	end
+    end
+
+    def test_signal_relation
+	e1, e2 = Roby::EventGenerator.new(true), Roby::EventGenerator.new(true)
+
+	e1.on e2
+	assert( e1.child_object?( e2, Roby::EventStructure::Signals ))
+	assert( e2.parent_object?( e1, Roby::EventStructure::Signals ))
+
+	e1.call(nil)
+	assert(e2.happened?)
+    end
+ 
+    def test_handlers
+	e1, e2 = Roby::EventGenerator.new(true), Roby::EventGenerator.new(true)
+	e1.on { e2.call(nil) }
+
+	FlexMock.use do |mock|
+	    e1.on { mock.e1 }
+	    e2.on { mock.e2 }
+	    mock.should_receive(:e1).once.ordered
+	    mock.should_receive(:e2).once.ordered
+	    e1.call(nil)
+	end
+    end
+
+    def test_simple_signal_handler_ordering
+	e1, e2, e3 = 4.enum_for(:times).map { Roby::EventGenerator.new(true) }
+	e1.on(e2)
+	e1.on { e2.remove_signal(e3) }
+	e2.on(e3)
+
+	e1.call(nil)
+	assert( e2.happened? )
+	assert( !e3.happened? )
+    end
+
+    def test_event_hooks
+        FlexMock.use do |mock|
+	    hooks = [:calling, :fired, :called]
+            mod = Module.new do
+		hooks.each do |name|
+		    define_method(name) do |context|
+			mock.send(name, self)
+		    end
+        	end
+            end
+
+            generator = Class.new(Roby::EventGenerator) do
+		include mod
+	    end.new(true)
+            
+	    hooks.each do |name|
+		mock.should_receive(name).once.with(generator).ordered
+	    end
+            generator.call(nil)
+        end
+    end
+
+    def test_postpone
+	# Simple postpone behavior
+	FlexMock.use do |mock|
+	    wait_for = Roby::EventGenerator.new(true)
+	    event = Roby::EventGenerator.new(true)
+	    event.singleton_class.class_eval do
+		define_method(:calling) do |context|
+		    super if defined? super
+		    postpone(wait_for, "bla") {}
+		end
+	    end
+
+	    event.on { mock.event }
+	    mock.should_receive(:event).never
+	    event.call(nil)
+	    assert(! event.happened? )
+	    assert(! event.pending? )
+	end
+
+	# Test propagation when the block given to postpone 
+	# signals some events
+        FlexMock.use do |mock|
+	    wait_for = Roby::EventGenerator.new(true)
+	    event = Roby::EventGenerator.new(true)
+	    event.singleton_class.class_eval do
+		define_method(:calling) do |context|
+		    super if defined? super
+		    if !wait_for.happened?
+			postpone(wait_for, "bla") do
+			    wait_for.call(nil)
+			end
+		    end
+		end
+	    end
+
+	    wait_for.on { mock.wait_for }
+	    event.on { mock.event }
+	    
+	    mock.should_receive(:wait_for).once.ordered
+	    mock.should_receive(:event).once.ordered
+	    event.call(nil)
+        end
+    end
+
+    def test_call_causal_warning
+	model = Roby::EventGenerator.new { }
+	model.call(nil)
+	assert(! model.active? )
+
+	source = Roby::EventGenerator.new { }
+	def source.active?(seen); true end
+	model = Roby::EventGenerator.new { source.add_causal_link model }
+	assert(! model.active?)
+	model.call(nil)
+	assert(model.active?)
+    end
+
+    def test_emit_on
+	e1, e2 = [nil,nil].map { Roby::EventGenerator.new(true) }
+	e1.emit_on e2
+        FlexMock.use do |mock|
+	    e1.on { mock.e1 }
+	    e2.on { mock.e2 }
+	    mock.should_receive(:e2).once.ordered
+	    mock.should_receive(:e1).once.ordered
+	    e2.call(nil)
+	end
+    end
+
+    def setup_event_aggregator(aggregator)
+	events = 10.enum_for(:times).map { Roby::EventGenerator.new(true) }
+	events.each { |ev| aggregator << ev }
+	events.each do |ev| 
+	    ev.call(nil)
+	    if ev != events[-1]
+		yield
+	    end
+	end
+    end
+
+    def test_or_generator
+	or_event = Roby::OrGenerator.new
+	setup_event_aggregator(or_event) do
+	    assert(or_event.happened?)
+	end
+    end
+
+    def test_and_generator
+	and_event = Roby::AndGenerator.new
+	setup_event_aggregator(and_event) do
+	    assert(!and_event.happened?)
+	end
+	assert(and_event.happened?)
+    end
+
+    def test_forwarder
+	destinations = 5.enum_for(:times).map { Roby::EventGenerator.new(true) }
+	source = Roby::ForwarderGenerator.new(*destinations)
+
+	assert(destinations.all? { |ev| ev.parent_object?(source, Roby::EventStructure::Signals) })
+	source.call(nil)
+	assert(destinations.all? { |ev| ev.happened? })
+    end
+
+    def setup_aggregation(mock)
+	e1, e2, m1, m2, m3 = 5.enum_for(:times).map { Roby::EventGenerator.new(true) }
+	e1.on e2
+	m1.on m2
+	m2.on m3
+
+        (e1 & e2 & m2).on { mock.and }
+        (e2 | m1).on { mock.or }
+        ((e2 & m1) | m2).on { mock.and_or }
+        ((e2 & m1) | m2).on { mock.and_or_p }.
+            permanent!
+
+        ((e2 | m1) & m2).on { mock.or_and }
+        [e1, e2, m1, m2, m3]
+    end
+
+    def test_aggregator
+        FlexMock.use do |mock|
+            e1, e2, m1, *_ = setup_aggregation(mock)
+	    e2.on m1
+            mock.should_receive(:or).once
+            mock.should_receive(:and).once
+            mock.should_receive(:and_or).once
+            mock.should_receive(:and_or_p).once
+            mock.should_receive(:or_and).once
+            e1.call(nil)
+        end
+
+        FlexMock.use do |mock|
+            e1, *_ = setup_aggregation(mock)
+            mock.should_receive(:or).once
+            mock.should_receive(:and).never
+            mock.should_receive(:and_or).never
+            mock.should_receive(:and_or_p).never
+            mock.should_receive(:or_and).never
+            e1.call(nil)
+        end
+
+        FlexMock.use do |mock|
+            _, _, m1 = setup_aggregation(mock)
+            mock.should_receive(:or).once
+            mock.should_receive(:and).never
+            mock.should_receive(:and_or).once
+            mock.should_receive(:and_or_p).once
+            mock.should_receive(:or_and).once
+            m1.call(nil)
+        end
+    end
+
+    def test_and
+	a, b, c = 3.enum_for(:times).map { Roby::EventGenerator.new(true) }
+
+        and_event = a & b
+        and_event.on c
+        assert( a.enum_for(:each_causal_link).find { |ev| ev == and_event } )
+        assert( b.enum_for(:each_causal_link).find { |ev| ev == and_event } )
+        assert( and_event.enum_for(:each_causal_link).find { |ev| ev == c } )
+	a.call(nil)
+	assert(! c.happened?)
+	b.call(nil)
+    end
+
+    def test_ever
+	a, b, c = 3.enum_for(:times).map { Roby::EventGenerator.new(true) }
+        ever_event = a.ever
+        ever_event.on c
+        assert( a.enum_for(:each_causal_link).find { |ev| ev == ever_event } )
+        assert( ever_event.enum_for(:each_causal_link).find { |ev| ev == c } )
+
+	FlexMock.use do |mock|
+	    c.on { mock.c }
+	    
+	    mock.should_receive(:c).twice
+	    a.call(nil)
+	    b.call(nil)
+	    b.ever.on c
+	    Roby.process_events
+	end
+    end
+
+    def test_or
+	a, b, c = 3.enum_for(:times).map { Roby::EventGenerator.new(true) }
+
+        or_event = (a | b)
+        or_event.on c
+        assert( a.enum_for(:each_causal_link).find { |ev| ev == or_event } )
+        assert( or_event.enum_for(:each_causal_link).find { |ev| ev == c } )
+	a.call(nil)
+	assert(c.happened?)
+    end
+
+    def test_ensure
+	setup = lambda do |mock|
+	    e1, e2 = Roby::EventGenerator.new(true), Roby::EventGenerator.new(true)
+	    e1.ensure e2
+	    e1.on { mock.e1 }
+	    e2.on { mock.e2 }
+	    [e1, e2]
+	end
+	FlexMock.use do |mock|
+	    e1, e2 = setup[mock]
+	    mock.should_receive(:e2).ordered.once
+	    mock.should_receive(:e1).ordered.once
+	    e1.call(nil)
+	end
+	FlexMock.use do |mock|
+	    e1, e2 = setup[mock]
+	    mock.should_receive(:e1).never
+	    mock.should_receive(:e2).once
+	    e2.call(nil)
+	end
+	FlexMock.use do |mock|
+	    e1, e2 = setup[mock]
+	    mock.should_receive(:e2).ordered.once
+	    mock.should_receive(:e1).ordered.once
+	    e2.call(nil)
+	    e1.call(nil)
+	end
+    end
+end
+
---
+++ test_suite/test_task.rb
@@ -0,0 +1,337 @@
+require 'test_config'
+require 'test/unit/testcase'
+require 'roby/task'
+require 'flexmock'
+require 'mockups/tasks'
+
+class TC_Task < Test::Unit::TestCase 
+    include Roby
+
+    def setup
+    end
+    attr_reader :task
+
+    def test_base_model
+	task = Class.new(Task) do
+	    event(:start)
+	    event(:stop1)
+	    event(:stop2)
+	    event(:stop3)
+	    event(:stop)
+	    on :stop1 => :stop
+	end.new
+	task.on(:stop2, task, :stop1)
+	task.on(:stop3, task, :stop)
+	assert(task.event(:aborted).terminal?)
+	assert(task.event(:stop1).terminal?)
+	assert(task.event(:stop2).terminal?)
+	assert(task.event(:stop3).terminal?)
+    end
+
+    # Tests Task::event
+    def test_event_declaration
+	klass = Class.new(Task) do
+	    def ev_not_controlable;     end
+	    def ev_method(event = :ev_method); :ev_method if event == :ev_redirected end
+	    def ev_controlable(event = :ev_controlable); :ev_controlable end
+
+	    event :ev_contingent
+	    event :ev_controlable
+	    event :ev_not_controlable, :command => false
+	    event :ev_redirected, :command => lambda { |task, event, *args| task.ev_method(event) }
+	end
+
+	# Must raise because :start is not set
+	assert_raise(TaskModelViolation) { task = klass.new }
+	klass.event :start, :command => true
+
+	# Must raise because there is not terminal event
+	klass.event :ev_terminal, :terminal => true, :command => true
+
+	task = klass.new
+	assert_respond_to(task, :start!)
+
+        # Test modifications to the class hierarchy
+        my_event = nil
+        assert_nothing_raised   { my_event = klass.const_get(:EvContingent) }
+        assert_raise(NameError) { klass.superclass.const_get(:EvContingent) }
+        assert_equal( TaskEvent, my_event.superclass )
+        assert_equal( :ev_contingent, my_event.symbol )
+        assert( klass.has_event?(:ev_contingent) )
+    
+        assert_nothing_raised   { my_event = klass.const_get(:EvTerminal) }
+        assert_equal( :ev_terminal, my_event.symbol )
+
+        # Check properties on EvContingent
+        assert( !klass::EvContingent.respond_to?(:call) )
+        assert( !klass::EvContingent.controlable? )
+        assert( !klass::EvContingent.terminal? )
+
+        # Check properties on EvControlable
+        assert( klass::EvControlable.respond_to?(:call) )
+        event = klass::EvControlable.new(task, nil)
+        # Check for the definition of :call
+        assert_equal(:ev_controlable, klass::EvControlable.call(task, :ev_controlable))
+        # Check for default argument in :call
+        assert_equal(task.ev_controlable, klass::EvControlable.call(task, nil))
+        assert( klass::EvControlable.controlable? )
+
+        # Check Event.terminal? if :terminal => true
+        assert( klass::EvTerminal.terminal? )
+
+        # Check :controlable => [proc] behaviour
+        assert( klass::EvRedirected.controlable? )
+        
+        # Check that :command => false disables controlable?
+        assert( :ev_not_controlable, !klass::EvNotControlable.controlable? )
+
+        # Check validation of options[:command]
+        assert_raise(ArgumentError) { klass.event :try_event, :command => "bla" }
+    end
+
+    def test_task_event_properties
+        task = EmptyTask.new
+	start_event = task.event(:start)
+
+        assert_equal(start_event, task.event(:start))
+        assert_equal([], start_event.handlers)
+        assert_equal([task.event(:success)], start_event.enum_for(:each_signal).to_a)
+        start_model = task.event_model(:start)
+        assert_equal(start_model, start_event.model)
+        assert_equal([:success], task.enum_for(:each_signal, :start).to_a)
+     end
+
+    def test_task_propagation
+        start_node = EmptyTask.new
+
+	# Check can_signal? for task events
+        start_event = start_node.event(:start)
+	stop_event  = start_node.event(:stop)
+	assert( start_event.can_signal?(stop_event) )
+
+	# Check that propagation is done properly in this simple task
+	FlexMock.use do |mock|
+	    start_node.on(:start) { |event| mock.started(event.context) }
+	    mock.should_receive(:started).once.with(42)
+	    start_node.start!(42)
+	end
+        assert(start_node.finished?)
+	event_history = start_node.history.map { |_, ev| ev.generator }
+	assert_equal([start_node.event(:start), start_node.event(:success), start_node.event(:stop)], event_history)
+    end
+
+    def test_inheritance_overloading
+        base = Class.new(Roby::Task) do 
+            extend Test::Unit::Assertions
+            event :ctrl, :command => true
+	    event :stop
+            assert(!find_event_model(:stop).controlable?)
+        end
+
+        Class.new(base) do
+            extend Test::Unit::Assertions
+
+            assert_nothing_raised { event :start, :command => true }
+            assert_raises(ArgumentError) { event :ctrl, :command => false }
+            assert_raises(ArgumentError) { event :failed, :terminal => false }
+            assert_raises(ArgumentError) { event :failed }
+
+            def stop(context)
+            end
+            assert_nothing_raised { event :stop }
+            assert(find_event_model(:stop).controlable?)
+        end
+
+	Class.new(base) do
+	    def start(context)
+	    end
+	    assert_nothing_raised { event :start }
+	end
+    end
+
+    def test_singleton
+	model = Class.new(Task) do
+	    def initialize
+		singleton_class.event(:start)
+		singleton_class.event(:stop)
+		super
+	    end
+	    event :inter
+	end
+
+	ev_models = Hash[*model.enum_for(:each_event).to_a.flatten]
+	assert_equal([:success, :aborted, :failed, :inter].to_set, ev_models.keys.to_set)
+
+	task = model.new
+	ev_models = Hash[*task.model.enum_for(:each_event).to_a.flatten]
+	assert_equal(6, ev_models.keys.size)
+	assert( ev_models[:start].symbol )
+	assert( ev_models[:start].name || ev_models[:start].name.length > 0 )
+    end
+
+    def test_check_running
+	task = Class.new(Roby::Task) do
+	    event(:start, :command => true)
+	    event(:inter, :command => true)
+	    event(:stop, :command => true)
+	end.new
+
+	assert_raises(Roby::TaskModelViolation) { task.inter! }
+	assert_equal(0, task.event(:inter).pending)
+	task.start!
+	assert_nothing_raised { task.inter! }
+	task.stop!
+	assert_raises(Roby::TaskModelViolation) { task.inter! }
+	assert_equal(0, task.event(:inter).pending)
+    end
+
+    def test_aborted_until
+	klass = Class.new(Roby::Task) do
+	    event(:start, :command => true)
+	    event(:stop, :command => true)
+	end
+	parent, child = klass.new, klass.new
+
+	# p:start -> c:start -> c:failed --> c:stop -> p:stop
+	#				 |-> p:aborted -> p:stop
+	# we make the c:failed -> p:aborted link being until p:stop
+	#child.on(:stop, parent, :stop)
+	#child.event(:failed).until(parent.event(:stop)).on parent.event(:aborted)
+
+	parent.on(:start, child, :start)
+	child.on(:start, child, :failed)
+	child.on(:failed, parent, :aborted)
+
+        FlexMock.use do |mock|
+	    parent.on(:start)	{ mock.p_start }
+	    child.on(:start)	{ mock.c_start }
+	    child.on(:failed)	{ mock.c_failed }
+	    parent.on(:aborted) { mock.p_aborted }
+	    parent.on(:stop)	{ mock.p_stop }
+	    child.on(:stop)	{ mock.c_stop }
+
+	    mock.should_receive(:p_start).once.ordered
+	    mock.should_receive(:c_start).once.ordered
+	    mock.should_receive(:c_failed).once.ordered
+	    mock.should_receive(:p_aborted).once.ordered(:aborted_stop)
+	    mock.should_receive(:c_stop).once.ordered(:aborted_stop)
+	    mock.should_receive(:p_stop).once.ordered
+
+	    parent.start!
+	end
+    end
+
+    def test_aborted_default_handler
+	klass = Class.new(Roby::Task) do
+	    event(:start, :command => true)
+	end
+
+	t1, t2 = klass.new, klass.new
+	t1.add_child(t2)
+
+	FlexMock.use do |mock|
+	    t1.on(:start) { mock.t1_start }
+	    t2.event(:aborted).on { mock.t2 }
+	    t1.event(:aborted).on { mock.t1 }
+	    mock.should_receive(:t1_start).once.ordered
+	    mock.should_receive(:t2).once.ordered
+	    mock.should_receive(:t1).once.ordered
+
+	    t1.start!
+	    t2.start!
+	    t1.aborted!
+	end
+    end
+
+    def test_event_loop
+        start_node = EmptyTask.new
+        next_event = [ start_node, :start ]
+        if_node    = ChoiceTask.new
+        start_node.on(:stop) { next_event = [if_node, :start] }
+	if_node.on(:stop) { raise Interrupt }
+            
+        Roby.event_processing << lambda do 
+            next unless next_event
+            task, event = *next_event
+            next_event = nil
+            task.event(event).call(nil)
+        end
+        assert_doesnt_timeout(1) { Roby.run }
+        assert(start_node.finished?)
+	assert(if_node.finished?)
+    end
+
+    def test_task_success_failure
+	FlexMock.use do |mock|
+	    t = EmptyTask.new
+	    [:start, :success, :stop].each do |name|
+		t.on(name) { mock.send(name) }
+		mock.should_receive(name).once.ordered
+	    end
+	    t.start!
+	end
+    end
+
+    def aggregator_test(a, *tasks)
+	FlexMock.use do |mock|
+	    [:start, :success, :stop].each do |name|
+		a.on(name) { mock.send(name) }
+		mock.should_receive(name).once.ordered
+	    end
+	    a.start!
+	    assert( tasks.all? { |t| t.finished? })
+	end
+    end
+
+    def test_task_parallel_aggregator
+        t1, t2 = EmptyTask.new, EmptyTask.new
+	aggregator_test((t1 | t2), t1, t2)
+        t1, t2 = EmptyTask.new, EmptyTask.new
+	aggregator_test( (t1 | t2).to_task, t1, t2 )
+    end
+
+    def test_task_sequence_aggregator
+        t1, t2 = EmptyTask.new, EmptyTask.new
+	aggregator_test( (t1 + t2), t1, t2 )
+        t1, t2 = EmptyTask.new, EmptyTask.new
+	s = t1 + t2
+	aggregator_test( s.to_task, t1, t2 )
+	assert(! t1.event(:stop).related_object?(s.event(:stop)))
+
+        t1, t2, t3 = EmptyTask.new, EmptyTask.new, EmptyTask.new
+        s = t2 + t3
+	s.unshift t1
+	aggregator_test(s, t1, t2, t3)
+	
+        t1, t2, t3 = EmptyTask.new, EmptyTask.new, EmptyTask.new
+        s = t2 + t3
+	s.unshift t1
+	aggregator_test(s.to_task, t1, t2, t3)
+    end
+
+    def test_multi_task_signalling
+	# Check a more complex setup
+        start_node = EmptyTask.new
+        if_node = ChoiceTask.new
+        start_node.on(:stop, if_node, :start)
+        start_node.start!
+        assert(start_node.finished? && if_node.finished?)
+
+	# Check history
+	event_history = if_node.history.map { |_, ev| ev.generator }
+	assert_equal(4, event_history.size, "  " + event_history.join("\n"))
+	assert_equal(if_node.event(:start), event_history.first)
+	assert( if_node.event(:a) == event_history[1] || if_node.event(:b) == event_history[1] )
+	assert_equal(if_node.event(:stop), event_history.last)
+
+        multi_hop = MultiEventTask.new
+        multi_hop.start!
+        assert(multi_hop.finished?)
+	event_history = multi_hop.history.map { |_, ev| ev.generator }
+	expected_history = [:start, :inter, :success, :stop].map { |name| multi_hop.event(name) }
+	assert_equal(expected_history, event_history)
+    end
+
+
+end
+
