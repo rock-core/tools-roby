be0b6049a8f5bced0c3905dbe14211c9a5955f11
tailor <sylvain.joyeux@laas.fr>
2006-08-21 10:55:45 +0000

    [support] use Logger::Hierarchy and Logger::Forward from utilrb

--- lib/roby/support.rb
+++ lib/roby/support.rb
@@ -3,6 +3,8 @@ require 'genom/support'
 
 require 'roby/graph'
 require 'utilrb/enumerable'
+require 'utilrb/logger'
+require 'utilrb/gc/force'
 require 'facet/kernel/constant'
 require 'facet/module/dirname'
 require 'monitor'
@@ -129,47 +131,6 @@ class Hash
     end
 end
 
-class Logger
-    # Define a hierarchy of loggers mapped to the module hierarchy.
-    # It defines the #logger accessor which either returns the logger
-    # attribute of the module, if one is defined, or its parent logger
-    # attribute.
-    # 
-    # module First
-    #   include Hierarchy
-    #   self.logger = Logger.new
-    #
-    #   module Second
-    #     include Hierarchy
-    #   end
-    # end
-    #
-    # Second.logger will return First.logger. If we do
-    # Second.logger = Logger.new, then this one would
-    # be returned.
-    module Hierarchy
-        attr_writer :logger
-        def logger
-            return @logger if defined?(@logger) && @logger
-            if kind_of?(Module)
-                constant(self.dirname).logger
-            else
-                self.class.logger
-            end
-        end
-    end
-
-    # Forward logger output methods to the logger attribute
-    module Forward
-        [ :debug, :info, :warn, :error, :fatal, :unknown ].each do |level|
-            class_eval <<-EOF
-                def #{level}(*args, &proc); logger.#{level}(*args, &proc) end
-            EOF
-        end
-    end
-end
-
-require 'logger'
 module Roby
     @logger = Logger.new(STDERR)
     @logger.level = Logger::DEBUG
