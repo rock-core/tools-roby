fa6358a5e661e76644480a2096bf3e984b177442
tailor <sylvain.joyeux@m4x.org>
2006-09-11 15:31:38 +0000

    [log] documentation updates

--- lib/roby/log/logger.rb
+++ lib/roby/log/logger.rb
@@ -3,6 +3,7 @@ require 'roby/log/hooks'
 module Roby::Log
     @loggers = Array.new
     class << self
+	# All logger objects in the system
 	attr_reader :loggers
 
 	# Iterates on all the logger objects. If +m+ is given, yields only the loggers
@@ -21,6 +22,8 @@ module Roby::Log
 	end
     end
 
+    # A logger object which marshals events in a IO object. The log files can
+    # be replayed (for off-line display) by calling FileLogger.replay(io)
     class FileLogger
 	@dumped = Hash.new
 	class << self
@@ -64,15 +67,18 @@ module Roby::Log
 	end
     end
 
+    # A logger object which dumps events in a human-readable form to an IO object. 
     class ConsoleLogger
 	def self.filter_names(name)
 	    name.gsub(/Roby::(?:Genom::)?/, '')
 	end
+	# Name of an event generator source
 	def self.gen_source(gen)
 	    if gen.respond_to?(:task) then gen.task.name
 	    else 'toplevel'
 	    end
 	end
+	# Human readable name for event generators
 	def self.gen_name(gen)
 	    if gen.respond_to?(:symbol) then "[#{gen.symbol}]"
 	    else gen.name
@@ -85,7 +91,7 @@ module Roby::Log
 	    @columns = Array.new
 	end
 
-	def display(time, *args)
+	def display(time, *args) # :nodoc:
 	    @reftime ||= time
 
 	    if @last_ref == args[0, 2]
@@ -108,21 +114,22 @@ module Roby::Log
 	    end
 	    io << "\n"
 	end
+	private :display
 
-	def generator_calling(time, gen, context)
+	def generator_calling(time, gen, context) # :nodoc:
 	    display(time, ConsoleLogger.gen_source(gen), ConsoleLogger.gen_name(gen), "call", "ctxt=#{context.inspect}")
 	end
-	def generator_fired(time, event)
+	def generator_fired(time, event) # :nodoc:
 	    display(time, ConsoleLogger.gen_source(event.generator), 
 		    ConsoleLogger.gen_name(event.generator), "fired", 
 		    "#{ConsoleLogger.gen_name(event)}!#{event.source_address.to_s(16)} ctxt=#{event.context.inspect}")
 	end
-	def generator_signalling(time, event, generator)
+	def generator_signalling(time, event, generator) # :nodoc:
 	    display(time, ConsoleLogger.gen_source(event.generator), 
 		    ConsoleLogger.gen_name(event.generator), "signal", 
 		    "#{ConsoleLogger.gen_name(event)}!#{event.source_address.to_s(16)} -> #{ConsoleLogger.gen_source(generator)}::#{ConsoleLogger.gen_name(generator)}")
 	end
-	def task_initialize(time, task, start, stop)
+	def task_initialize(time, task, start, stop) # :nodoc:
 	    display(time, task.name, "", "new_task")
 	end
     end
