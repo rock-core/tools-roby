ef82cf5c8e0a6b155505c366a08cee07de4d764d
tailor <sylvain.joyeux@laas.fr>
2006-10-02 15:18:16 +0000

    [test] add transaction tests to test_core

--- test/test_core.rb
+++ test/test_core.rb
@@ -8,6 +8,8 @@ require 'test_task.rb'
 
 require 'test_plan.rb'
 require 'test_planning.rb'
+require 'test_transactions.rb'
+require 'test_transactions_proxy.rb'
 
 require 'test_server.rb'
 require 'test_state.rb'
