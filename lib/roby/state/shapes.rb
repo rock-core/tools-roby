c1e5d793b6aaf9cbf2f834781e7a77164dafb452
tailor <sylvain.joyeux@laas.fr>
2006-10-16 08:08:44 +0000

    [state] there is not enough shapes to need a deep directory hierarchy. Move Cylinder in state/shapes.rb and add the Cube shape

--- lib/roby/state/shapes.rb
+++ lib/roby/state/shapes.rb
@@ -1,5 +1,32 @@
+class Cylinder
+    attr_accessor :radius, :height, :axis
+    def initialize(radius, height, axis)
+        @radius, @height, @axis = radius.to_f, height.to_f, axis.to_f
+    end
+    def diameter(axis)
+	if axis == self.axis
+	    radius * 2
+	else
+	    raise NotImplementedError
+	end
+    end
+    alias :max_length :diameter 
+    def length; diameter(:z) end 
+    def width; diameter(:z) end 
+end
 
-Dir.glob( File.join(File.dirname(__FILE__), 'shapes', '**', '*.rb') ) do |file|
-    require file
+class Cube
+    attr_accessor :length, :width, :height
+    def initialize(length, width, height)
+	@length, @width, @height = length.to_f, width.to_f, height.to_f
+    end
+
+    def max_length(axis)
+	if axis == :z
+	    [length, width].max
+	else
+	    height
+	end
+    end
 end
 
