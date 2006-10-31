d2587d5c0bacf09a6ef5fffc31ced6a5850a1897
tailor <sylvain.joyeux@laas.fr>
2006-10-31 17:57:38 +0000

    [planned_by] define #planned_tasks

--- lib/roby/relations/planned_by.rb
+++ lib/roby/relations/planned_by.rb
@@ -3,6 +3,7 @@ require 'roby/task'
 module Roby::TaskStructure
     relation :PlannedBy, :child_name => :planning_task, :parent_name => :planned_task, :noinfo => true do
 	def planned_task; enum_for(:each_planned_task).find { true } end
+	def planned_tasks; enum_for(:each_planned_task) end
 	def planning_task; enum_for(:each_planning_task).find { true } end
         def planned_by(task)
             raise TaskModelViolation.new(self), "this task already has a planner" if planning_task
