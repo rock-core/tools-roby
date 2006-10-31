c718a3428f4745e9e29cd60a91d65b3f79d595e2
tailor <sylvain.joyeux@laas.fr>
2006-10-31 17:56:20 +0000

    [signal] added #eqv? to test temporal equivalence

--- lib/roby/relations/signals.rb
+++ lib/roby/relations/signals.rb
@@ -1,6 +1,14 @@
 require 'roby/event'
 module Roby::EventStructure
-    relation :Signal, :noinfo => true
+    relation :Signal, :noinfo => true do
+	# Returns true if the two events are linked via a signal one
+	# way or the other
+	def eqv?(other)
+	    self.child_object?(other, Roby::EventStructure::Signal) || 
+		other.child_object?(self, Roby::EventStructure::Signal)
+	end
+    end
+
     relation :Forwarding, :noinfo => true
 end
 
