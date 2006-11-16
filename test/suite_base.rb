03d2a31ea7621b72542cf9a3da701e20a0e9333d
tailor <sylvain.joyeux@laas.fr>
2006-11-16 14:15:02 +0000

    [test] splitted test_core into a sub test suite suite_base

---
+++ test/suite_base.rb
@@ -0,0 +1,11 @@
+require 'test_support'
+
+require 'test_bgl'
+require 'test_relations'
+require 'test_event'
+require 'test_task'
+require 'test_state'
+
+require 'test_propagation'
+require 'test_exceptions'
+
---
+++ test/suite_core.rb
@@ -0,0 +1,13 @@
+require 'suite_base'
+
+require 'test_executed_by'
+require 'test_plan'
+require 'test_planning'
+require 'test_transactions'
+require 'test_transactions_proxy'
+
+require 'test_server'
+require 'test_control'
+require 'test_log'
+require 'test_distributed'
+
