f7232bdea82dde52fcf22f2ae01ac9eaf81fc82e
tailor <sylvain.joyeux@m4x.org>
2006-12-06 10:49:12 +0000

    [distributed,test] add signalling test

--- test/distributed/test_execution.rb
+++ test/distributed/test_execution.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('..', File.dirname(__FILE__))
 require 'distributed/common.rb'
 require 'mockups/tasks'
+require 'flexmock'
 
 class TC_DistributedExecution < Test::Unit::TestCase
     include DistributedTestCommon
@@ -19,14 +20,24 @@ class TC_DistributedExecution < Test::Unit::TestCase
 
     def test_signalling
 	peer2peer do |remote|
-	    remote.insert(Task.new(:id => 1))
+	    remote.plan.insert(task = SimpleTask.new(:id => 1))
+	    remote.class.class_eval do
+		define_method(:start_task) { task.start! }
+	    end
 	end
 	r_task = remote_task(:id => 1)
-	task = Task.new
-
-	# Get a proxy for r_task
 	p_task = remote_peer.proxy(r_task)
-	# More tricky, get a proxy *on remote* for +task+
+
+	FlexMock.use do |mock|
+	    ev = EventGenerator.new do |event|
+		mock.called(event)
+	    end
+
+	    mock.should_receive(:called).once
+	    p_task.event(:start).on ev
+	    remote.start_task
+	    apply_remote_command
+	end
     end
 end
 
