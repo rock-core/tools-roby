165e1522272f7d15d1fe2aa4cfce35c3a56de4b8
tailor <sylvain.joyeux@laas.fr>
2006-12-18 17:36:21 +0000

    [test] complete suite_core with the missing files

--- test/suite_core.rb
+++ test/suite_core.rb
@@ -1,12 +1,16 @@
 require 'suite_base'
 
-require 'test_executed_by'
 require 'test_plan'
+require 'test_query'
 require 'test_planning'
 require 'test_transactions'
 require 'test_transactions_proxy'
 
+require 'test_executed_by'
+require 'test_realized_by'
+
 require 'test_server'
 require 'test_control'
+require 'test_control_interface'
 require 'test_log'
 
