4094d4f1391c9439b7befc2f51caa13747ddcfbd
tailor <sylvain.joyeux@laas.fr>
2006-12-18 15:52:54 +0000

    [ext] add a -rpath option for the libraries found by pkg-config

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 CONFIG['CC'] = "g++"
 $LDFLAGS += "-module"
-pkg_config('utilmm')
+create_, ldflags, _ = pkg_config 'utilmm'
+$LDFLAGS << ldflags.gsub('-L', ' -Wl,-rpath -Wl,')
 create_makefile("bgl")
 
