b86023b93aefe26157c2d8de1840471f9829d659
tailor <sylvain.joyeux@laas.fr>
2007-01-11 17:41:33 +0000

    [state] define ExtendedStruct#to_hash

--- lib/roby/state/state.rb
+++ lib/roby/state/state.rb
@@ -88,6 +88,8 @@ module Roby
 	    name = name.to_s if name
 	    @observers[name] << block
 	end
+
+	def to_hash; @members.dup end
         
 	# Update a set of values on this struct
 	# If a hash is given, it is an name => value hash of attribute
