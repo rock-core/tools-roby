9429f405653c0ea16056a5c648b1948fcaaa0c6c
tailor <sylvain.joyeux@m4x.org>
2007-02-12 14:30:58 +0000

    [app-template] add comments to config/init

--- lib/roby/app/config/init.rb
+++ lib/roby/app/config/init.rb
@@ -1,4 +1,18 @@
-# App-global configuration
+# This file is called to do application-global configuration. For configuration
+# specific to a robot, edit config/NAME.rb, where NAME is the robot name.
 
 # Roby setup. Always require this file
 require 'roby/app/config/roby'
+
+# Require separate Roby components
+#
+# Planning
+# ROBY_COMPONENTS << "planning"
+#
+# Genom adapter
+# ROBY_COMPONENTS << "genom"
+#
+# Distributed Roby
+# ROBY_COMPONENTS << "distributed"
+
+
