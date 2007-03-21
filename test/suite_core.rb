01353e580400093b3b4b12a2a77ec3491db821f2
tailor <sylvain.joyeux@laas.fr>
2007-03-21 08:24:20 +0000

    [test] remove suite_base by merging it into suite_core

--- test/suite_core.rb
+++ test/suite_core.rb
@@ -1,4 +1,14 @@
-require 'suite_base'
+require 'test_support'
+
+require 'test_bgl'
+require 'test_relations'
+require 'test_event'
+require 'test_task'
+require 'test_state'
+
+require 'test_propagation'
+require 'test_exceptions'
+
 
 require 'test_plan'
 require 'test_query'
