b2a05f07487edfe925ca1b5f9021d38edb645056
tailor <sylvain.joyeux@laas.fr>
2007-03-28 13:14:23 +0000

    [app] the application configuration file is now config/app.yml. config/roby.yml is used by the 'roby' tool

--- lib/roby/app/load.rb
+++ lib/roby/app/load.rb
@@ -8,7 +8,7 @@ $LOAD_PATH.unshift APP_DIR
 
 # We expect here that both APP_DIR and ROBOT are set
 conf = Roby.app
-file = File.join(APP_DIR, 'config', 'roby.yml')
+file = File.join(APP_DIR, 'config', 'app.yml')
 file = YAML.load(File.open(file))
 
 conf.load(file)
