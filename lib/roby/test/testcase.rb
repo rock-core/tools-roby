2819b524c4ee79c5bcc0ae15aaadee1202193878
tailor <sylvain.joyeux@laas.fr>
2007-04-14 12:51:36 +0000

    [app-test] better implementation of assert_events, which has been renamed assert_any_event for the occasion

--- lib/roby/test/testcase.rb
+++ lib/roby/test/testcase.rb
@@ -4,52 +4,134 @@ class String # :nodoc: all
 end
 
 require 'roby/test/common'
+require 'roby/app'
 require 'fileutils'
 
 module Roby
     module Test
-	@waiting_threads = []
-	def self.waiting_threads; @waiting_threads end
+	@event_assertions = []
+	@waiting_threads  = []
 
-	module Assertions
-	    def assert_events(positive, negative = [])
-		done_mutex = Mutex.new
-		done       = ConditionVariable.new
-		positive = Array[*positive]
-		negative = Array[*negative]
+	ASSERT_ANY_EVENTS_TLS = :assert_any_events
 
-		done_mutex.synchronize do
-		    loop do
-			Roby::Control.synchronize do
-			    if positive.any? { |ev| ev.happened? }
-				return
-			    elsif failure = negative.find { |ev| ev.happened? }
-				flunk("event #{failure} happened")
-			    else
-				(positive + negative).each do |ev| 
-				    ev.on do 
-					done_mutex.synchronize do
-					    done.broadcast 	
-					end
-				    end
-				end
-			    end
-			end
-			yield if block_given?
-			Roby::Test.waiting_threads << Thread.current
-			done.wait(done_mutex)
+	class << self
+	    # A [thread, cv, positive, negative] list of event assertions
+	    attr_reader :event_assertions
+
+	    # Tests for events in +positive+ and +negative+ and returns
+	    # the set of failing events if the assertion has finished.
+	    # If the set is empty, it means that the assertion finished
+	    # successfully
+	    def assert_any_event_result(positive, negative)
+		if positive.any? { |ev| ev.happened? }
+		    return []
+		else
+		    failure = negative.find_all { |ev| ev.happened? }
+		    unless failure.empty?
+			return failure
 		    end
 		end
 
+		nil
+	    end
+
+	    # This method is inserted in the control thread to implement
+	    # Assertions#assert_events
+	    def check_event_assertions
+		event_assertions.delete_if do |thread, cv, positive, negative|
+		    if result = assert_any_event_result(positive, negative)
+			thread[ASSERT_ANY_EVENTS_TLS] = result
+			cv.broadcast
+			true
+		    end
+		end
+	    end
+
+	    def finalize_event_assertions
+		check_event_assertions
+		event_assertions.dup.each do |thread, *_|
+		    thread.raise ControlQuitError
+		end
+	    end
+
+	    # A set of threads waiting for something to happen. This is used
+	    # during #teardown to make sure no threads are block indefinitely
+	    attr_reader :waiting_threads
+
+	    # This proc is to be called by Control when it quits. It makes sure
+	    # that threads which are waiting are interrupted
+	    def interrupt_waiting_threads
+		waiting_threads.dup.each do |task|
+		    task.raise ControlQuitError
+		end
 	    ensure
-		Roby::Test.waiting_threads.delete(Thread.current)
+		waiting_threads.clear
+	    end
+	end
+	Roby::Control.at_cycle_end(&method(:check_event_assertions))
+	Roby::Control.finalizers << method(:finalize_event_assertions)
+	Roby::Control.finalizers << method(:interrupt_waiting_threads)
+
+	module Assertions
+	    # Wait for any event in +positive+ to happen. If +negative+ is
+	    # non-empty, any event happening in this set will make the
+	    # assertion fail. If events in +positive+ are task events, the
+	    # :stop events of the corresponding tasks are added to negative
+	    # automatically.
+	    #
+	    # If a block is given, it is called from within the control thread
+	    # after the checks are in place
+	    #
+	    # So, to check that a task fails, do
+	    #
+	    #	assert_events(task.event(:fail)) do
+	    #	    task.start!
+	    #	end
+	    #
+	    def assert_any_event(positive, negative = [], &block)
+		Roby.condition_variable(false) do |cv|
+		    positive = Array[*positive].to_value_set
+		    negative = Array[*negative].to_value_set
+		    positive.each do |ev|
+			if ev.respond_to?(:task)
+			    stop = ev.task.event(:stop)
+			    unless positive.include?(stop)
+				negative << stop
+			    end
+			end
+		    end
+
+		    Roby::Control.synchronize do
+			failing_events = nil
+			unless failing_events = Test.assert_any_event_result(positive, negative)
+			    this_thread = Thread.current
+
+			    Test.event_assertions << [this_thread, cv, positive, negative]
+			    Roby.once(&block) if block_given?
+			    begin
+				cv.wait(Roby::Control.mutex)
+			    ensure
+				Test.event_assertions.delete_if { |thread, *_| thread == this_thread }
+			    end
+
+			    failing_events = this_thread[ASSERT_ANY_EVENTS_TLS]
+			end
+
+			unless failing_events.empty?
+			    flunk("events #{failing_events.join(", ")} happened")
+			end
+		    end
+		end
 	    end
 
 	    # Starts +task+ and checks it succeeds
 	    def assert_succeeds(task)
-		plan.permanent(task)
-		assert_events(task.event(:success), task.event(:stop)) do
-		    Roby::Control.once { task.start! }
+		Roby::Control.synchronize do
+		    plan.permanent(task)
+		end
+
+		assert_any_event(task.event(:success), task.event(:stop)) do
+		    task.start!
 		end
 	    end
 	end
@@ -66,23 +148,12 @@ module Roby
 
 	    def run(result)
 		Roby::Test.waiting_threads.clear
-		Roby::Control.finalizers << method(:control_finalizer)
 
 		Roby.app.simulation
 		Roby.app.single
 		Roby.app.setup
 		Roby.app.run do
 		    super
-		end
-
-	    ensure
-		Roby::Control.finalizers.delete(method(:control_finalizer))
-	    end
-
-	    attr_reader :waiting_threads
-	    def control_finalizer
-		Roby::Test.waiting_threads.each do |task|
-		    task.raise ControlQuitError
 		end
 	    end
 
--- test/test_testcase.rb
+++ test/test_testcase.rb
@@ -4,69 +4,73 @@ require 'roby/test/testcase'
 require 'mockups/tasks'
 require 'flexmock'
 
-class TC_Task < Test::Unit::TestCase 
+class TC_Test_TestCase < Test::Unit::TestCase 
     include Roby::Test
     include Roby::Test::Assertions
 
-    def test_assert_events
+    def test_assert_any_event
 	plan.discover(t = SimpleTask.new)
 	t.start!
 	assert_nothing_raised do
-	    assert_events(t.event(:start))
+	    assert_any_event(t.event(:start))
 	end
 
 	t.success!
 	assert_nothing_raised do
-	    assert_events(t.event(:start))
-	    assert_events([t.event(:success)], [t.event(:stop)])
+	    assert_any_event(t.event(:start))
+	    assert_any_event([t.event(:success)], [t.event(:stop)])
 	end
 
 	plan.discover(t = SimpleTask.new)
 	t.start!
 	t.failed!
 	assert_raises(Test::Unit::AssertionFailedError) do
-	    assert_events([t.event(:success)], [t.event(:stop)])
+	    assert_any_event([t.event(:success)], [t.event(:stop)])
 	end
 
-	Roby.logger.level = Logger::DEBUG
+	Roby.logger.level = Logger::FATAL
 	Roby.control.run :detach => true
 	plan.insert(t = SimpleTask.new)
-
-	main = Thread.current
-	watchdog = Thread.new do
-	    # Wait for the main thread to acquire the lock and start the task
-	    wait_thread_stopped(main)
-	    Roby::Control.once do 
-		t.start!
-		t.success!
-	    end
+	assert_any_event(t.event(:success)) do 
+	    t.start!
+	    t.success!
 	end
-	assert_events([t.event(:success)], [t.event(:stop)])
-	watchdog.join
 
-	# Make control quit and check what assert_events does
+	# Make control quit and check that we get ControlQuitError
 	plan.insert(t = SimpleTask.new)
-	main = Thread.current
-	watchdog = Thread.new do
-	    # Wait for the main thread to acquire the lock and start the task
-	    wait_thread_stopped(main)
-	    Roby::Control.once do 
+	assert_raises(Test::Unit::AssertionFailedError) do
+	    assert_any_event(t.event(:success)) do
 		t.start!
 		t.failed!
 	    end
 	end
-	assert_events([t.event(:success)], [t.event(:stop)])
-	watchdog.join
+
+	## Same test, but check that the assertion succeeds since we *are*
+	## checking that +failed+ happens
+	Roby.control.run :detach => true
+	plan.insert(t = SimpleTask.new)
+	assert_nothing_raised do
+	    assert_any_event(t.event(:failed)) do
+		t.start!
+		t.failed!
+	    end
+	end
     end
 
     def test_assert_succeeds
 	Roby.control.run :detach => true
-	model = Class.new(SimpleTask) do
+    
+	task = Class.new(SimpleTask) do
 	    forward :start => :success
-	end
-	task = model.new
-
+	end.new
 	assert_nothing_raised do
+	    assert_succeeds(task)
+	end
+
+	task = Class.new(SimpleTask) do
+	    forward :start => :failed
+	end.new
+	assert_raises(Test::Unit::AssertionFailedError) do
 	    assert_succeeds(task)
 	end
     end
