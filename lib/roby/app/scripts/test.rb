2f87e161fd46e25aae619ab883c30813325afffc
tailor <sylvain.joyeux@laas.fr>
2007-04-14 12:49:58 +0000

    [app] for Test::Unit::AutoRunner, all TestCase subclasses *are* test cases. Thus, Roby::Test::TestCase is one as well. Filter this class out in script/test

--- lib/roby/app/scripts/test.rb
+++ lib/roby/app/scripts/test.rb
@@ -1,7 +1,12 @@
 require 'roby'
 require 'roby/app'
 require 'test/unit'
-(r = Test::Unit::AutoRunner.new(true)).process_args(ARGV) or
+r = Test::Unit::AutoRunner.new(true)
+r.filters << lambda do |t|
+    t.class != Roby::Test::TestCase
+end
+
+r.process_args(ARGV) or
   abort r.options.banner + " tests..."
 exit r.run
 
