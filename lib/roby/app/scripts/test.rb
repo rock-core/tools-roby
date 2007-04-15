c686ee6eac15f7e0046eef88e13108a05eb8e807
tailor <sylvain.joyeux@laas.fr>
2007-04-15 18:06:37 +0000

    [app] in scripts/test, add the default filter only if no filter has been given on command line   Otherwise, all testcases are always executed

--- lib/roby/app/scripts/test.rb
+++ lib/roby/app/scripts/test.rb
@@ -2,11 +2,14 @@ require 'roby'
 require 'roby/app'
 require 'test/unit'
 r = Test::Unit::AutoRunner.new(true)
-r.filters << lambda do |t|
-    t.class != Roby::Test::TestCase
-end
-
 r.process_args(ARGV) or
   abort r.options.banner + " tests..."
+
+if r.filters.empty?
+    r.filters << lambda do |t|
+	t.class != Roby::Test::TestCase
+    end
+end
+
 exit r.run
 
