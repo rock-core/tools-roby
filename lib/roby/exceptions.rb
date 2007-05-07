0103e86973126eed43157fb09ff39396e7122afa
tailor <sylvain.joyeux@laas.fr>
2007-05-07 12:18:26 +0000

    [core] fixed filter_backtrace: the changes related to dRoby handling have broken filtering for non-dRoby backtraces (too much spaces)

--- lib/roby/exceptions.rb
+++ lib/roby/exceptions.rb
@@ -134,7 +134,7 @@ module Roby
     def self.filter_backtrace(backtrace)
 	backtrace = backtrace.dup
 	backtrace.delete_if do |caller|
-	    caller =~ /^( (?:\(roby:\/\/.+\) )?#{Regexp.quote(ROBY_LIB_DIR)}|scripts\/)/
+	    caller =~ /^((?:\s*\(roby:\/\/.+\)\s*)?#{Regexp.quote(ROBY_LIB_DIR)}|scripts\/)/
 	end
 	backtrace
     end
