675aa85bf54a90c22e29f228114fa7a2ee8afaa5
tailor <sylvain.joyeux@laas.fr>
2007-05-10 06:57:30 +0000

    [benchmark] add benchmark/alloc_misc, which measures object allocation in various Ruby structures

---
+++ benchmark/alloc_misc.rb
@@ -0,0 +1,88 @@
+require 'utilrb/objectstats'
+require 'utilrb/value_set'
+require 'enumerator'
+require 'set'
+
+GC.disable
+
+puts "==== Iteration"
+[(1..10_000), (1..10_000).to_a, (1..10_000).to_value_set, (1..10_000).enum_for, (1..10_000).to_set].each do |set|
+    before = ObjectSpace.live_objects
+    for obj in set
+	10
+    end
+    after  = ObjectSpace.live_objects
+    puts "#{set.class} for: #{after - before}"
+
+    before = ObjectSpace.live_objects
+    set.each { |obj| 10 }
+    after  = ObjectSpace.live_objects
+    puts "#{set.class} each: #{after - before}"
+end
+
+puts "===== Method calls"
+def bm_args_yield(a, b)
+    yield(a, b) if block_given?
+end
+def bm_args_yield_through_block(a, b, &block)
+    block.call(a, b) if block
+end
+
+def bm_method_call
+end
+
+def bm_yield
+    yield
+end
+def bm_block_yield(&block)
+    yield
+end
+
+def bm_block(&block)
+    block.call
+end
+
+before = ObjectSpace.live_objects
+bm_method_call
+after  = ObjectSpace.live_objects
+puts "Method call: #{after - before}"
+
+before = ObjectSpace.live_objects
+bm_method_call { 10 }
+after  = ObjectSpace.live_objects
+puts "Method call with block: #{after - before}"
+
+before = ObjectSpace.live_objects
+bm_yield { 10 }
+after  = ObjectSpace.live_objects
+puts "Yield: #{after - before}"
+
+before = ObjectSpace.live_objects
+bm_block_yield { 10 }
+after  = ObjectSpace.live_objects
+puts "Block and yield: #{after - before}"
+
+before = ObjectSpace.live_objects
+bm_block { 10 }
+after  = ObjectSpace.live_objects
+puts "Block: #{after - before}"
+
+puts "=== Exceptions"
+def bm_exception
+rescue
+ensure
+end
+before = ObjectSpace.live_objects
+begin
+rescue Exception
+ensure
+end
+after  = ObjectSpace.live_objects
+puts "begin-rescue-ensure: #{after - before}"
+
+before = ObjectSpace.live_objects
+bm_exception
+after  = ObjectSpace.live_objects
+puts "begin-rescue-ensure method: #{after - before}"
+
+
