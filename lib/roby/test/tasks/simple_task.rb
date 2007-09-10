4317ffa5f210a7efe3887fcf80ce4d77a61b6ef8
Sylvain Joyeux <sylvain.joyeux@laas.fr>
2007-09-10 17:38:37 +0200

    [test] make 'id' an argument of SimpleTask, and provide one value by default
      This is to allow the use of queries to get a given SimpleTask in tests

--- lib/roby/test/tasks/simple_task.rb
+++ lib/roby/test/tasks/simple_task.rb
@@ -3,6 +3,13 @@ require 'roby/task'
 module Roby
     module Test
 	class SimpleTask < Roby::Task
+	    argument :id
+
+	    def initialize(arguments)
+		arguments = { :id => object_id.to_s }.merge(arguments)
+		super(arguments)
+	    end
+
 	    event :start, :command => true
 	    event :success, :command => true, :terminal => true
 	    terminates
