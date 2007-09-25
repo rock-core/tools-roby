558ded2d9c9d012813081e725552a85dbf880394
Sylvain Joyeux <sylvain.joyeux@laas.fr>
2007-09-25 15:01:36 +0200

    [test] add missing test suites in suite_relations

--- test/suite_relations.rb
+++ test/suite_relations.rb
@@ -1,6 +1,8 @@
 require 'test_relations'
 require 'relations/test_hierarchy'
 require 'relations/test_executed_by'
+require 'relations/test_planned_by'
+require 'relations/test_conflicts'
 
 require 'relations/test_ensured'
 
