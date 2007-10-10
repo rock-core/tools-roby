fd3f4165c09429b5c9c735768ee5ee03f0cda461
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2007-10-10 10:41:32 +0200

    [app] adapt scripts/bookmarks to the new dumping of time
      Moreover, display start/end point of bookmarked ranges whenever
      we detect them

--- lib/roby/app/scripts/generate/bookmarks.rb
+++ lib/roby/app/scripts/generate/bookmarks.rb
@@ -26,15 +26,15 @@ class Roby::Log::EventMatcher
 
     def filter(log)
 	while log.has_sample?
-	    log.read_and_decode.each_slice(2) do |m, args|
-		event(m, args)
+	    log.read_and_decode.each_slice(4) do |m, sec, usec, args|
+		event(m, sec, usec, args)
 	    end
 	end
     end
 
-    def event(m, args)
+    def event(m, sec, usec, args)
 	if m == :discovered_tasks || m == :discovered_events
-	    objects = args[2]
+	    objects = args[1]
 	    for object in objects
 		for remote_id in object.remote_siblings.values
 		    remote_ids[remote_id] = object
@@ -42,14 +42,16 @@ class Roby::Log::EventMatcher
 	    end
 
 	elsif m == :finalized_task || m == :finalized_event
-	    object = args[2]
+	    object = args[1]
 	    remote_ids.delete(object)
 	end
 
+	time = nil
 	if predicate_set = notifications[m]
 	    for p, callback in predicate_set
 		if p.call(args)
-		    callback.call(args)
+		    time ||= Time.at(sec, usec)
+		    callback.call(time, args)
 		end
 	    end
 	end
@@ -62,7 +64,7 @@ class Roby::Log::EventMatcher
 	    task_model = Regexp.new($2)
 	    symbol = $1.to_sym
 	    predicate = lambda do |args|
-		generator = local_object(args[1])
+		generator = local_object(args[0])
 		if generator.respond_to?(:task) && generator.symbol == symbol
 		    task = local_object(generator.task)
 		    Regexp.new(task_model) === local_object(task).model.ancestors[0][0]
@@ -122,12 +124,14 @@ bookmarks = Hash.new
 filter = Roby::Log::EventMatcher.new
 config.each do |name, (start_point, end_point)|
     bookmarks[name] = []
-    filter.parse(start_point) do |args|
-	bookmarks[name] << [args[0]]
+    filter.parse(start_point) do |time, args|
+	STDERR.puts "#{time.to_hms} starting point for #{name}"
+	bookmarks[name] << [time]
     end
-    filter.parse(end_point) do |args|
+    filter.parse(end_point) do |time, args|
 	if bookmarks[name].last && bookmarks[name].last[0] && !bookmarks[name].last[1]
-	    bookmarks[name].last[1] = args[0]
+	    STDERR.puts "#{time.to_hms} end point for #{name}"
+	    bookmarks[name].last[1] = time
 	end
     end
 end
