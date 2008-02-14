ed98174a0e03a6e74f113339ffc9c3195a2698b1
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-02-14 11:29:23 +0100

    [ext] better error message when external libraries are not found

--- ext/graph/extconf.rb
+++ ext/graph/extconf.rb
@@ -2,7 +2,11 @@ require 'mkmf'
 CONFIG['CC'] = "g++"
 dir_config 'boost'
 $LDFLAGS += "-module"
+
 _, ldflags, _ = pkg_config 'utilmm'
+unless ldflags
+    raise 'Util-- not found. It is required to install Roby'
+end
 $LDFLAGS << ldflags.gsub('-L', ' -Wl,-rpath -Wl,')
 create_makefile("bgl")
 
