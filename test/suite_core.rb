e16a9b3004f4c2dac2aef3bf82363e8eb93b387e
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-04-15 10:39:15 +0200

    [test] test_planning is no more, hail to suite_planning

--- test/suite_core.rb
+++ test/suite_core.rb
@@ -12,10 +12,10 @@ require 'test_exceptions'
 
 require 'test_plan'
 require 'test_query'
-require 'test_planning'
 require 'test_transactions'
 require 'test_transactions_proxy'
 
+require 'suite_planning'
 require 'suite_relations'
 
 require 'test_control'
