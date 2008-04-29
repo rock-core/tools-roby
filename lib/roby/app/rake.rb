3d577f5da03a67058b1cb40555a5340bdfed638e
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-04-29 09:37:56 +0200

    [doc] use jamis instead of allison for default
      The allison template is prettier, but the JS code is slow
      when a lot of names are involved (which is the case in Roby).

--- lib/roby/app/rake.rb
+++ lib/roby/app/rake.rb
@@ -10,35 +10,26 @@ module Roby
         # Returns the rdoc template path the documentation
         # generation should be using in Rakefile
         #
-        # The default template is allison, using the gem-provided allison if
-        # present and falling back to the one in-source. If the RDOC_TEMPLATE
-        # environment variable is defined, use that template instead, with the
-        # special value of 'jamis' using the in-source jamis template.
+        # Two non-standard templates are provided:
+        # allison[http://blog.evanweaver.com/files/doc/fauna/allison/files/README.html]
+        # and jamis[http://weblog.jamisbuck.org/2005/4/8/rdoc-template]. The
+        # default is jamis. Allison is nicer, but the javascript-based indexes
+        # are slow given the count of classes and methods there is in Roby.
         def self.rdoc_template
-            result = nil
             if ENV['RDOC_TEMPLATE']
-                result = if ENV['RDOC_TEMPLATE'] == 'jamis'
-                             Roby::Rake.info "using in-source jamis template"
-                             File.expand_path('doc/styles/jamis')
-                         elsif ENV['RDOC_TEMPLATE'] == 'allison'
-                             Roby::Rake.info "using in-source allison template"
-                             File.expand_path('doc/styles/allison')
-                         else
-                             Roby::Rake.info "using the #{ENV['RDOC_TEMPLATE']} template"
-                             ENV['RDOC_TEMPLATE']
-                         end
+                if ENV['RDOC_TEMPLATE'] == 'jamis'
+                    Roby::Rake.info "using in-source jamis template"
+                    File.expand_path('doc/styles/jamis')
+                elsif ENV['RDOC_TEMPLATE'] == 'allison'
+                    Roby::Rake.info "using in-source allison template"
+                    File.expand_path('doc/styles/allison')
+                else
+                    Roby::Rake.info "using the #{ENV['RDOC_TEMPLATE']} template"
+                    ENV['RDOC_TEMPLATE']
+                end
             else
-                allison_path = `allison --path`.chomp.chomp
-                result = if allison_path.empty?
-                             Roby::Rake.info "using in-source allison template"
-                             File.expand_path('doc/styles/allison')
-                         else
-                             Roby::Rake.info "using the allison gem at #{allison_path}"
-                             allison_path
-                         end
+                File.expand_path('doc/styles/jamis')
             end
-
-            result
         end
 
         # Invoke the given target in all plugins found in plugins/ that define it
