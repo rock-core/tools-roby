rock-core/tools-roby

commit 926ab1e83cf86a395e468448be19a534fc72a0f6

[core] fix memory leak in Event
  Until now, Event#source was keeping around the sources of propagation.
  Since the sources themselves were keeping their sources -- and since
  Event keeps references on generator and tasks -- it was potentially a
  source of memory leak for long-running high-level tasks.

  This patch references the event sources by using WeakRef. That should
  not be a problem since #sources is not used often (mainly in error
  handling)

lib/roby/event.rb
@@ -12,8 +12,23 @@ module Roby
 	end
 
 	attr_accessor :propagation_id, :context, :time
-	attr_accessor :sources
 	protected :propagation_id=, :context=, :time=
+
+        def sources
+            result = []
+            sources.delete_if do |ref|
+                obj = begin 
+                          ref.__getobj__
+                      rescue WeakRef::RefError
+                      end
+                if obj
+                    result << obj
+                end
+            end
+        end
+        def sources=(sources)
+            @sources = sources.map { |obj| WeakRef.new(obj) }
+        end
 
 	# To be used in the event generators ::new methods, when we need to reemit
 	# an event while changing its 

lib/roby.rb
@@ -16,6 +16,7 @@ module Roby
 end
 
 require 'drb'
+require 'weakref'
 require 'pp'
 require 'thread'
 require 'set'

commit 7fd1a22aa9a1ed91d687d9c505076d5a34942760

[core] fix Event#sources
  Test suites are there for something ... I suck. This problem has been
  introduced in 926ab1e83cf86a395e468448be19a534fc72a0f6.

lib/roby/event.rb
@@ -21,15 +21,20 @@ module Roby
 
         def sources
             result = []
-            sources.delete_if do |ref|
+            @sources.delete_if do |ref|
                 obj = begin 
                           ref.__getobj__
                       rescue WeakRef::RefError
                       end
+
                 if obj
                     result << obj
+                    false
+                else
+                    true
                 end
             end
+            result
         end
         def sources=(sources)
             @sources = sources.map { |obj| WeakRef.new(obj) }

lib/roby/task.rb
@@ -56,14 +56,13 @@ module Roby
 	# event
 	def task_sources
 	    result = ValueSet.new
-	    if sources
-		for ev in sources
-		    gen = ev.generator
-		    if gen.respond_to?(:task) && gen.task == task
-			result.merge ev.task_sources
-		    end
-		end
-	    end
+	    event_sources = sources
+            for ev in event_sources
+                gen = ev.generator
+                if gen.respond_to?(:task) && gen.task == task
+                    result.merge ev.task_sources
+                end
+            end
 	    if result.empty?
 		result << self
 	    end