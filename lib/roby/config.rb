df22f55a88fd2ae0fb1bfe2e30adcb35826acde8
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-06-14 18:41:21 +0200

    [dist] refuse loading on 1.9

--- lib/roby/config.rb
+++ lib/roby/config.rb
@@ -1,3 +1,12 @@
+if RUBY_VERSION >= "1.9"
+    STDERR.puts <<-EOB
+The stable version of Roby is not compatible with 1.9. If you really want to
+try it on 1.9, checkout the 1.9 branch of the development repository on github
+(see README.txt for URLs)
+    EOB
+    exit(1)
+end
+
 module Roby
     VERSION = '0.7.2'
     ROBY_LIB_DIR  = File.expand_path( File.join(File.dirname(__FILE__), '..') )
