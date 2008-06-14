6412d4d78d020da62de47350cc2d6ee2a154f59b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-06-14 17:12:38 +0200

    [core] prepare release of 0.7.2

--- lib/roby/config.rb
+++ lib/roby/config.rb
@@ -1,5 +1,5 @@
 module Roby
-    VERSION = '0.7.1'
+    VERSION = '0.7.2'
     ROBY_LIB_DIR  = File.expand_path( File.join(File.dirname(__FILE__), '..') )
     ROBY_ROOT_DIR = File.expand_path( File.join(ROBY_LIB_DIR, '..') )
 end
