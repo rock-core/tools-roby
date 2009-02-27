ec14a9c75a84cea58efb456048088d6f0cb14b16
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-02-27 12:02:31 +0100

    re-enable backtrace filtering by default
    The backtrace filter had been disabled directly in scripts/run for
    debugging purposes, but that has slipped. This commit re-enables it by
    default, as it should be.

--- lib/roby/app/scripts/run.rb
+++ lib/roby/app/scripts/run.rb
@@ -1,6 +1,5 @@
 require File.join(File.dirname(__FILE__), '..', 'run')
 app = Roby.app
-Roby.app.filter_backtraces = false
 
 robot_name = ARGV.shift
 app.robot robot_name, (ARGV.shift || robot_name)
