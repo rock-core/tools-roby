rock-core/tools-roby

commit 050aa07e5a9ced9127d9a0f46730a5f7e7bf4cce

workaround a problem in exception handling in poll blocks
The problem is the following. Let's assume a user wants to both *emit* and
*react* to an event in the poll block. And he does it that way:

  poll do
    if success_condition
      emit :success
      ... react to success event ...
    end
  end

What would happen if the reaction has errors ? The generic poll implementation
will emit failed and we have a task that both failed and succeeded at the same
time.

The model would actually dictates that the reaction is done in an event handler.
But user's don't always follow the model.

So right now, I worked around a minimal side-effect of this problem, which is
MissionFailedError assuming that #terminal_event is the final event. Instead, we
actually use the history of +failed+.

lib/roby/standard_errors.rb
@@ -201,7 +201,7 @@ module Roby
     class MissionFailedError < LocalizedError
         # Create a new MissionFailedError for the given mission
 	def initialize(task)
-	    super(task.terminal_event)
+	    super(task.event(:failed).history.last)
 	end
 
         def pretty_print(pp)

commit 839c9d3da34b64ffc1bab1b104227df8b1b93417

core: fix event generalization bug introduced by 050aa07e5a9ced9127d9a0f46730a5f7e7bf4cce
The fix in 050aa07e5a9ced9127d9a0f46730a5f7e7bf4cce actually broke
the event generalization principle, which says that the exceptions
failure point should be the most specialized event that made the
task failed.

This commit introduces a Roby::Task#failure_event attribute that holds
that "most specialized event". It

lib/roby/relations/planned_by.rb
@@ -41,7 +41,7 @@ module Roby
 
 	def initialize(planned_task, planning_task)
 	    @planned_task = planned_task
-	    super(planning_task.terminal_event)
+	    super(planning_task.failure_event)
 	end
 
 	def message # :nodoc:

lib/roby/standard_errors.rb
@@ -201,7 +201,7 @@ module Roby
     class MissionFailedError < LocalizedError
         # Create a new MissionFailedError for the given mission
 	def initialize(task)
-	    super(task.event(:failed).history.last)
+	    super(task.failure_event)
 	end
 
         def pretty_print(pp)

lib/roby/task.rb
@@ -923,6 +923,13 @@ module Roby
 
 	# The event which has finished the task (if there is one)
 	attr_reader :terminal_event
+
+        # The event that caused this task to fail. This is equivalent to taking
+        # the first emitted element of
+        #   task.event(:failed).last.task_sources
+        #
+        # It is only much more efficient
+        attr_reader :failure_event
 	
 	# Call to update the task status because of +event+
 	def update_task_status(event) # :nodoc:
@@ -936,6 +943,7 @@ module Roby
 		self.success = false
 		self.finished = true
 		@terminal_event ||= event
+                @failure_event  ||= event
 	    elsif event.terminal? && !finished?
 		plan.task_index.set_state(self, :finished?)
 		self.finished = true

test/test_task.rb
@@ -974,7 +974,7 @@ class TC_Task < Test::Unit::TestCase
 	ev = EventGenerator.new(true)
 	ev.forward task.event(:specialized_failure)
 	ev.call
-	assert_equal([task.event(:specialized_failure).last], task.event(:failed).last.task_sources.to_a)
+	assert_equal([task.event(:specialized_failure).last], task.event(:stop).last.task_sources.to_a)
     end
 
     def test_virtual_task