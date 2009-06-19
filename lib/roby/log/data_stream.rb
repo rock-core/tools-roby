rock-core/tools-roby

commit 0a095de24704a69ed06cdb2371e6b9bf3e544a38

gui: allow proper handling of "empty cycles"
This commit changes the data flow/display interface so that the
GUI can properly ignore cycles that don't contain data useful for
either the decoder or the display.

The replay application is now able to detect cycles that don't
give data to the plan builder, and therefore ignore them.

A new "Play next nonempty cycle" button has been added to expose
this very useful functionality, thus allowing to drop periods where
nothing was happening.

lib/roby/log/data_stream.rb
@@ -100,12 +100,18 @@ module Roby::Log
 	    super if defined? super
 	end
 
-	# Do a read - decode - feed cycle
-	def advance
+	# Do a read and decode the data
+        #
+        # It returns false if no decoders have found interesting updates in the
+        # decoded data, and true otherwise. The method relies on the decoder's
+        # #process method to return true/false when required.
+        #
+        # See DataDecoder#process
+        def advance
 	    data = decode(read)
-	    decoders.each do |dec|
+	    !decoders.find_all do |dec|
 		dec.process(data)
-	    end
+	    end.empty?
 	end
 
 	def init(data)
@@ -171,11 +177,15 @@ module Roby::Log
 	    end
 	end
 
-	# Update the display to the current state of the decoder
+	# Updates the displays that are associated with this decoder. Returns
+        # true if one of the displays have been changed, and false otherwise.
+        #
+        # It relies on the display's #update method to return true if something
+        # has changed on the display and false otherwise.
 	def display
-	    displays.each do |display| 
+	    !displays.find_all do |display| 
 		display.update
-	    end
+	    end.empty?
 	end
     end
 

lib/roby/log/gui/replay.rb
@@ -50,6 +50,8 @@ class Replay < Qt::MainWindow
     attr_accessor :initial_time
     # A set of procs which are to be called to set up the display
     attr_accessor :initial_setup
+    # Set to true if we are playing some data right now
+    attr_predicate :running?, true
 
     def initialize
 	super()
@@ -180,6 +182,7 @@ class Replay < Qt::MainWindow
     end
 
     def stop
+        self.running = false
 	if play_timer
 	    ui_controls.play.checked = false
 	    play_timer.stop
@@ -197,8 +200,14 @@ class Replay < Qt::MainWindow
 	play_until(time + time_slice * play_speed)
     end
     slots 'play_step_timer()'
+
+    def play_next_nonempty
+        self.running = true
+        while running? && !play_step
+        end
+    end
     
-    def play_until(max_time, integrate = true)
+    def play_until(max_time, integrate = true, skip_empty = false)
 	start_at = Time.now
 	displayed_streams.inject(timeline = []) do |timeline, s| 
 	    if s.next_time
@@ -218,9 +227,11 @@ class Replay < Qt::MainWindow
 	while !timeline.empty? && (timeline[0][0] - max_time) < 0.001
 	    @time, stream = timeline.first
 
-	    stream.advance
+	    if stream.advance
+                updated_streams << stream
+            end
+
 	    stream.clear_integrated unless integrate
-	    updated_streams << stream
 	    if next_time = stream.next_time
 		timeline[0] = [next_time, stream]
 	    else
@@ -250,6 +261,7 @@ class Replay < Qt::MainWindow
 	    end
 	end
 	update_time_display
+        !updated_streams.empty?
 
     rescue Exception => e
 	message = "<html>#{Qt.escape(e.message)}<ul><li>#{e.backtrace.join("</li><li>")}</li></ul></html>"

lib/roby/log/plan_rebuilder.rb
@@ -239,18 +239,30 @@ module Roby
 		clear
 	    end
 	    
+            # Processes one cycle worth of data coming from an EventStream, and
+            # builds the corresponding plan representation
+            #
+            # It returns true if there was something noteworthy in there, and
+            # false otherwise.
 	    def process(data)
 		@time = data.last[0][:start]
 	        @start_time ||= @time
 
+                done_something = false
 		data.each_slice(4) do |m, sec, usec, args|
 		    time = Time.at(sec, usec)
 		    reason = catch :ignored do
 			begin
 			    if respond_to?(m)
 				send(m, time, *args)
+                                done_someting = true
 			    end
-			    displays.each { |d| d.send(m, time, *args) if d.respond_to?(m) }
+			    displays.each do |d|
+                                if d.respond_to?(m)
+                                    done_something = true
+                                    d.send(m, time, *args) 
+                                end
+                            end
 			rescue Exception => e
 			    display_args = args.map do |obj|
 				case obj
@@ -269,6 +281,7 @@ module Roby
 			Roby.warn "Ignored #{m}(#{args.join(", ")}): #{reason}"
 		    end
 		end
+                done_something
 	    end
 
 	    def local_object(object)

lib/roby/log/relations.rb
@@ -939,6 +939,7 @@ module Roby
 		    execution_events.partition { |fired, ev| fired }
 
 		postponed_events.clear
+                true
 	    end
 
 	    def remove_graphics(item, scene = nil)

lib/roby/log/gui/replay_controls.rb
@@ -161,6 +161,9 @@ class Ui::ReplayControls
 	play_step.connect(SIGNAL("clicked()")) do
 	    replay.play_step
 	end
+	play_next_nonempty.connect(SIGNAL("clicked()")) do
+	    replay.play_next_nonempty
+	end
     end
 
     def handleGoto

lib/roby/log/gui/replay_controls.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>432</width>
-    <height>168</height>
+    <width>442</width>
+    <height>167</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -19,9 +19,9 @@
      <property name="title">
       <string>Play</string>
      </property>
-     <layout class="QVBoxLayout">
+     <layout class="QVBoxLayout" name="verticalLayout">
       <item>
-       <layout class="QHBoxLayout">
+       <layout class="QHBoxLayout" name="horizontalLayout">
         <item>
          <widget class="QProgressBar" name="progress">
           <property name="value">
@@ -147,7 +147,7 @@ p, li { white-space: pre-wrap; }
        </layout>
       </item>
       <item>
-       <layout class="QHBoxLayout">
+       <layout class="QHBoxLayout" name="horizontalLayout_2">
         <item>
          <widget class="QToolButton" name="bookmarks">
           <property name="text">
@@ -171,44 +171,66 @@ p, li { white-space: pre-wrap; }
           </property>
           <property name="sizeHint" stdset="0">
            <size>
-            <width>40</width>
+            <width>101</width>
             <height>20</height>
            </size>
           </property>
          </spacer>
         </item>
         <item>
-         <widget class="QPushButton" name="seek_start">
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+         <layout class="QGridLayout" name="gridLayout">
+          <item row="0" column="0">
+           <widget class="QPushButton" name="seek_start">
+            <property name="toolTip">
+             <string>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;&quot;&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Go to start&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-          <property name="text">
-           <string>Go To Beginning</string>
-          </property>
-          <property name="shortcut">
-           <string/>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QPushButton" name="play_step">
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+            </property>
+            <property name="text">
+             <string>Go To Beginning</string>
+            </property>
+            <property name="shortcut">
+             <string/>
+            </property>
+           </widget>
+          </item>
+          <item row="0" column="1">
+           <widget class="QPushButton" name="play_step">
+            <property name="toolTip">
+             <string>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;&quot;&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Go forward one step&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-          <property name="text">
-           <string>Step</string>
-          </property>
-         </widget>
+            </property>
+            <property name="text">
+             <string>Step</string>
+            </property>
+           </widget>
+          </item>
+          <item row="1" column="0" colspan="2">
+           <widget class="QPushButton" name="play_next_nonempty">
+            <property name="toolTip">
+             <string>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Go to start&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+            </property>
+            <property name="text">
+             <string>Play next non-empty cycle</string>
+            </property>
+            <property name="shortcut">
+             <string/>
+            </property>
+           </widget>
+          </item>
+         </layout>
         </item>
        </layout>
       </item>
      </layout>
+     <zorder>play_next_nonempty</zorder>
+     <zorder>faster</zorder>
     </widget>
    </item>
   </layout>

commit 27e0130572678975842420767ba0c34eb9150a97

gui: fix and use the #clear_integrated method
That method had been added before to better manage the cleanup of
old data in the plan displays. The thing is: it was disabled.

Resurrect it, which brings some simplifications and probably some
display bugfixes. Moreover, it fixes a bug introduced with
0a095de24704a69ed06cdb2371e6b9bf3e544a38. When an empty cycle was
encountered, the "integrated data" was not cleared. Thus, we did
not remove the signal propagation display after a "step" for
instance, which was confusing.

lib/roby/log/data_stream.rb
@@ -121,10 +121,17 @@ module Roby::Log
 	    self.class.decode(data)
 	end
 
+        # Displays may have the ability to not display a per-cycle information,
+        # but to add information as it is coming over. This call asks them to
+        # remove the information integrated until now.
+        #
+        # It forwards the call to the decoders' #clear_integrated call. These
+        # methods should return true if a change has been needed on any of their
+        # display, and false otherwise.
 	def clear_integrated
-	    decoders.each do |decoder|
+	    !decoders.find_all do |decoder|
 		decoder.clear_integrated
-	    end
+	    end.empty?
 	end
 
 	# Update the displays
@@ -171,10 +178,19 @@ module Roby::Log
 	    displays.each { |d| d.clear }
 	end
 
+        # Displays may have the ability to not display a per-cycle information,
+        # but to add information as it is coming over. This call asks them to
+        # remove the information integrated until now.
+        #
+        # It forwards the call to the displays' #clear_integrated call(if any). These
+        # methods should return true if a change has been needed on any of their
+        # display, and false otherwise.
 	def clear_integrated
-	    displays.each do |display|
-		display.clear_integrated if display.respond_to?(:clear_integrated)
-	    end
+	    !displays.find_all do |display|
+                if display.respond_to?(:clear_integrated)
+                    display.clear_integrated 
+                end
+	    end.empty?
 	end
 
 	# Updates the displays that are associated with this decoder. Returns

lib/roby/log/gui/replay.rb
@@ -211,7 +211,7 @@ class Replay < Qt::MainWindow
         end
     end
     
-    def play_until(max_time, integrate = true, skip_empty = false)
+    def play_until(max_time, integrate = false)
 	start_at = Time.now
 	displayed_streams.inject(timeline = []) do |timeline, s| 
 	    if s.next_time
@@ -225,17 +225,24 @@ class Replay < Qt::MainWindow
 	    return
 	end
 
+        needs_display = Set.new
 	updated_streams = Set.new
 
 	timeline.sort_by { |t, _| t }
 	while !timeline.empty? && (timeline[0][0] - max_time) < 0.001
 	    @time, stream = timeline.first
 
+            unless integrate
+                if stream.clear_integrated 
+                    needs_display << stream
+                end
+            end
+
 	    if stream.advance
+                needs_display << stream
                 updated_streams << stream
             end
 
-	    stream.clear_integrated unless integrate
 	    if next_time = stream.next_time
 		timeline[0] = [next_time, stream]
 	    else
@@ -246,7 +253,7 @@ class Replay < Qt::MainWindow
 
 	replayed = Time.now
 
-	updated_streams.each do |stream|
+	needs_display.each do |stream|
 	    stream.display
 	end
 

lib/roby/log/plan_rebuilder.rb
@@ -308,29 +308,14 @@ module Roby
 	    end
 
 	    def clear_integrated
-		last_finalized.clear
+                updated = false
 		plans.each do |plan|
+                    updated = !(plan.finalized_events.empty? && plan.finalized_tasks.empty?)
 		    plan.clear_finalized(plan.finalized_tasks.dup, plan.finalized_events.dup)
 		end
 
-		super
-	    end
-
-	    def display
-		plans.each do |plan|
-		    if finalized = last_finalized[plan]
-			plan.clear_finalized(*finalized)
-		    end
-		end
-
-		super
-		
-		# Save a per-plan set of finalized tasks, to be removed the
-		# next time #display is called
-		@last_finalized = Hash.new
-		plans.each do |plan|
-		    last_finalized[plan] = [plan.finalized_tasks.dup, plan.finalized_events.dup]
-		end
+		super_result = super
+                super_result || updated
 	    end
 
 	    def local_plan(plan, allow_new = false)

lib/roby/log/relations.rb
@@ -773,9 +773,12 @@ module Roby
 	    end
 
 	    def clear_integrated
-		postponed_events.clear
-		execution_events.clear
-		@execution_events = execution_events.find_all { |fired, ev| !fired }
+                unless keep_signals
+                    last_propagated_events, @propagated_events = propagated_events, Array.new
+                    last_execution_events, @execution_events = 
+                        execution_events.partition { |fired, ev| fired }
+                end
+                !(last_propagated_events.empty? && last_execution_events.empty?)
 	    end
 
             # Update the display with new data that has come from the data
@@ -785,11 +788,6 @@ module Roby
             # updated, so the method always returns true
 	    def update
 		return unless decoder
-
-		if keep_signals
-		    @execution_events = @last_execution_events.concat(execution_events)
-		    @propagated_events.concat @last_propagated_events
-		end
 
 		update_prefixes_removal
 		clear_flashing_objects
@@ -934,11 +932,6 @@ module Roby
 		    end
 		end
 
-		@last_propagated_events, @propagated_events = propagated_events, Array.new
-		@last_execution_events, @execution_events = 
-		    execution_events.partition { |fired, ev| fired }
-
-		postponed_events.clear
                 true
 	    end
 