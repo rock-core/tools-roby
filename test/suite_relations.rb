f3e6ca17ba4053841c438ac8004315e3aaa072ca
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-12-15 19:49:13 +0100

    test: fix suite_core.rb
    the relations/test_hierarchy.rb has been renamed

--- test/suite_relations.rb
+++ test/suite_relations.rb
@@ -1,9 +1,8 @@
 $LOAD_PATH.unshift File.expand_path(File.join('..', 'lib'), File.dirname(__FILE__))
 require 'test_relations'
-require 'relations/test_hierarchy'
+require 'relations/test_dependency'
 require 'relations/test_executed_by'
 require 'relations/test_planned_by'
 require 'relations/test_conflicts'
-
 require 'relations/test_ensured'
 
