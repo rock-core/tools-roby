3f8a30352f2dd1e340ede4110cbcd90378fee66b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-01-24 15:23:12 +0100

    fault_injection: fixed the tests

--- plugins/fault_injection/test/test_fault_injection.rb
+++ plugins/fault_injection/test/test_fault_injection.rb
@@ -7,40 +7,35 @@ require 'fault_injection'
 
 class TC_FaultInjection < Test::Unit::TestCase
     include Roby::Test
+    include Roby::Test::Assertions
 
     def test_inject_fault
-	Roby.control.run :detach => true
+        model = Class.new(Roby::Test::SimpleTask) do
+            event :specialized_fault
+            forward :specialized_fault => :failed
+        end
+        plan.add_permanent(task = model.new)
 
-	task = nil
-	Roby.execute do
-	    model = Class.new(Roby::Test::SimpleTask) do
-		event :specialized_fault
-		forward :specialized_fault => :failed
-	    end
+        engine.run
 
-	    plan.permanent(task = model.new)
+        assert_any_event(task.start_event) do
 	    assert_raises(ArgumentError) { task.inject_fault(:start) }
-
 	    task.start!
 	end
-	Roby.wait_one_cycle
 
 	fake_task = nil
-	Roby.execute do
+	assert_any_event(task.stop_event) do
 	    assert_raises(ArgumentError) { task.inject_fault(:updated_data) }
 	    assert_nothing_raised { task.inject_fault(:specialized_fault) }
 	    assert_equal(2, plan.known_tasks.size)
 	    fake_task = plan.known_tasks.find { |t| t != task }
 	end
-	Roby.wait_one_cycle
 
 	assert(fake_task.finished?)
 	assert(fake_task.event(:specialized_fault).happened?)
     end
 
     def test_apply
-	Roby.control.run :detach => true
-
 	model = Class.new(Roby::Test::SimpleTask) do
 	    event :specialized_fault
 	    forward :specialized_fault => :failed
@@ -49,24 +44,23 @@ class TC_FaultInjection < Test::Unit::TestCase
 	fault_models = Hash.new { |h, k| h[k] = Hash.new }
 	fault_models[model][:specialized_fault] = FaultInjection::Rate.new(0.01, 1.0)
 	fault_models[Roby::Test::SimpleTask][:stop] = FaultInjection::Rate.new(1_000_000, 1.0)
+        plan.add_permanent(simple = Roby::Test::SimpleTask.new)
+        plan.add_permanent(specialized = model.new)
+        both_started = simple.start_event & specialized.start_event
 	
-	simple, specialized = nil
-	Roby.execute do
-	    plan.permanent(simple = Roby::Test::SimpleTask.new)
-	    plan.permanent(specialized = model.new)
+        engine.run
+
+        assert_any_event(both_started) do
 	    simple.start!
 	    specialized.start!
 	end
-	Roby.wait_one_cycle
 
-	sleep(0.5)
-	fake_specialized = nil
-	Roby.execute do
-	    result = Roby::FaultInjection.apply(fault_models)
+        fake_specialized = engine.execute do
+	    result = Roby::FaultInjection.apply(fault_models, plan)
 	    assert_equal(1, result.size)
-	    fake_specialized = result.first.last
+	    result.first.last
 	end
-	Roby.wait_one_cycle
+	engine.wait_one_cycle
 
 	assert(simple.running?)
 	assert(specialized.finished?)
