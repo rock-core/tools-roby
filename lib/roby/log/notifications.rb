f3a3dde26bfe4f90dfd2f9e681ec568da03b96a4
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2010-04-22 16:24:03 +0200

    log: fix the notification view
    Use ObjectIDManager to manage the mapping from IDs to objects, and
    use QtWebKit for HTML display.

--- lib/roby/log/notifications.rb
+++ lib/roby/log/notifications.rb
@@ -1,4 +1,5 @@
 require 'roby/log/data_stream'
+require 'qtwebkit'
 
 class Roby::Task::DRoby
     attr_accessor :mission
@@ -9,60 +10,81 @@ module Roby
 	class Notifications < DataDecoder
 	    GENERATOR_CALL_LIMIT = 0.1
 
+            attr_reader :manager
 	    attr_reader :tasks
+            attr_reader :transactions
 
 	    attr_reader :histories
 	    def initialize(name)
-		@tasks     = Hash.new
+                @manager = ObjectIDManager.new
+                @transactions = ValueSet.new
 		@histories = Hash.new { |h, k| h[k] = Array.new }
 		super(name)
 	    end
 
+            def added_transaction(trsc)
+                transactions << manager.local_object(trsc)
+            end
+            def removed_transaction(trsc)
+                local_obj = manager.remove(trsc)
+                transactions.delete(local_obj)
+            end
+
 	    def added_task(task)
-		task.remote_siblings.each_value do |id|
-		    tasks[id] = task
-		end
+                manager.local_object(task)
 	    end
 	    def removed_task(remote_id)
-		task = tasks.delete(remote_id)
-		task.remote_siblings.each_value do |id|
-		    tasks.delete(id)
-		end
+                manager.remove(remote_id)
 	    end
 
 	    def clear
 		super
 
-		@tasks.clear
 		@histories.clear
 	    end
 
 
 	    def process(data)
 		data.each_slice(4) do |m, sec, usec, args|
+                    m = m.to_s
 		    time = Time.at(sec, usec)
 		    case m.to_s
+                    when /added_transaction/
+                        plan, trsc = args[0], args[1]
+                        added_transaction(trsc)
+
 		    when /added_mission/
-			task = tasks[args[1]]
+                        plan, task = args[0], args[1]
+                        plan = manager.local_object(plan)
+                        next if transactions.include?(plan)
+
+			task = manager.local_object(task)
 			task.mission = true
-			event :added_mission, args[0], task
+			event :added_mission, time, plan, task
 
 		    when /unmarked_mission/
-			task = tasks[args[1]]
+                        plan, task = args[0], args[1]
+                        plan = manager.local_object(plan)
+                        next if transactions.include?(plan)
+
+			task = manager.local_object(args[1])
 			task.mission = false
-			event :unmarked_mission, args[0], task
+			event :unmarked_mission, time, plan, task
 
 		    when /added_tasks/
 			args[1].each { |t| added_task(t) }
 
 		    when /finalized_task/
-			id = args[1]
-			task = tasks[id]
-			if histories[id].empty?
-			    event :finalized_pending, time, task
+                        plan, task = args[0], args[1]
+                        plan = manager.local_object(plan)
+                        next if transactions.include?(plan)
+
+			task = manager.local_object(task)
+			if histories[task].empty?
+			    event :finalized_pending, time, plan, task
 			end
 			histories.delete(task)
-			removed_task(args[1])
+			removed_task(task)
 
 		    when /generator_calling/
 			@current_call = [time, args[0]]
@@ -77,14 +99,18 @@ module Roby
 			
 		    when /exception/
 			error, involved_tasks = *args
-			involved_tasks = involved_tasks.map { |id| tasks[id] }
+			involved_tasks = involved_tasks.map do |id|
+                            manager.local_object(id)
+                        end
 			event m, time, error, involved_tasks
+
 		    when /generator_fired/
 			generator = args[0]
 			if generator.respond_to?(:task)
-			    histories[generator.task] << args
+                            task = manager.local_object(generator.task)
+			    histories[task] << args
 			    if generator.symbol == :failed
-				event :failed_task, time, tasks[generator.task], histories[generator.task]
+				event :failed_task, time, task, histories[task]
 			    end
 			end
 		    end
@@ -100,7 +126,7 @@ module Roby
 	    end
 	end
 
-	class NotificationsDisplay < Qt::TextBrowser
+	class NotificationsDisplay < Qt::WebView
 	    include DataDisplay
 	    decoder Notifications
 
@@ -108,9 +134,8 @@ module Roby
 	    attr_reader :text
 
 	    STYLESHEET = <<-EOS
-		    h1 { font-size: large; }
+		    h1 { font-size: 100%; }
 		    h1 { margin-bottom: 3px; }
-		    h2 { font-size: medium; }
 		    .time { 
 			margin-right: 10px; 
 		    }
@@ -140,37 +165,36 @@ module Roby
 
 		resize(500, 600)
 		@main     = self
-		@document = Qt::TextDocument.new
-
-		self.document = document
-		document.setDefaultStyleSheet(STYLESHEET)
+                @text = ""
 	    end
 
 
 	    def render_event(kind, time, title)
-		@text = ""
-		text << "\n<div class=#{kind}>\n  <h1><span class=\"time\">#{time.to_hms}</span> #{title}</h1>\n  "
+		text << "\n<div class=\"#{kind}\">\n  <h1><span class=\"time\">#{time.to_hms}</span> #{title}</h1>\n"
 		yield
 
 	    ensure
 		text << "\n</div>"
-		insertHtml(text)
-		verticalScrollBar.value = verticalScrollBar.maximum
+
+                html = "<html><style>#{STYLESHEET}</style><body>#{text}</body></html>"
+		setHtml(html)
+                main_frame = page.main_frame
+                max = main_frame.scroll_bar_maximum(Qt::Vertical)
+                main_frame.set_scroll_bar_value(Qt::Vertical, max)
 	    end
 
 	    def render_task(task)
 		remote_siblings = "{ " << task.remote_siblings.map { |peer, id| id.to_s(peer) }.join(", ") << " }"
-		text << "<div class=\"task\">
-		    #{task.model.ancestors.first.first}#{remote_siblings}\n  "
+		text << "  <div class=\"task\">#{task.model.ancestors.first.first}#{remote_siblings}\n"
 		
 		unless task.arguments.empty?
-		    text << "<ul class=\"task-arguments\">\n    "
+		    text << "      <ul class=\"task-arguments\">\n"
 		    task.arguments.each do |key, value|
-			text << "    <li>#{key}: #{value}<li>\n"
+			text << "        <li>#{key}: #{value}</li>\n"
 		    end
-		    text << "  </ul>\n"
+		    text << "      </ul>\n"
 		end
-		text << "</div>"
+		text << "  </div>"
 	    end
 
 	    def render_history(history)
@@ -182,15 +206,15 @@ module Roby
 	    end
 
 	    def clear
-		document.clear
+		setHtml("")
 	    end
 
-	    def finalized_pending(time, task)
+	    def finalized_pending(time, plan, task)
 		render_event("warn", time, "Finalized pending task") do
 		    render_task(task)
 		end
 	    end
-	    def added_mission(time, task)
+	    def added_mission(time, plan, task)
 		render_event("info", time, "New mission") do
 		    render_task(task)
 		end
