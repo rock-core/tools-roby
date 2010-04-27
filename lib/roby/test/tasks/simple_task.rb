983762acff9283416e5ad75e23795deaa12d82f2
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2010-04-27 11:23:09 +0200

    core: in deprecated task models, display what file is loading the deprecated file

--- lib/roby/test/tasks/simple_task.rb
+++ lib/roby/test/tasks/simple_task.rb
@@ -2,6 +2,10 @@ require 'roby/tasks/simple'
 module Roby
     module Test
         Roby.warn "Roby::Test::SimpleTask is deprecated, use Roby::Tasks::Simple instead"
+        backtrace = caller
+        backtrace.delete_if { |l| l =~ /require/ }
+        Roby.warn "  loaded from #{backtrace[0]}"
+
         SimpleTask = Roby::Tasks::Simple
     end
 end
