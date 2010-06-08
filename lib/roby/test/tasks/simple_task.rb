6611ed79260d26f44a436ac3090ed342e3afb34b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-06-08 10:56:00 +0200

    more useful warning message for test/simple_task

--- lib/roby/test/tasks/simple_task.rb
+++ lib/roby/test/tasks/simple_task.rb
@@ -3,8 +3,10 @@ module Roby
     module Test
         Roby.warn "Roby::Test::SimpleTask is deprecated, use Roby::Tasks::Simple instead"
         backtrace = caller
-        backtrace.delete_if { |l| l =~ /require/ }
         Roby.warn "  loaded from #{backtrace[0]}"
+        backtrace[1..-1].each do |backtrace_line|
+            Roby.warn "        #{backtrace_line}"
+        end
 
         SimpleTask = Roby::Tasks::Simple
     end
