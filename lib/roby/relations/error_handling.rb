8448b2f49fda92259d2a74c9c9b7c7b6ab3611f0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-10-13 17:47:37 +0200

    core: add a remove_when_done option to #handle_with
    This is akin to the corresponding option in #depends_on.

--- lib/roby/relations/error_handling.rb
+++ lib/roby/relations/error_handling.rb
@@ -1,10 +1,23 @@
 module Roby::TaskStructure
     class Roby::TaskEventGenerator
 	# Mark this event as being handled by the task +task+
-	def handle_with(repairing_task)
+	def handle_with(repairing_task, options = Hash.new)
+            options = Kernel.validate_options options,,
+                :remove_when_done => true
+
 	    if !task.child_object?(repairing_task, ErrorHandling)
 		task.add_error_handler repairing_task, ValueSet.new
 	    end
+
+            if options[:remove_when_done]
+                repairing_task.on :stop do |event|
+                    symbol_set = task[repairing_task, ErrorHandling]
+                    symbol_set.delete(symbol)
+                    if symbol_set.empty?
+                        task.remove_error_handler(repairing_task)
+                    end
+                end
+            end
 
 	    task[repairing_task, ErrorHandling] << symbol
 	end
