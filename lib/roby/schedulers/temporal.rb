d66362e6eabf448c8caf31dd860bc464a7fde44c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-02-09 11:00:27 +0100

    core: in the temporal scheduler, give the option to apply "normal" constraints to tasks even if they have normal constraints as well

--- lib/roby/schedulers/temporal.rb
+++ lib/roby/schedulers/temporal.rb
@@ -5,14 +5,17 @@ module Roby
         # scheduler information given by the temporal constraint network.
         #
         # See the documentation of Roby::Schedulers for more information
-        #
-        # More specifically, this scheduler starts the tasks for which:
-        #  * the task is pending, executable and owned by the local plan manager
-        #  * the task's start event is root in the signal and forward relations
-        #  * the task is root in the dependency relationship, or all its parents
-        #    are started
-        #  * the temporal constraints of the task's start event are met
         class Temporal < Basic
+            # If true, the basic scheduler's constraints must be met for all
+            # tasks. If false, they are applied only on tasks for which no
+            # temporal constraints are set.
+            attr_predicate :basic_constraints?, true
+
+            def initialize(with_basic = true, with_children = true, plan = nil)
+                super(with_children, plan)
+                @basic_constraints = with_basic
+            end
+
             # Starts all tasks that are eligible. See the class documentation
             # for an in-depth description
 	    def can_schedule?(task)
@@ -22,7 +25,14 @@ module Roby
 
                 start_event = task.start_event
                 if start_event.has_scheduling_constraints?
-                    start_event.can_be_scheduled?(Time.now)
+                    if !start_event.can_be_scheduled?(Time.now)
+                        return false
+                    end
+                    if basic_constraints?
+                        super
+                    else
+                        return true
+                    end
                 else
                     super
 		end
