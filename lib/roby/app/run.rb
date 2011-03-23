b1f5cd02b7b595066454f279f506beb97c667b92
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-03-23 09:51:52 +0100

    app: add a banner to scripts/run
    So that the user gets a useful command line description in
    scripts/run --help

--- lib/roby/app/run.rb
+++ lib/roby/app/run.rb
@@ -3,6 +3,7 @@ require 'roby'
 app = Roby.app
 require 'optparse'
 parser = OptionParser.new do |opt|
+    opt.banner = "#{$0} [options] [robot_name [robot_type]]"
     opt.on("--sim", "run in simulation mode") do
 	app.simulation
     end
