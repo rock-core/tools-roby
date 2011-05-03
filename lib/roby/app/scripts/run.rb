32f08518fc139241b03e0dec5e41be6c9e2bd6cc
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-05-03 17:33:22 +0200

    app: in scripts/run, fallback to controllers/robot_type.rb if robot_name.rb is not available

--- lib/roby/app/scripts/run.rb
+++ lib/roby/app/scripts/run.rb
@@ -15,11 +15,15 @@ Roby.display_exception do
 	# Load the controller
 	Roby.execute do
 	    begin
-		controller_file = File.join(APP_DIR, "controllers", "#{app.robot_name}.rb")
-		if File.readable?(controller_file)
-		    Robot.info "loading controller file #{controller_file}"
-		    load controller_file
-		end
+		controller_files = [app.robot_name, app.robot_type]
+                controller_files.each do |name|
+                    controller_file = File.join(APP_DIR, "controllers", "#{name}.rb")
+                    if File.readable?(controller_file)
+                        Robot.info "loading controller file #{controller_file}"
+                        load controller_file
+                        break
+                    end
+                end
 		Robot.info "done initialization"
 	    rescue Interrupt
 	    end
