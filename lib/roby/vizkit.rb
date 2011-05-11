c73f16a10cffe973086e5b946cce3694cb1552ef
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-05-11 11:32:09 +0200

    log: fix roby/vizkit

--- lib/roby/vizkit.rb
+++ lib/roby/vizkit.rb
@@ -1,7 +1,14 @@
 require 'Qt'
 require 'roby/log/event_stream'
 require 'roby/log/plan_rebuilder'
+require 'roby/log/server'
 require 'roby/log/relations_view/relations_view'
 require 'vizkit'
 
-Vizkit::UiLoader.register_ruby_widget('RobyRelations', Roby::Log::RelationsDisplay::RelationsView.method(:new))
+begin
+    DRb.current_server
+rescue DRb::DRbServerNotFound
+    DRb.start_service
+end
+
+Vizkit::UiLoader.register_ruby_widget('RobyPlanDisplay', Roby::LogReplay::RelationsDisplay::RelationsView.method(:new))
