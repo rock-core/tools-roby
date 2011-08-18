fcd376c397a95e0cb354849a5568c1256b0d57ed
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-08-18 10:49:14 +0200

    core: make #influenced_by accept objects that respond to #as_plan

--- lib/roby/relations/influence.rb
+++ lib/roby/relations/influence.rb
@@ -2,7 +2,12 @@ module Roby
     module TaskStructure
 	relation :Influence, :child_name => 'influenced_task', :noinfo => true, :weak => true do
 	    def influenced_by(task)
+                if task.respond_to?(:as_plan)
+                    task = task.as_plan
+                end
+
 		add_influenced_task(task)
+		task
 	    end
         end
 
