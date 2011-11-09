00aa5ca0f5c36ef3e674929a39716d152d20539b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-11-09 13:46:11 +0100

    gui: disable the vizkit plugin for now
    Nobody's using it anyway ...

--- lib/roby/vizkit.rb
+++ lib/roby/vizkit.rb
@@ -2,13 +2,13 @@ require 'Qt'
 require 'roby/log/event_stream'
 require 'roby/log/plan_rebuilder'
 require 'roby/log/server'
-require 'roby/log/relations_view/relations_view'
+#require 'roby/log/relations_view/relations_view'
 require 'vizkit'
 
-begin
-    DRb.current_server
-rescue DRb::DRbServerNotFound
-    DRb.start_service
-end
+# begin
+#     DRb.current_server
+# rescue DRb::DRbServerNotFound
+#     DRb.start_service
+# end
 
-Vizkit::UiLoader.register_ruby_widget('RobyPlanDisplay', Roby::LogReplay::RelationsDisplay::RelationsView.method(:new))
+# Vizkit::UiLoader.register_ruby_widget('RobyPlanDisplay', Roby::LogReplay::RelationsDisplay::RelationsView.method(:new))
