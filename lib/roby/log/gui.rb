dd2dc1e8f44f439ee1559c1fdef29e0224b61853
Thomas Roehr <thomas.roehr@dfki.de>
2011-12-07 11:58:53 +0100

    Add fallback for Qt Versions that don't support toMSecSinceEpoch for DateTime

--- lib/roby/log/gui.rb
+++ lib/roby/log/gui.rb
@@ -1,3 +1,13 @@
+require 'Qt4'
+
+if !Qt::DateTime.method_defined?('toMSecsSinceEpoch')
+    class Qt::DateTime
+        def toMSecsSinceEpoch
+            toTime_t * 1000 + time.msec
+        end
+    end
+end
+
 require 'roby/log/gui/log_display'
 require 'roby/log/gui/plan_rebuilder_widget'
 require 'roby/log/gui/styles'
