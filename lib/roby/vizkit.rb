4346fbc810442f7de0427fc86bb359333d26716b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-02-02 11:21:04 +0100

    log: disable the vizkit integration for now

--- lib/roby/vizkit.rb
+++ lib/roby/vizkit.rb
@@ -1,14 +1,9 @@
-require 'Qt'
-require 'roby/log/event_stream'
-require 'roby/log/plan_rebuilder'
-require 'roby/log/server'
-#require 'roby/log/relations_view/relations_view'
 require 'vizkit'
 
-# begin
-#     DRb.current_server
-# rescue DRb::DRbServerNotFound
-#     DRb.start_service
+# Vizkit::UiLoader.register_ruby_widget('RobyPlanDisplay') do
+#     require 'roby/log/event_stream'
+#     require 'roby/log/plan_rebuilder'
+#     require 'roby/log/server'
+#     Roby::LogReplay::RelationsDisplay::RelationsView.new
 # end
 
-# Vizkit::UiLoader.register_ruby_widget('RobyPlanDisplay', Roby::LogReplay::RelationsDisplay::RelationsView.method(:new))
