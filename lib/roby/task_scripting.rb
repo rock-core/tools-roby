1952ea8ea8aa909bd77bc3b3e30780ed2f96c0aa
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-03-20 17:00:44 +0100

    core: document task scripting

--- lib/roby/task_scripting.rb
+++ lib/roby/task_scripting.rb
@@ -1,13 +1,20 @@
 module Roby
+    # Implementation module for the task scripting capabilities. See
+    # TaskScripting::Script for details
     module TaskScripting
         extend Logger::Forward
         extend Logger::Hierarchy
 
-        class Script
+        # Implementation of the logic that executes scripts. Scripts are defined
+        # using the Script class.
+        class ScriptEngine
+            # The scripting execution blocks
             attr_reader :elements
-
+            # The logger that should be used. It is Roby::TaskScripting by
+            # default
             attr_reader :logger
 
+            # The task that holds this scripting engine 
             def __task__
                 @task
             end
@@ -19,16 +26,19 @@ module Roby
 
             def initialize_copy(original)
                 super
-
                 @elements = original.dup
             end
 
+            # Changes the logger
             def setup_logger(logger)
                 @logger = logger
             end
 
             include Logger::Forward
 
+            # Called at the very beginning of the task execution. It can be used
+            # by execution blocks to resolve some of the necessary information
+            # (children, ...)
             def prepare(task)
                 @task = task
                 super if defined? super
@@ -67,7 +77,7 @@ module Roby
             end
 
             def load(&block)
-                loader = DSLLoader.new(self)
+                loader = Script.new(self)
                 loader.load(&block)
             end
 
@@ -91,11 +101,14 @@ module Roby
             end
         end
 
+        # Proxy object returned by the *_event methods to allow access to child
+        # events
         class Event
             def initialize(chain, event_name)
                 @chain, @event_name = chain, event_name
             end
 
+            # Returns the specified event when applied on +task+
             def resolve(task)
                 task = task.resolve_role_path(@chain)
                 task.event(@event_name)
@@ -106,6 +119,8 @@ module Roby
             end
         end
 
+        # Proxy object returned by the *_child methods to allow access to task
+        # children
         class Child
             def initialize(chain)
                 @chain = chain
@@ -123,6 +138,7 @@ module Roby
                 super
             end
 
+            # Returns the specified child when applied on +task+
             def resolve(task)
                 task.resolve_role_path(@chain)
             end
@@ -132,57 +148,119 @@ module Roby
             end
         end
 
+        # Exception thrown by Script#timeout when the timeout is reached
         class Timeout < RuntimeError; end
 
-        class DSLLoader
-            attr_reader :script
+        # The main interface to scripting
+        #
+        # The commands that are available to task scripts are instance methods
+        # on this object. Each command can "block" the script until a specified
+        # condition (e.g. #wait can wait for a certain number of seconds). Once
+        # that command's end condition is reached, the next one is activated and
+        # so on.
+        #
+        # Nothing special happens if the script reaches its end. If you want
+        # that, for instance, the :success event is emitted, simply do
+        #
+        #   emit :sucess
+        #
+        # Commands that require events can have this event specified in the
+        # following manner:
+        #
+        # * as a symbol (the event name), in which case it refers to an event of
+        #   the task that will hold the script
+        # * as an object accessed with the <eventname>_event pattern (i.e.
+        #   success_event is equivalent to success)
+        # * as an event of one of the task's children. The child is accessed
+        #   with the <childname>_child methods and the event with
+        #   <eventname>_event. For instance:
+        #   
+        #     wait(localization_child.start_event)
+        #
+        #   will wait for the start event of the localization child to be
+        #   emitted. The child must be added with the 'localization' role when
+        #   building the plan
+        #
+        #     root_task.depends_on(localization_task, :role => 'localization')
+        #
+        class Script
+            # The ScriptEngine instance that is going to execute this script
+            attr_reader :script_engine
 
-            def initialize(script)
-                @script = script
+            def initialize(script_engine)
+                @script_engine = script_engine
             end
 
             def load(&block)
                 instance_eval(&block)
             end
 
+            # Use +logger+ as the script logger object. It can be any object
+            # that responds to the normal debug methods #debug, #info, #warn and #fatal
             def setup_logger(logger)
-                script.setup_logger(logger)
+                script_engine.setup_logger(logger)
             end
 
+            # Execute the given block until it calls transition!
             def poll(&block)
-                script.elements << Poll.new(script, &block)
+                script_engine.elements << Poll.new(script_engine, &block)
             end
 
+            # Describe the next operation. It is mainly used in script debugging
+            # to track what is happening. For instance:
+            #
+            #   describe "wait for the position to reach a threshold"
+            #   poll do
+            #     if State.position.x > 10
+            #       transition!
+            #     end
+            #   end
+            #
             def describe(description)
                 @next_description = description
             end
 
+            # Call the provided block and process the definitions there, using
+            # +fallback_description+ as the blocks' descriptions if they don't
+            # provide one themselves.
             def with_description(fallback_description)
-                element_size = script.elements.size
+                element_size = script_engine.elements.size
                 yield
 
             ensure
-                script.elements[element_size..-1].each do |el|
+                script_engine.elements[element_size..-1].each do |el|
                     el.description = @next_description || fallback_description
                 end
                 @next_description = nil
             end
 
+            # Execute the block once at this point in the script
             def execute(&block)
-                script.elements << Execute.new(script, &block)
+                script_engine.elements << Execute.new(script_engine, &block)
             end
             
+            # call-seq:
+            #   timeout 0.5
+            #   timeout 0.5, :emit => :failed
+            #
+            # Execute the given block, but raise a Timeout exception if its
+            # execution takes more than +duration+ seconds (as a numerical
+            # value)
+            #
+            # Alternatively, if the :emit option is given, the specified event
+            # will be emitted instead of having an exception raised. The event
+            # should be terminal
             def timeout(duration, options = Hash.new, &block)
                 options = Kernel.validate_options options, :emit => nil
 
-                subscript = Script.new
+                subscript = ScriptEngine.new
                 subscript.load(&block)
 
                 start_time = nil
-                script = self.script
+                script_engine = self.script_engine
                 execute do
                     start_time = Time.now
-                    subscript.prepare(script.__task__)
+                    subscript.prepare(script_engine.__task__)
                 end
                 poll do
                     if subscript.execute
@@ -197,6 +275,12 @@ module Roby
                 end
             end
 
+            # call-seq:
+            #   poll_until localization_child.ready_event
+            #
+            # Execute the given block at each execution cycle until +event_spec+
+            # happens.
+            #
             def poll_until(event_spec, options = Hash.new, &block)
                 options = Kernel.validate_options options, :after => nil
 
@@ -224,6 +308,10 @@ module Roby
                 end
             end
 
+            # Wait for the specified event to be emitted. It will return
+            # immediately if the event already got emitted in the past.
+            #
+            # Use #wait to wait for a new emission
             def wait_any(event_spec)
                 with_description "WaitAny(#{event_spec}): #{caller(1).first}" do
                     poll do
@@ -236,6 +324,10 @@ module Roby
                 end
             end
 
+            # If given a time (as numeric), will wait for that many seconds
+            #
+            # If given an event, will wait for the specified event to be
+            # emitted. It will return only for new emissions of the event
             def wait(event_spec_or_time, options = Hash.new)
                 options = Kernel.validate_options options, :after => nil
 
@@ -255,6 +347,7 @@ module Roby
                 end
             end
 
+            # Emit the specified event
             def emit(event_spec)
                 with_description "Emit(#{event_spec}): #{caller(1).first}" do
                     execute do
@@ -264,6 +357,7 @@ module Roby
                 end
             end
 
+            # Implementation of the *_child and *_event handlers
             def method_missing(m, *args, &block)
                 if args.empty? && !block
                     case m.to_s
@@ -278,11 +372,17 @@ module Roby
             end
         end
         
+        # Base class for all execution blocks
+        #
+        # Subclasses must define an #execute method. This method should return
+        # false if the block needs to be called again at the next execution cycle,
+        # and false otherwise
         class Base
+            # Description of this block's purpose
             attr_accessor :description
 
-            def initialize(script, &block)
-                @script = script
+            def initialize(script_engine, &block)
+                @script_engine = script_engine
                 if block
                     @defined_at = caller(3).first
                     singleton_class.class_eval do
@@ -299,10 +399,10 @@ module Roby
             end
 
             def method_missing(m, *args, &block)
-                if m == :execute || !@script
+                if m == :execute || !@script_engine
                     super
                 end
-                @script.send(m, *args, &block)
+                @script_engine.send(m, *args, &block)
             end
 
             def prepare(task)
@@ -313,6 +413,7 @@ module Roby
             end
         end
 
+        # Implementation of Script#execute
         class Execute < Base
             def execute
                 do_execute
@@ -320,10 +421,13 @@ module Roby
             end
         end
 
+        # Implementation of polling capabilities
+        #
+        # It is used to implement most of the waiting and polling blocks
         class Poll < Base
             attr_reader :end_conditions
             
-            def initialize(script, &block)
+            def initialize(script_engine, &block)
                 super
                 @end_conditions = Hash.new
             end
@@ -332,11 +436,13 @@ module Roby
                 instance_eval(&block)
             end
 
+            # Sets a termination condition. #execute will return as soon as
+            # +any+ of the termination conditions reutrn true
             def end_if(&block)
                 call_site = caller(1)[5]
                 cond = end_conditions[call_site]
                 if !cond
-                    cond = [block, PollEndCondition.new(@script, &block)]
+                    cond = [block, PollEndCondition.new(@script_engine, &block)]
                     end_conditions[call_site] = cond
                 end
 
@@ -356,10 +462,13 @@ module Roby
                 end
             end
 
+            # Requires the poll block to finish
             def transition!
                 throw :transition_required, true
             end
 
+            # Called when this poll block is active. See the documentation of
+            # the Base class
             def execute
                 transition = catch(:transition_required) do
                     do_execute
@@ -369,6 +478,7 @@ module Roby
             end
         end
 
+        # Holder for end conditions in Poll
         class PollEndCondition < Base
             def wait(timeout)
                 @wait_start = Time.now
@@ -389,8 +499,10 @@ module Roby
     class Task
         inherited_enumerable(:script, :scripts) { Array.new }
 
+        # Adds a script that is going to be executed for every instance of this
+        # task model
         def self.script(&block)
-            script = TaskScripting::Script.new
+            script = TaskScripting::ScriptEngine.new
             script.load(&block)
             scripts << script
         end
@@ -415,8 +527,10 @@ module Roby
             end
         end
 
+        # Adds a task script that is going to be executed while this task
+        # instance runs.
         def script(&block)
-            script = TaskScripting::Script.new
+            script = TaskScripting::ScriptEngine.new
             script.load(&block)
             if running?
                 script.prepare(self)
