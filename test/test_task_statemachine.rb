a9a0650ead1bdfb73b210d3f6bba4a4eeec2c1b6
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-03-28 10:06:49 +0200

    core,test: add some missing tests on the state machine implementation

--- test/test_task_statemachine.rb
+++ test/test_task_statemachine.rb
@@ -1,50 +1,59 @@
 $LOAD_PATH.unshift File.expand_path(File.join('..', 'lib'), File.dirname(__FILE__))
-
-include Roby::TaskStateHelper 
-
-class TestTask < Roby::Task
-    refine_running_state do
-        event :one do 
-            transition [:running, :zero] => :one
-        end
-
-        event :two do
-            transition [:one] => :two
-        end
-
-        event :three do
-            transition [:two] => :three
-        end
-
-        event :reset do
-            transition all => :zero
-        end
-    end
-end
-
-class SecondTestTask < Roby::Task
-    refine_running_state :namespace => 'test' do
-        event :firstly do 
-            transition [:running] => :first
-        end 
-
-        event :secondly do
-            transition [:first] => :second
-        end
-    end
-end
-
-class DerivedTask < TestTask
-    refine_running_state do
-        event :four do
-	    transition [:three] => :four
-	end
-    end
-end
+require 'roby/test/common'
+require 'roby/tasks/simple'
+require 'roby/test/tasks/empty_task'
+require 'roby/tasks/simple'
+require 'flexmock/test_unit'
 
 class TC_TaskStateMachine < Test::Unit::TestCase
     include Roby::Test
     include Roby::Test::Assertions
+
+    class TestTask < Roby::Task
+        refine_running_state do
+            event :one do 
+                transition [:running, :zero] => :one
+            end
+
+            event :two do
+                transition [:one] => :two
+            end
+
+            event :three do
+                transition [:two] => :three
+            end
+
+            event :reset do
+                transition all => :zero
+            end
+        end
+
+        terminates
+    end
+
+    class SecondTestTask < Roby::Task
+        refine_running_state :namespace => 'test' do
+            event :firstly do 
+                transition [:running] => :first
+            end 
+
+            event :secondly do
+                transition [:first] => :second
+            end
+        end
+
+        terminates
+    end
+
+    class DerivedTask < TestTask
+        refine_running_state do
+            event :four do
+                transition [:three] => :four
+            end
+        end
+
+        terminates
+    end
 
     def setup
         super
@@ -90,6 +99,27 @@ class TC_TaskStateMachine < Test::Unit::TestCase
         assert(scndTask.state_machine.status == 'first')
     end
 
+    def test_automatically_created_new_events
+        model = Class.new(Roby::Task) do
+            terminates
+            refine_running_state do
+                on(:intermediate) { transition :running => :one }
+            end
+        end
+        assert model.event_model(:intermediate).controlable?
+
+        task = prepare_plan :add => 1, :model => model
+        task.start!
+        assert_equal 'running', task.state_machine.status
+        task.intermediate!
+        assert_equal 'one', task.state_machine.status
+
+        task = prepare_plan :add => 1, :model => model
+        task.start!
+        assert_equal 'running', task.state_machine.status
+        task.emit :intermediate
+        assert_equal 'one', task.state_machine.status
+    end
     def test_inheritance
 	derivedTask = DerivedTask.new
 	assert(derivedTask.state_machine.proxy.respond_to?(:one))
@@ -101,10 +131,34 @@ class TC_TaskStateMachine < Test::Unit::TestCase
         event = [ :one, :two, :three, :four, :reset ] 
 	begin
 	    event.each do |event|
-		eval("derivedTask.state_machine.#{event}!")
+                derivedTask.state_machine.send("#{event}!")
             end
 	rescue Exception => e
-	    assert (false, "Calling event '#{event} failed")
+	    flunk("Calling event '#{event} failed")
 	end
+    end
+
+    def test_has_no_interaction_with_regular_poll
+        mock = flexmock
+        mock.should_receive(:poll_called).at_least.once
+        mock.should_receive(:one_called).at_least.once
+
+        model = Class.new(Roby::Task) do
+            terminates
+            event :intermediate
+
+            poll do
+                mock.poll_called
+            end
+
+            refine_running_state do
+                state_poll :running do
+                    mock.one_called
+                end
+            end
+        end
+
+        task = prepare_plan :permanent => 1, :model => model
+        task.start!
     end
 end
