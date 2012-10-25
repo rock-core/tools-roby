460713976c415252e2bcfb0e7ab9c001399a50c0
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-10-25 10:00:19 +0200

    core,test: add test for when_unreachable in transactions

--- test/test_transactions.rb
+++ test/test_transactions.rb
@@ -1118,6 +1118,18 @@ class TC_Transactions < Test::Unit::TestCase
             assert(trsc.task_index.by_state[:finished?].include?(p3))
 	end
     end
+
+    def test_when_unreachable_is_propagated_to_the_plan
+	t1 = prepare_plan :add => 1, :model => Tasks::Simple
+        mock = flexmock
+        mock.should_receive(:is_unreachable).once
+        transaction_commit(plan, t1) do |trsc, p1|
+            p1.stop_event.when_unreachable do |*_|
+                mock.is_unreachable
+            end
+        end
+        plan.remove_object(t1)
+    end
 end
 
 class TC_RecursiveTransaction < Test::Unit::TestCase
