846bc7adfb63ae628aaa1364705722c3eeba939e
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-12-18 12:26:28 +0100

    promote error messages from poll blocks to warnings

--- lib/roby/task.rb
+++ lib/roby/task.rb
@@ -2491,10 +2491,10 @@ module Roby
                     poll_block.block.call(self)
                 end
             rescue LocalizedError => e
-                Roby.log_pp(e, Roby.logger, :debug)
+                Roby.log_pp(e, Roby.logger, :warn)
                 emit :internal_error, e
             rescue Exception => e
-                Roby.log_pp(e, Roby.logger, :debug)
+                Roby.log_pp(e, Roby.logger, :warn)
                 emit :internal_error, CodeError.new(e, self)
             end
         end
