4c16fb4d2511a871e4b57d6fbd6fbccc673dcf3b
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-07-31 10:08:34 +0200

    gui: in ActionInterface, create links to the returned types

--- lib/roby/gui/model_views/action_interface.rb
+++ lib/roby/gui/model_views/action_interface.rb
@@ -10,7 +10,7 @@ module Roby
                 def compute_toplevel_links(model, options)
                     actions = model.each_action.map do |action|
                         arguments = action.arguments.map { |arg| ":#{arg.name}" }.join(", ")
-                        format = "#{action.name}(#{arguments}) => #{action.returned_type.name}: #{action.doc}"
+                        format = "#{action.name}(#{arguments}) => #{page.link_to(action.returned_type)}: #{action.doc}"
                         Element.new(action.name, format, element_link_target(action, options[:interactive]), action.name, Hash.new)
                     end
                 end
