beaba48fbae99623c09860d0512bffa6326588e4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-09-19 17:01:17 +0200

    test: enable the event watching functionality in Test::Spec

--- lib/roby/test/spec.rb
+++ lib/roby/test/spec.rb
@@ -1,6 +1,25 @@
 module Roby
     module Test
         class Spec < MiniTest::Spec
+            include Test::Assertions
+
+            def plan; Roby.plan end
+            def engine; Roby.plan.engine end
+
+            def setup
+                super
+
+                @watch_events_handler_id = engine.add_propagation_handler(:type => :external_events) do |plan|
+                    Test.verify_watched_events
+                end
+            end
+
+            def teardown
+                super
+                if @watch_events_handler_id
+                    engine.remove_propagation_handler(@watch_events_handler_id)
+                end
+            end
         end
     end
 end
