c1fb3e8f338211aa5c07deae0e7f7bced7443679
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-28 15:48:58 +0100

    make "roby gen" display the list of installed generators

--- lib/roby/app/scripts/gen.rb
+++ lib/roby/app/scripts/gen.rb
@@ -3,4 +3,21 @@ require 'rubigen/scripts/generate'
 RubiGen::Base.__sources = [RubiGen::PathSource.new(:roby, File.join(Roby::ROBY_ROOT_DIR, "generators"))]
 
 gen_name = ARGV.shift
-RubiGen::Scripts::Generate.new.run(ARGV, :generator => gen_name)
+if !gen_name
+    usage = "\nInstalled Generators\n"
+    RubiGen::Base.sources.inject([]) do |mem, source|
+        # Using an association list instead of a hash to preserve order,
+        # for aesthetic reasons more than anything else.
+        label = source.label.to_s.capitalize
+        pair = mem.assoc(label)
+        mem << (pair = [label, []]) if pair.nil?
+        pair[1] |= source.names(:visible)
+        mem
+    end.each do |label, names|
+        usage << "  #{label}: #{names.join(', ')}\n" unless names.empty?
+    end
+    puts usage
+else
+    RubiGen::Scripts::Generate.new.run(ARGV, :generator => gen_name)
+end
+
