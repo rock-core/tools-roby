e27145a7031fb978af0a70b6935198842f20b0a0
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-28 14:23:57 +0100

    gen: test_task in task generator

--- generators/task/templates/test_task.rb
+++ generators/task/templates/test_task.rb
@@ -1,7 +1,6 @@
-require 'models/<%= subdir %>/<%= basename %>'
-
+require '<%= Roby::App.resolve_robot_in_path("models/#{subdir}/#{basename}") %>'
 <% indent, open, close = ::Roby::App::GenBase.in_module(*class_name[0..-2]) %>
 <%= open %>
-<%= indent %>describe <%= class_name.join("::") %> do
+<%= indent %>describe <%= class_name.last %> do
 <%= indent %>end
 <%= close %>
