8d1a3560ecf357916526fd7f76a664060700afae
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-03-24 12:05:16 -0300

    gen: modify GenModelBase to follow the new naming conventions
    The new conventions are much more systematic, so it is possible to put
    them in the common code.

--- generators/actions/actions_generator.rb
+++ generators/actions/actions_generator.rb
@@ -3,7 +3,6 @@ class ActionsGenerator < Roby::App::GenModelClass
     def initialize(runtime_args, runtime_options = Hash.new)
         @model_type = "actions"
         super
-        @class_name = [class_name.first, "Actions"] + class_name[1..-1]
     end
 end
 
--- generators/task/task_generator.rb
+++ generators/task/task_generator.rb
@@ -3,7 +3,6 @@ class TaskGenerator < Roby::App::GenModelClass
     def initialize(runtime_args, runtime_options = Hash.new)
         @model_type = "tasks"
         super
-        @class_name = [class_name.first, "Tasks"] + class_name[1..-1]
     end
 end
 
