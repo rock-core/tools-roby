050ba4713124a8fe38afef243568f0d4e7e6aea4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-03-24 10:31:45 -0300

    gen: update to the agreed-upon naming scheme

--- generators/task/task_generator.rb
+++ generators/task/task_generator.rb
@@ -3,6 +3,7 @@ class TaskGenerator < Roby::App::GenModelClass
     def initialize(runtime_args, runtime_options = Hash.new)
         @model_type = "tasks"
         super
+        @class_name = [class_name.first, "Tasks"] + class_name[1..-1]
     end
 end
 
