d7ab13cd01a88868ec92b3e42e3d3dea6d31f363
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-02 19:42:42 -0300

    interface: create an async API modelled against Orocos::Async

---
+++ lib/roby/interface/async.rb
@@ -0,0 +1,8 @@
+require 'roby'
+require 'roby/interface'
+require 'concurrent'
+require 'hooks'
+
+require 'roby/interface/async/job_monitor'
+require 'roby/interface/async/new_job_listener'
+require 'roby/interface/async/interface'
