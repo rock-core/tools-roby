97864c867fefe2d3005bdf1d8991aa183e40edd3
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-24 11:15:04 -0300

    gui: update ExceptionView to match latest metaruby changes

--- lib/roby/gui/exception_view.rb
+++ lib/roby/gui/exception_view.rb
@@ -1,12 +1,19 @@
 module Roby
     module GUI
-        class ExceptionView < MetaRuby::GUI::ExceptionView
-            def filter_backtrace(backtrace)
-                Roby.filter_backtrace(backtrace, force: true)
+        class ExceptionRendering < MetaRuby::GUI::ExceptionRendering
+            def filter_backtrace(parsed_backtrace, raw_backtrace)
+                Roby.filter_backtrace(raw_backtrace, force: true)
             end
 
             def user_file?(file)
                 Roby.app.app_file?(file)
+            end
+        end
+
+        class ExceptionView < MetaRuby::GUI::ExceptionView
+            def initialize(*)
+                super
+                @exception_rendering = ExceptionRendering.new(self.exception_rendering.linker)
             end
 
             def each_exception
