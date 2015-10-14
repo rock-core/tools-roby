901fd0bbb48b645789325d90713670b34daaf190
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-10-14 18:31:35 -0300

    core: display the scheduler reports on the debug channel

--- lib/roby/schedulers/reporting.rb
+++ lib/roby/schedulers/reporting.rb
@@ -14,14 +14,17 @@ module Roby
             end
 
             def report_pending_non_executable_task(*args)
+                Roby::Schedulers.debug { State.format_message_into_string(*args) }
                 state.pending_non_executable_tasks << args
             end
 
             def report_trigger(event)
+                Roby::Schedulers.debug { "called #{event}" }
                 state.called_generators << event
             end
 
             def report_holdoff(msg, task, *args)
+                Roby::Schedulers.debug { State.format_message_into_string(msg, task, *args) }
                 state.non_scheduled_tasks[task] << [msg, args]
             end
         end
