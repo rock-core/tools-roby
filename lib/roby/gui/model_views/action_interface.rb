2e233cea2d0f9eaa3c84acb3118d7c2fa9047151
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-11-04 14:02:43 -0200

    gui: display source file and require line for action interfaces

--- lib/roby/gui/model_views/action_interface.rb
+++ lib/roby/gui/model_views/action_interface.rb
@@ -16,8 +16,32 @@ module Roby
                 end
 
                 def render(model, options = Hash.new)
+                    ActionInterface.html_defined_in(page, model, with_require: true)
+
                     actions = compute_toplevel_links(model, options)
                     render_links('Actions', actions)
+                end
+
+                def self.find_definition_place(model)
+                    model.definition_location.find do |file, _, method|
+                        return if method == :require || method == :using_task_library
+                        Roby.app.app_file?(file)
+                    end
+                end
+
+                def self.html_defined_in(page, model, with_require: true, definition_location: nil, format: "<b>Defined in</b> %s")
+                    path, lineno = *definition_location || find_definition_place(model)
+                    if path
+                        path = Pathname.new(path)
+                        path_link = page.link_to(path, "#{path}:#{lineno}", lineno: lineno)
+                        page.push(nil, "<p>#{format % [path_link]}</p>")
+                        if with_require
+                            if req_base = $LOAD_PATH.find { |p| path.fnmatch?(File.join(p, "*")) }
+                                req = path.relative_path_from(Pathname.new(req_base))
+                                page.push(nil, "<code>require '#{req.sub_ext("")}'</code>")
+                            end
+                        end
+                    end
                 end
             end
         end
