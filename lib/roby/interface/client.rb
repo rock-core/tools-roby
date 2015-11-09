e53908f800145191180997669b8da27f6f16adb0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-11-09 16:04:25 -0200

    interface: document Client

--- lib/roby/interface/client.rb
+++ lib/roby/interface/client.rb
@@ -41,26 +41,43 @@ module Roby
                 @actions, @commands = handshake(id)
             end
 
+            # Whether the communication channel to the server is closed
             def closed?
                 io.closed?
             end
 
+            # Close the communication channel
             def close
                 io.close
             end
 
+            # The underlying IO object
             def to_io
                 io.to_io
             end
 
+            # Find an action by its name
+            #
+            # This is a local operation using the information gathered at
+            # connection time
+            #
+            # @param [String] name the name of the action to look for
+            # @return [Actions::Models::Action,nil]
             def find_action_by_name(name)
                 actions.find { |act| act.name == name }
             end
 
+            # Finds all actions whose name matches a pattern
+            #
+            # @param [#===] matcher the matching object (usually a Regexp or
+            #   String)
+            # @return [Array<Actions::Models::Action>]
             def find_all_actions_matching(matcher)
                 actions.find_all { |act| matcher === act.name }
             end
 
+            # @api private
+            #
             # Reads what is available on the given IO and processes the message
             #
             # @param [#read_packet] io packet-reading object
@@ -68,11 +85,10 @@ module Roby
             #   has been processed, the second one if it was a cycle_end message
             def read_and_process_packet(io)
                 m, *args = io.read_packet
+
                 if m == :cycle_end
                     return true, true
-                end
-
-                if m == :bad_call
+                elsif m == :bad_call
                     e = args.first
                     raise e, e.message, e.backtrace
                 elsif m == :reply
@@ -92,8 +108,10 @@ module Roby
 
             # Polls for new data on the IO channel
             #
-            # @raise [ComError] if the link seem to be broken
             # @return [Object] a call reply
+            # @raise [ComError] if the link seem to be broken
+            # @raise [ProtocolError] if some errors happened when validating the
+            #   protocol
             def poll(expected_count = 0)
                 result = nil
                 timeout = if expected_count > 0 then nil
@@ -119,10 +137,15 @@ module Roby
                 return result, has_cycle_end
             end
 
+            # @api private
+            #
+            # Allocation of unique IDs for notification messages
             def allocate_message_id
                 @message_id += 1
             end
 
+            # @api private
+            #
             # Push a job notification to {#job_progress_queue}
             #
             # See the yield parameters of {Interface#on_job_notification} for
@@ -131,26 +154,43 @@ module Roby
                 job_progress_queue.push [allocate_message_id, [kind, job_id, job_name, *args]]
             end
 
+            # Whether some job progress information is currently queued
             def has_job_progress?
                 !job_progress_queue.empty?
             end
 
+            # Remove and return the oldest job information message
+            #
+            # @return [(Integer,Array)] a unique and monotonically-increasing
+            #   message ID and the arguments to job progress as specified on
+            #   {Interface#on_job_notification}.
             def pop_job_progress
                 job_progress_queue.shift
             end
 
+            # @api private
+            #
+            # Push a generic notification to {#notification_queue}
             def queue_notification(source, level, message)
                 notification_queue.push [allocate_message_id, [source, level, message]]
             end
 
+            # Whether some generic notifications have been queued
             def has_notifications?
                 !notification_queue.empty?
             end
 
+            # Remove and return the oldest generic notification message
+            #
+            # @return [(Integer,Array)] a unique and monotonically-increasing
+            #   message ID and the generic notification information as specified
+            #   by (Application#notify)
             def pop_notification
                 notification_queue.shift
             end
 
+            # @api private
+            #
             # Push an exception notification to {#exception_queue}
             #
             # It can be retrieved with {#pop_exception}
@@ -161,10 +201,16 @@ module Roby
                 exception_queue.push [allocate_message_id, [kind, error, tasks, job_ids]]
             end
 
+            # Whether some exception notifications have been queued
             def has_exceptions?
                 !exception_queue.empty?
             end
 
+            # Remove and return the oldest exception notification
+            #
+            # @return [(Integer,Array)] a unique and monotonically-increasing
+            #   message ID and the generic notification information as specified
+            #   by (Interface#on_exception)
             def pop_exception
                 exception_queue.shift
             end
@@ -172,6 +218,18 @@ module Roby
             # Method called when trying to start an action that does not exist
             class NoSuchAction < NoMethodError; end
 
+            # @api private
+            #
+            # Call a method on the interface or on one of the interface's
+            # subcommands
+            #
+            # @param [Array<String>] path path to the subcommand. Empty means on
+            #   the interface object itself.
+            # @param [Symbol] m command or action name. Actions are always
+            #   formatted as action_name!
+            # @param [Object] args the command or action arguments
+            # @return [Object] the command result, or -- in the case of an
+            #   action -- the job ID for the newly created action
             def call(path, m, *args)
                 if m.to_s =~ /(.*)!$/
                     action_name = $1
@@ -186,16 +244,26 @@ module Roby
                 end
             end
 
+            # @api private
+            #
+            # Object used to gather commands in a batch
+            #
+            # @see Client#create_batch Client#process_batch
             class BatchContext < BasicObject
+                # Creates a new batch context
+                #
+                # @param [Object] context the underlying interface object
                 def initialize(context)
                     @context = context
                     @calls = Array.new
                 end
 
+                # The set of calls on {#context} that have been gathered so far
                 def __calls
                     @calls
                 end
 
+                # Pushes a call in the batch
                 def push(path, m, *args)
                     @calls << [path, m, *args]
                 end
@@ -227,15 +295,30 @@ module Roby
                     end
                 end
 
+                # Process the batch and return the list of return values for all
+                # the calls in {#__calls}
                 def process
                     @context.process_batch(self)
                 end
             end
 
+            # Create a batch context
+            #
+            # Messages sent to the returned object are validated as much as
+            # possible and gathered in a list. Call {#process_batch} to send all
+            # the gathered calls at once to the remote server
+            #
+            # @return [BatchContext]
             def create_batch
                 BatchContext.new(self)
             end
 
+            # Send all commands gathered in a batch for processing on the remote
+            # server
+            #
+            # @param [BatchContext] batch
+            # @return [Array] the return values of each of the calls gathered in
+            #   the batch
             def process_batch(batch)
                 call([], :process_batch, batch.__calls)
             end
