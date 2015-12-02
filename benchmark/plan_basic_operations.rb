23edb8d310072634ca8f71aee120c063eee2518f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-12-02 16:26:49 -0200

    benchmark: start a new series of benchmarks

---
+++ benchmark/plan_basic_operations.rb
@@ -0,0 +1,28 @@
+require 'roby'
+require 'benchmark'
+
+require 'ruby-prof'
+
+COUNT = 1000
+Benchmark.bm(30) do |x|
+    tasks = Array.new
+    x.report("allocates #{COUNT} tasks") do
+        COUNT.times { tasks << Roby::Task.new }
+    end
+
+    plan = Roby::Plan.new
+    x.report("adds #{COUNT} tasks") do
+        tasks.each { |t| plan.add(t) }
+    end
+
+    plan = Roby::Plan.new
+    tasks = (1..COUNT).map { Roby::Task.new }
+    x.report("add 1 time #{COUNT} tasks") do
+        plan.add(tasks)
+    end
+
+    x.report("remove 1 tasks 100 times") do
+        tasks.each { |t| plan.remove_object(t) }
+    end
+end
+
---
+++ benchmark/relations/graph.rb
@@ -0,0 +1,66 @@
+require 'roby'
+require 'benchmark'
+
+require 'ruby-prof'
+
+class Graph < Roby::Relations::Graph
+    def initialize
+        super('test')
+    end
+end
+
+class Vertex
+    include Roby::Relations::DirectedRelationSupport
+
+    attr_reader :relation_graphs
+    def initialize(graph)
+        @relation_graphs = Hash[graph => graph, graph.class => graph]
+    end
+end
+
+COUNT = 10_000
+Benchmark.bm(80) do |x|
+    tasks = Array.new
+
+    graph = Graph.new
+    vertices = (1..COUNT).map { Vertex.new(graph) }
+    x.report("insert #{COUNT} vertices") do
+        vertices.each { |o| graph.insert(o) }
+    end
+    x.report("remove #{COUNT} vertices") do
+        vertices.each { |o| graph.remove(o) }
+    end
+
+    graph = Roby::Relations::Graph.new('test')
+    vertices = (1..2*COUNT).map { Vertex.new(graph) }
+    vertices.each { |o| graph.insert(o) }
+    x.report("#{COUNT} separate links between already inserted vertices") do
+        vertices.each_slice(2) { |parent, child| graph.add_relation(parent, child, nil) }
+    end
+    x.report("unlink #{COUNT} separate links") do
+        vertices.each_slice(2) { |parent, child| graph.remove_relation(parent, child) }
+    end
+
+    graph = Roby::Relations::Graph.new('test')
+    vertices = (1..2*COUNT).map { Vertex.new(graph) }
+    x.report("#{COUNT} separate links, inserted vertices") do
+        vertices.each_slice(2) { |parent, child| graph.add_relation(parent, child, nil) }
+    end
+
+    graph = Roby::Relations::Graph.new('test')
+    graph.insert(shared_v = Vertex.new(graph))
+    vertices = (1..COUNT).map { Vertex.new(graph) }
+    vertices.each { |o| graph.insert(o) }
+    x.report("#{COUNT} links with a shared vertex between already inserted vertices") do
+        vertices.each { |o| graph.add_relation(shared_v, o, nil) }
+    end
+
+    graph = Roby::Relations::Graph.new('test')
+    shared_v = Vertex.new(graph)
+    vertices = (1..COUNT).map { Vertex.new(graph) }
+    x.report("#{COUNT} links with a shared vertex, inserting vertices at the same time") do
+        vertices.each { |o| graph.add_relation(shared_v, o, nil) }
+    end
+end
+
+
