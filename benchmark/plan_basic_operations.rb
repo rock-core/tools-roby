e07b053674d1515afcd5c47405edb679a696b860
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-12-02 22:53:15 -0200

    benchmark: fix unnecessary GC pressure in plan_basic_operations
    The benchmark was permanently storing the created task objects,
    which obviously was adding GC pressure that is not exactly needed.

--- benchmark/plan_basic_operations.rb
+++ benchmark/plan_basic_operations.rb
@@ -5,12 +5,12 @@ require 'ruby-prof'
 
 COUNT = 1000
 Benchmark.bm(30) do |x|
-    tasks = Array.new
     x.report("allocates #{COUNT} tasks") do
-        COUNT.times { tasks << Roby::Task.new }
+        COUNT.times { Roby::Task.new }
     end
 
     plan = Roby::Plan.new
+    tasks = (1..COUNT).map { Roby::Task.new }
     x.report("adds #{COUNT} tasks") do
         tasks.each { |t| plan.add(t) }
     end
@@ -21,7 +21,7 @@ Benchmark.bm(30) do |x|
         plan.add(tasks)
     end
 
-    x.report("remove 1 tasks 100 times") do
+    x.report("remove 1 tasks #{tasks.size} times") do
         tasks.each { |t| plan.remove_object(t) }
     end
 end
