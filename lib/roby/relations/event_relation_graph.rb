01cdfe71312a5a1915392ffa0bbf75ae66fbc964
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-12-14 18:48:55 -0200

    core: move the task_graph functionality in SchedulingConstraints
    It is the only graph that uses it, and it's pretty costly

--- lib/roby/relations/event_relation_graph.rb
+++ lib/roby/relations/event_relation_graph.rb
@@ -13,57 +13,6 @@ module Roby
         #
         # will return true
         class EventRelationGraph < Relations::Graph
-            # The graph of tasks related to each other by their events
-            attr_reader :task_graph
-
-            def initialize(*args)
-                super
-                @task_graph = BidirectionalDirectedAdjacencyGraph.new
-            end
-
-            def add_edge(from, to, info)
-                super
-
-                if from.respond_to?(:task) && to.respond_to?(:task)
-                    from_task, to_task = from.task, to.task
-                    if from_task != to_task && !task_graph.has_edge?(from_task, to_task)
-                        task_graph.add_edge(from_task, to_task, nil)
-                    end
-                end
-            end
-
-            def merge(graph)
-                super
-                task_graph.merge(graph.task_graph)
-            end
-
-            def replace(graph)
-                super
-                task_graph.replace(graph.task_graph)
-            end
-
-            def remove_vertex(event)
-                super
-                if event.respond_to?(:task)
-                    task_graph.remove_vertex(event.task)
-                end
-            end
-
-            def remove_edge(from, to)
-                super
-
-                if from.respond_to?(:task) && to.respond_to?(:task)
-                    task_graph.remove_edge(from.task, to.task)
-                end
-            end
-
-            def related_tasks?(ta, tb)
-                task_graph.has_edge?(ta, tb)
-            end
-
-            def clear
-                task_graph.clear
-            end
         end
     end
 end
