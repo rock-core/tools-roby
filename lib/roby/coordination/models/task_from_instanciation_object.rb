0bf876ad401d77bf9974d490f92fceb1991605c8
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-06 16:30:00 -0200

    coordination: indentation fix

--- lib/roby/coordination/models/task_from_instanciation_object.rb
+++ lib/roby/coordination/models/task_from_instanciation_object.rb
@@ -1,31 +1,30 @@
 module Roby
     module Coordination
         module Models
+            # Definition of a state from an object that responds to instanciate
+            class TaskFromInstanciationObject < TaskWithDependencies
+                # @return [#instanciate] the object that will be used to generate
+                #   the state's task when needed
+                attr_reader :instanciation_object
 
-        # Definition of a state from an object that responds to instanciate
-        class TaskFromInstanciationObject < TaskWithDependencies
-            # @return [#instanciate] the object that will be used to generate
-            #   the state's task when needed
-            attr_reader :instanciation_object
+                # @param [#instanciate] instanciation_object this object will be
+                #   used to generate the task that will perform the state's action.
+                #   It will be called with #instanciate(plan)
+                # @param [Model<Roby::Task>] task_model the model of the task
+                #   returned by the instanciation object
+                def initialize(instanciation_object, task_model)
+                    super(task_model)
+                    @instanciation_object = instanciation_object
+                end
 
-            # @param [#instanciate] instanciation_object this object will be
-            #   used to generate the task that will perform the state's action.
-            #   It will be called with #instanciate(plan)
-            # @param [Model<Roby::Task>] task_model the model of the task
-            #   returned by the instanciation object
-            def initialize(instanciation_object, task_model)
-                super(task_model)
-                @instanciation_object = instanciation_object
+                # Called by the state machine implementation to create a Roby::Task
+                # instance that will perform the state's actions
+                def instanciate(plan, variables = Hash.new)
+                    instanciation_object.instanciate(plan)
+                end
+
+                def to_s; "#{instanciation_object}[#{model}]" end
             end
-
-            # Called by the state machine implementation to create a Roby::Task
-            # instance that will perform the state's actions
-            def instanciate(plan, variables = Hash.new)
-                instanciation_object.instanciate(plan)
-            end
-
-            def to_s; "#{instanciation_object}[#{model}]" end
-        end
         end
     end
 end
--- lib/roby/coordination/models/task_with_dependencies.rb
+++ lib/roby/coordination/models/task_with_dependencies.rb
@@ -1,46 +1,46 @@
 module Roby
     module Coordination
         module Models
-        # Generic representation of an execution context task that can be
-        # instanciated 
-        class TaskWithDependencies < Task
-            # @return [Set<(Task,String)>] set of dependencies needed for this
-            #   task, as a (task,role) pair
-            attr_reader :dependencies
+            # Generic representation of an execution context task that can be
+            # instanciated 
+            class TaskWithDependencies < Task
+                # @return [Set<(Task,String)>] set of dependencies needed for this
+                #   task, as a (task,role) pair
+                attr_reader :dependencies
 
-            # (see Task#initialize)
-            def initialize(model)
-                super
-                @dependencies = Set.new
-            end
+                # (see Task#initialize)
+                def initialize(model)
+                    super
+                    @dependencies = Set.new
+                end
 
-            def initialize_copy(old)
-                super
-                @dependencies = @dependencies.dup
-            end
+                def initialize_copy(old)
+                    super
+                    @dependencies = @dependencies.dup
+                end
 
-            # Modify this task's internal structure to change relationships
-            # between tasks
-            def map_tasks(mapping)
-                super
-                @dependencies = dependencies.map do |task, role|
-                    [mapping[task] || task, role]
+                # Modify this task's internal structure to change relationships
+                # between tasks
+                def map_tasks(mapping)
+                    super
+                    @dependencies = dependencies.map do |task, role|
+                        [mapping[task] || task, role]
+                    end
+                end
+
+                def find_child_model(name)
+                    if d = dependencies.find { |_, role| role == name }
+                        d[0].model
+                    end
+                end
+
+                def depends_on(action, role: nil)
+                    if !action.kind_of?(Coordination::Models::Task)
+                        raise ArgumentError, "expected a task, got #{action}. You probably forgot to convert it using #task or #state"
+                    end
+                    dependencies << [action, role]
                 end
             end
-
-            def find_child_model(name)
-                if d = dependencies.find { |_, role| role == name }
-                    d[0].model
-                end
-            end
-
-            def depends_on(action, role: nil)
-                if !action.kind_of?(Coordination::Models::Task)
-                    raise ArgumentError, "expected a task, got #{action}. You probably forgot to convert it using #task or #state"
-                end
-                dependencies << [action, role]
-            end
-        end
         end
     end
 end
