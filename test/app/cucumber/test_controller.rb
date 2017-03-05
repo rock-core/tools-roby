2812546c0633ee280ff0e04f639a62040fd47e76
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-03-05 14:45:27 -0300

    cucumber: explicitly provide a temporary log dir to #roby_start in cucumber tests
    This avoids polluting the local machine's log dir with temporary
    directories

--- test/app/cucumber/test_controller.rb
+++ test/app/cucumber/test_controller.rb
@@ -6,10 +6,11 @@ module Roby
     module App
         module Cucumber
             describe Controller do
-                attr_reader :controller, :roby_app_dir
+                attr_reader :controller, :roby_app_dir, :roby_log_dir
                 before do
                     @controller = Controller.new
                     @roby_app_dir = make_tmpdir
+                    @roby_log_dir = make_tmpdir
                     app = Roby::Application.new
                     app.app_dir = roby_app_dir
                     installer = Roby::Installer.new(app, quiet: true)
@@ -27,19 +28,19 @@ module Roby
 
                 describe "#roby_start" do
                     it "starts and connects by default" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         assert controller.roby_running?
                         assert controller.roby_connected?
                     end
 
                     it "does not connect if connect: false is given" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         assert controller.roby_running?
                         refute controller.roby_connected?
                     end
 
                     it "raises if attempting to start a new controller while one is running" do
-                        pid = controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        pid = controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         interface = controller.roby_interface
                         assert_raises(Controller::InvalidState) do
                             controller.roby_start('default', 'default', app_dir: roby_app_dir)
@@ -59,7 +60,7 @@ module Roby
 
                 describe "#roby_stop" do
                     it "stops and joins by default" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         controller.roby_stop
                         refute controller.roby_running?
                         refute controller.roby_connected?
@@ -72,7 +73,7 @@ module Roby
                     end
 
                     it "raises if the controller is not connected" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         assert_raises(Controller::InvalidState) do
                             controller.roby_stop
                         end
@@ -81,12 +82,12 @@ module Roby
 
                 describe "#roby_disconnect" do
                     it "disconnects from the remote interface" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         controller.roby_disconnect
                         refute controller.roby_connected?
                     end
                     it "allows for a reconnection" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         controller.roby_disconnect
                         controller.roby_connect
                         assert controller.roby_connected?
@@ -99,7 +100,7 @@ module Roby
                     end
 
                     it "returns true if it is connected" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         controller.roby_disconnect
                         controller.roby_interface.attempt_connection
                         controller.roby_interface.wait_connection_attempt_result
@@ -107,7 +108,7 @@ module Roby
                     end
 
                     it "returns true if it is already connected" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         assert controller.roby_try_connect
                     end
                 end
@@ -118,7 +119,7 @@ module Roby
                     end
 
                     it "returns true if the controller has been started" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         assert controller.roby_running?
                     end
                 end
@@ -129,18 +130,18 @@ module Roby
                     end
 
                     it "returns false if the controller has been started and the we're not connected" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         refute controller.roby_connected?
                     end
 
                     it "returns true if we're connected" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         controller.roby_connect
                         assert controller.roby_connected?
                     end
 
                     it "returns false if the remote host is stopped" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         controller.roby_connect
                         controller.roby_stop(join: false)
                         refute controller.roby_connected?
@@ -149,7 +150,7 @@ module Roby
                     end
 
                     it "returns false if we've called #roby_stop and joined" do
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, connect: false, log_dir: roby_log_dir)
                         controller.roby_connect
                         controller.roby_stop
                         refute controller.roby_connected?
@@ -206,7 +207,7 @@ module Roby
                             Robot.actions { use_library CucumberTestActions }
                             EOACTION
                         end
-                        controller.roby_start('default', 'default', app_dir: roby_app_dir)
+                        controller.roby_start('default', 'default', app_dir: roby_app_dir, log_dir: roby_log_dir)
                         controller.roby_enable_backtrace_filtering(enable: false)
                     end
 
