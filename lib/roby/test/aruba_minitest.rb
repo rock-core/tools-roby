6929df97b130e64f085b75c876d786c0817ee2af
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-02-15 17:41:46 -0200

    bin: fix 'roby shell'
    'shell' is defined by Thor, so it passes the "is it an instance method ?"
    test but then we go nowhere.

--- lib/roby/test/aruba_minitest.rb
+++ lib/roby/test/aruba_minitest.rb
@@ -55,6 +55,7 @@ module Roby
             end
 
             def assert_command_finished_successfully(cmd)
+                refute cmd.timed_out?, "#{cmd} timed out on stop"
                 assert_equal 0, cmd.exit_status, "#{cmd} finished with a non-zero exit status (#{cmd.exit_status})\n-- STDOUT\n#{cmd.stdout}\n-- STDERR\n#{cmd.stderr}"
             end
         end
