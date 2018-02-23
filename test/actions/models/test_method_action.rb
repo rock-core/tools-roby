19220dd52aa11d56f14c846ae2be71118a81ddd8
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-02-23 14:45:30 -0300

    actions: fix returning a service model from an action

--- test/actions/models/test_method_action.rb
+++ test/actions/models/test_method_action.rb
@@ -4,15 +4,25 @@ module Roby
     module Actions
         module Models
             describe MethodAction do
-                describe "droby marshalling" do
-                    attr_reader :interface_m, :action_m
-                    before do
-                        @interface_m = Actions::Interface.new_submodel(name: 'Actions')
-                        @action_m = MethodAction.new(interface_m)
-                        action_m.name = 'test'
-                        interface_m.register_action 'test', action_m
-                    end
+                attr_reader :interface_m, :action_m
+                before do
+                    @interface_m = Actions::Interface.new_submodel(name: 'Actions')
+                    @action_m = MethodAction.new(interface_m)
+                    action_m.name = 'test'
+                    interface_m.register_action 'test', action_m
+                end
 
+                it "creates a placeholder task for services that can be replaced" do
+                    srv_m = Roby::TaskService.new_submodel
+                    @action_m.returns(srv_m)
+                    task_m = Roby::Task.new_submodel
+                    task_m.provides srv_m
+
+                    plan.add(placeholder = @action_m.as_plan)
+                    plan.replace_task(placeholder, task_m.new)
+                end
+
+                describe "droby marshalling" do
                     it "marshals the interface and name" do
                         unmarshalled = droby_transfer(action_m)
                         assert(unmarshalled.action_interface_model < Actions::Interface)
