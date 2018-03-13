f5d5af67866826e5f8b464954c71d17c8099b076
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-03-13 11:03:02 -0300

    droby: mark plain classes and modules as DRoby-unmarshallable
    It ensures that one gets an error right away if they get used as
    arguments in a task

--- lib/roby/droby/marshallable.rb
+++ lib/roby/droby/marshallable.rb
@@ -2,6 +2,12 @@ class Object
     def droby_marshallable?; true end
 end
 
+class Module
+    def droby_marshallable?
+        false
+    end
+end
+
 module Roby
     module DRoby
         module Unmarshallable
