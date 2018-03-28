a771b017ca575006b7b6d23e786df9afd0065181
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-03-28 10:24:34 -0300

    release 3.0

--- lib/roby/version.rb
+++ lib/roby/version.rb
@@ -1,5 +1,5 @@
 module Roby
-    VERSION = "2.0.0.rc1"
+    VERSION = "3.0.0"
     ROBY_LIB_DIR  = File.expand_path( File.join(File.dirname(__FILE__), '..') )
     ROBY_ROOT_DIR = File.expand_path( File.join(ROBY_LIB_DIR, '..') )
 end
