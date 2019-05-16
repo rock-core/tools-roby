b5f118dca80d0cabfecd2f670113ac09823f2235
Sylvain <sylvain.joyeux@tidewise.io>
2019-05-16 17:13:13 -0300

    rename all templates to .rb.erb to please IDE's syntax highlighting autodection
    The old pattern will still work, to make sure gen is not broken if
    we forget some files, and/or we're not breaking other packages
    (e.g. Syskit) while they migrate too,.

--- lib/roby/cli/gen/roby_app/config/init.rb
+++ lib/roby/cli/gen/roby_app/config/init.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 # This file is called to do application-global configuration. For configuration
 # specific to a robot, edit config/NAME.rb, where NAME is the robot name.
-# 
+#
 # Here are some of the most useful configuration options
 
 # Use backward-compatible naming and behaviour, when applicable
@@ -13,5 +15,5 @@ Roby.app.backward_compatible_naming = false
 # name is inferred from the base directory name (e.g. an app located in
 # bundles/flat_fish would have an app name of flat_fish and a module name of
 # FlatFish
-# 
+#
 # Roby.app.module_name = 'Override'
