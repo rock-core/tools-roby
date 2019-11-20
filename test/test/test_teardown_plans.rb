a3c979d810eab40bc537c231054c2d9f1503c430
Sylvain <sylvain.joyeux@tidewise.io>
2019-11-20 16:21:25 -0300

    test: fix teardown_registered_plans to sleep between loops
    Without the poll time, any test that involve asynchronous processes
    (e.g. external processes, threads, syskit components) are likely
    to trigger the error - and cause a busy loop on the test process.

---
+++ test/test/test_teardown_plans.rb
@@ -0,0 +1,38 @@
+require 'roby/test/self'
+
+module Roby
+    module Test
+        describe TeardownPlans do
+            describe '#teardown_registered_plans' do
+                it 'warns about tasks that block the plan after teardown_warn seconds' do
+                    task_m = Roby::Task.new_submodel do
+                        event(:stop) { |event| }
+                    end
+                    plan.add(task = task_m.new)
+
+                    messages = []
+                    warn_time = nil
+                    flexmock(Roby).should_receive(:warn).and_return do |msg|
+                        warn_time ||= Time.now
+                        messages << msg
+                        execution_engine.once { task.stop_event.emit }
+                    end
+
+                    execute { task.start_event.emit }
+                    tic = Time.now
+                    teardown_registered_plans(teardown_warn: 0.1)
+                    assert((0.08..0.2).include?(warn_time - tic))
+
+                    matcher = Regexp.new(
+                        "more than \\d+ iterations while trying to shut down #{plan} "\
+                        "after .*teardown_registered_plans#test_\\d+_warns about "\
+                        "tasks that block the plan after teardown_warn "\
+                        "seconds, quarantine=0 tasks, tasks=1 tasks")
+                    assert_match matcher, messages[0]
+
+                    assert_equal "  #{task} running=true finishing=true", messages[2]
+                end
+            end
+        end
+    end
+end
