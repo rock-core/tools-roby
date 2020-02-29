40b2b7e017839233d5f6211b5991ab5e2c967bbf
Sylvain <sylvain.joyeux@tidewise.io>
2020-02-29 17:50:31 -0300

    gui: fix loading the chronicle widget with no data

--- lib/roby/gui/chronicle_widget.rb
+++ lib/roby/gui/chronicle_widget.rb
@@ -911,7 +911,7 @@ module Roby
 
             def update_scroll_ranges
                 vertical_scroll_bar.setRange(0, current_tasks.size - 1)
-                @start_line = [current_tasks.size - 1, @start_line].min
+                @start_line = [current_tasks.size - 1, @start_line].compact.min
                 return if horizontal_scroll_bar_down?
 
                 if base_time && current_time && display_time
