ec3d6319c0470439907fa7b2f8488c4ac4b48be6
Sylvain <sylvain.joyeux@tidewise.io>
2020-04-08 17:51:57 -0300

    fix offenses on lib/roby/actions/models/method_action.rb

--- lib/roby/actions/models/method_action.rb
+++ lib/roby/actions/models/method_action.rb
@@ -5,6 +5,8 @@ module Roby
         module Models
             # Action defined by a method on an {Interface}
             class MethodAction < Action
+                # Exception raised when a method-action returns a task that does not
+                # match its declared return type
                 class InvalidReturnedType < RuntimeError
                     def initialize(action_interface_model, method_name,
                         returned_task, expected_type)
@@ -15,9 +17,10 @@ module Roby
                     end
 
                     def pretty_print(pp)
-                        pp.text "method '#{@method_name}' of #{@action_interface_model}"
+                        pp.text "action '#{@action_interface_model.name}.#{@method_name}'"
+                        pp.text " was expected to return"
                         pp.breakable
-                        pp.text "was expected to return a task of type "
+                        pp.text "a task of type "
                         @expected_type.pretty_print(pp)
                         pp.text ","
                         pp.breakable
@@ -42,45 +45,82 @@ module Roby
 
                 # Instanciate this action on the given plan
                 def instanciate(plan, arguments = {})
+                    arguments = arguments.transform_keys(&:to_sym)
+                    detect_unknown_arguments(arguments)
+                    required_and_default_arguments(arguments)
+
                     action_interface = action_interface_model.new(plan)
-
-                    if self.arguments.empty?
-                        unless arguments.empty?
-                            raise ArgumentError, "#{name} expects no arguments, but #{arguments.size} are given"
+                    result =
+                        if self.arguments.empty?
+                            action_interface.send(name)
+                        else
+                            action_interface.send(name, **arguments)
                         end
 
-                        result = action_interface.send(name).as_plan
-                    else
-                        default_arguments = self.arguments.each_with_object({}) do |arg, h|
-                            h[arg.name] = arg.default
-                        end
-                        arguments = Kernel.validate_options arguments, default_arguments
-                        self.arguments.each do |arg|
-                            if arg.required && !arguments.has_key?(arg.name.to_sym)
-                                raise ArgumentError, "required argument #{arg.name} not given to #{name}"
-                            end
-                        end
-                        result = action_interface.send(name, arguments).as_plan
-                    end
-
-                    unless result.fullfills?(returned_task_type)
-                        raise InvalidReturnedType.new(
-                            action_interface_model, name, result, returned_task_type),
-                              "method '#{name}' of #{action_interface_model} was expected "\
-                              "to return a task of type #{returned_task_type}, but "\
-                              "returned #{result}"
-                    end
+                    result = validate_returned_task_type(result)
+                    plan.add(result)
 
                     # Make the planning task inherit the model/argument flags
-                    if planning_task = result.planning_task
-                        if planning_task.respond_to?(:action_model=)
-                            planning_task.action_model ||= self
+                    update_planning_task(result)
+                    result
+                end
+
+                def detect_unknown_arguments(arguments)
+                    known_arguments = self.arguments.map(&:name).map(&:to_sym)
+                    invalid_arg = arguments
+                                  .each_key.find { |k| !known_arguments.include?(k) }
+                    return unless invalid_arg
+
+                    expected_arguments =
+                        if known_arguments.empty?
+                            "The action accepts no arguments"
+                        else
+                            "The action accepts the following arguments: "\
+                            "#{known_arguments.sort.join(', ')}"
                         end
-                        if planning_task.respond_to?(:action_arguments=)
-                            result.planning_task.action_arguments ||= arguments
+
+                    raise ArgumentError,
+                          "unknown argument '#{invalid_arg}' given to action #{self}. "\
+                          "#{expected_arguments}"
+                end
+
+                def required_and_default_arguments(arguments)
+                    self.arguments.each do |arg|
+                        arg_sym = arg.name.to_sym
+                        next if arguments.key?(arg_sym)
+
+                        if arg.required
+                            raise ArgumentError,
+                                  "required argument '#{arg.name}' not given to action "\
+                                  "#{self}"
+                        elsif arg.default
+                            arguments[arg_sym] = arg.default
                         end
                     end
-                    result
+                end
+
+                def validate_returned_task_type(result)
+                    result = result.as_plan
+                    return result if result.fullfills?(returned_task_type)
+
+                    e = InvalidReturnedType.new(
+                        action_interface_model, name, result, returned_task_type
+                    )
+
+                    raise e, "action '#{self}' was expected to return a task of "\
+                             "type #{returned_task_type}, but returned #{result}"
+                end
+
+                def update_planning_task(result)
+                    return unless (planning_task = result.planning_task)
+
+                    if planning_task.respond_to?(:action_model=)
+                        planning_task.action_model ||= self
+                    end
+                    if planning_task.respond_to?(:action_arguments=)
+                        planning_task.action_arguments ||= arguments
+                    end
+                    nil
                 end
 
                 # Create a new action model that is bound to a different interface model
@@ -104,7 +144,8 @@ module Roby
 
                     planner = Roby::Actions::Task.new(
                         Hash[action_model: self,
-                             action_arguments: arguments].merge(job_id))
+                             action_arguments: arguments].merge(job_id)
+                    )
                     planner.planning_result_task
                 end
 
--- test/actions/models/test_method_action.rb
+++ test/actions/models/test_method_action.rb
@@ -8,19 +8,111 @@ module Roby
             describe MethodAction do
                 attr_reader :interface_m, :action_m
                 before do
-                    @interface_m = Actions::Interface.new_submodel(name: "Actions")
+                    @interface_m = Actions::Interface.new_submodel(name: "TestActions")
                     @action_m = MethodAction.new(interface_m)
                     action_m.name = "test"
                     interface_m.register_action! "test", action_m
+                end
+
+                describe "#instanciate argument handling" do
+                    describe "actions with no arguments" do
+                        before do
+                            @task_m = Roby::Task.new_submodel
+                            @interface_m.describe("a test action").returns(@task_m)
+                            @interface_m.class_eval do
+                                define_method(:no_args) do
+                                end
+                            end
+                        end
+
+                        it "calls the method" do
+                            flexmock(@interface_m)
+                                .new_instances.should_receive(:no_args)
+                                .once.with_no_args.and_return(task = @task_m.new)
+
+                            assert_equal task, @interface_m.no_args.instanciate(plan)
+                        end
+
+                        it "raises if arguments are given" do
+                            flexmock(@interface_m)
+                                .new_instances.should_receive(:no_args).never
+
+                            e = assert_raises(ArgumentError) do
+                                @interface_m.no_args.instanciate(plan, some: 10)
+                            end
+                            assert_equal "unknown argument 'some' given to action "\
+                                         "TestActions.no_args. The action accepts "\
+                                         "no arguments", e.message
+                        end
+                    end
+
+                    describe "actions with arguments" do
+                        before do
+                            @task_m = Roby::Task.new_submodel
+                            @interface_m.describe("a test action").returns(@task_m)
+                                        .required_arg("req", "some doc")
+                                        .optional_arg("opt_without_value", "no value")
+                                        .optional_arg("opt_with_value", "some value", 10)
+                            @interface_m.class_eval do
+                                define_method(:args) do |**args|
+                                end
+                            end
+                        end
+
+                        it "raises if undefined arguments are provided" do
+                            flexmock(@interface_m)
+                                .new_instances.should_receive(:args).never
+
+                            e = assert_raises(ArgumentError) do
+                                @interface_m.args.instanciate(plan, req: 20, some: 10)
+                            end
+                            assert_equal "unknown argument 'some' given to action "\
+                                         "TestActions.args. The action accepts "\
+                                         "the following arguments: opt_with_value, "\
+                                         "opt_without_value, req", e.message
+                        end
+
+                        it "raises if required arguments are not provided" do
+                            flexmock(@interface_m)
+                                .new_instances.should_receive(:args).never
+
+                            e = assert_raises(ArgumentError) do
+                                @interface_m.args.instanciate(plan)
+                            end
+                            assert_equal "required argument 'req' not given to action "\
+                                         "TestActions.args", e.message
+                        end
+
+                        it "passes caller-provided optional arguments" do
+                            flexmock(@interface_m)
+                                .new_instances.should_receive(:args)
+                                .with(req: 42, opt_with_value: 10, opt_without_value: 20)
+                                .once.and_return(task = @task_m.new)
+
+                            @interface_m.args.instanciate(
+                                plan, req: 42, opt_with_value: 10, opt_without_value: 20
+                            )
+                        end
+
+                        it "fills only default arguments that have a value" do
+                            flexmock(@interface_m)
+                                .new_instances.should_receive(:args)
+                                .with(req: 42, opt_with_value: 10)
+                                .once.and_return(task = @task_m.new)
+
+                            @interface_m.args.instanciate(plan, req: 42)
+                        end
+                    end
                 end
 
                 describe "validation of the returned value" do
                     before do
                         @parent_task_m = Roby::Task.new_submodel
                         @task_m = @parent_task_m.new_submodel
-                        flexmock(interface_m).new_instances.should_receive(:test)
+                        flexmock(interface_m).new_instances
+                            .should_receive(:test)
                             .explicitly
-                            .and_return(@task = @task_m.new)
+                            .and_return(@task = @task_m.new).by_default
                     end
                     it "accepts a task of the expected returned task model" do
                         @action_m.returns(@task_m)
@@ -42,12 +134,12 @@ module Roby
                         e = assert_raises(MethodAction::InvalidReturnedType) do
                             @action_m.instanciate(plan)
                         end
-                        assert_equal "method 'test' of #{@interface_m} was expected "\
+                        assert_equal "action 'TestActions.test' was expected "\
                             "to return a task of type #{expected_m}, "\
                             "but returned #{@task}", e.message
                         pp_message = <<~MESSAGE
-                            method 'test' of #{@interface_m}
-                            was expected to return a task of type #{expected_m},
+                            action 'TestActions.test' was expected to return
+                            a task of type #{PP.pp(expected_m, ''.dup, 0).chomp},
                             but returned #{PP.pp(@task, ''.dup, 0)}
                         MESSAGE
                         assert_equal pp_message.chomp, PP.pp(e, "".dup)
@@ -66,6 +158,23 @@ module Roby
                         assert_raises(MethodAction::InvalidReturnedType) do
                             @action_m.instanciate(plan)
                         end
+                    end
+
+                    it "calls #as_plan on the returned value" do
+                        @action_m.returns(@task_m)
+                        task = @task_m.new
+                        flexmock(interface_m).new_instances
+                            .should_receive(:test)
+                            .explicitly
+                            .and_return(flexmock(as_plan: task))
+
+                        assert_equal task, @action_m.instanciate(plan)
+                    end
+
+                    it "auto-adds the result to the plan" do
+                        @action_m.returns(@task_m)
+                        @action_m.instanciate(plan)
+                        assert plan.has_task?(@task)
                     end
                 end
 
