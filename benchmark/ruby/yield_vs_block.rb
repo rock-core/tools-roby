cf0e3d5541eb6e79faf73726def9a59e980455a0
Sylvain <sylvain.joyeux@tidewise.io>
2020-04-08 16:46:12 -0300

    fix offenses on benchmark/ruby/yield_vs_block

--- benchmark/ruby/yield_vs_block.rb
+++ benchmark/ruby/yield_vs_block.rb
@@ -6,8 +6,8 @@ def call_yield(enum)
     enum.each { |v| yield(v) }
 end
 
-def call_block(enum, &b)
-    enum.each(&b)
+def call_block(enum, &block)
+    enum.each(&block)
 end
 
 enum = (1..10_000_000)
@@ -15,22 +15,24 @@ cost_of_call = 10_000_000
 call_count = 1_000
 call_enum  = (1..100)
 Benchmark.bm(40) do |x|
-    x.report(format("yield - enumerate %.0e elements", enum.size)) do
+    x.report(format("yield - enumerate %<calls>.0e elements", calls: enum.size)) do
         call_yield(enum) { |v| }
     end
-    x.report(format("block - enumerate %.0e elements", enum.size)) do
+    x.report(format("block - enumerate %<calls>.0e elements", calls: enum.size)) do
         call_block(enum) { |v| }
     end
-    x.report(format("yield - %.0e calls", cost_of_call)) do
+    x.report(format("yield - %<calls>.0e calls", calls: cost_of_call)) do
         cost_of_call.times { call_yield((1..1)) { |v| } }
     end
-    x.report(format("block - %.0e calls", cost_of_call)) do
+    x.report(format("block - %<calls>.0e calls", calls: cost_of_call)) do
         cost_of_call.times { call_block((1..1)) { |v| } }
     end
-    x.report(format("yield - %.0e calls and %.0e elements", call_count, call_enum.size)) do
+    x.report(format("yield - %<calls>.0e calls and %<elements>.0e elements",
+                    calls: call_count, elements: call_enum.size)) do
         call_count.times { call_yield(call_enum) { |v| } }
     end
-    x.report(format("block - %.0e calls and %.0e elements", call_count, call_enum.size)) do
+    x.report(format("block - %<calls>.0e calls and %<elements>.0e elements",
+                    calls: call_count, elements: call_enum.size)) do
         call_count.times { call_block(call_enum) { |v| } }
     end
 end
