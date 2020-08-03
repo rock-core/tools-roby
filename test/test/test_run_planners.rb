732e93aac7162d0a16c70fb8ae41db62ad144cc5
Sylvain <sylvain.joyeux@tidewise.io>
2020-08-03 18:33:52 -0300

    test: handle being given multiple root tasks in run_planners

--- test/test/test_run_planners.rb
+++ test/test/test_run_planners.rb
@@ -7,11 +7,11 @@ module Roby
         describe RunPlanners do
             before do
                 task_m = @task_m = Roby::Task.new_submodel { terminates }
-                planned_task = @planned_task = task_m.new
+                planned_tasks = @planned_tasks = [task_m.new, task_m.new]
                 @action_m = Roby::Actions::Interface.new_submodel do
-                    describe("the test action").returns(task_m)
-                    define_method :test_action do
-                        planned_task
+                    describe("the test action").optional_arg(:id, "", 0).returns(task_m)
+                    define_method :test_action do |id: 0|
+                        planned_tasks[id]
                     end
                     describe("the action for children").returns(task_m)
                     define_method :test_child do
@@ -19,10 +19,10 @@ module Roby
                     end
                     describe("an action that adds a new planner").returns(task_m)
                     define_method :test_action_with_child do
-                        planned_task.depends_on(
+                        planned_tasks[0].depends_on(
                             self.class.test_child.as_plan, role: "test"
                         )
-                        planned_task
+                        planned_tasks[0]
                     end
                 end
             end
@@ -68,42 +68,56 @@ module Roby
                 assert @handler_class.valid?
             end
 
+            it "handles being given an object instead of an array" do
+                plan.add(root_task = @action_m.test_action(id: 0).as_plan)
+                assert_equal @planned_tasks[0], run_planners(
+                    root_task, recursive: false
+                )
+            end
+
             describe "recursive: false" do
-                it "runs the planner of the toplevel task and returns the planned task" do
-                    plan.add(root_task = @action_m.test_action.as_plan)
-                    assert_equal @planned_task, run_planners(root_task, recursive: false)
+                it "runs the planner of the toplevel tasks and "\
+                   "returns the planned task" do
+                    plan.add(root_task1 = @action_m.test_action(id: 0).as_plan)
+                    plan.add(root_task2 = @action_m.test_action(id: 1).as_plan)
+                    assert_equal @planned_tasks, run_planners(
+                        [root_task1, root_task2], recursive: false
+                    )
                 end
 
                 it "does not run existing planners in the hierarchy" do
                     plan.add(root_task = @action_m.test_action.as_plan)
                     root_task.depends_on(child = @action_m.test_child.as_plan)
-                    run_planners(root_task, recursive: false)
+                    run_planners([root_task], recursive: false)
                     assert child.abstract?
                 end
 
                 it "does not run planners added by the action" do
                     plan.add(root_task = @action_m.test_action_with_child.as_plan)
-                    root_task = run_planners(root_task, recursive: false)
-                    assert root_task.test_child.abstract?
+                    root_task = run_planners([root_task], recursive: false)
+                    assert root_task.first.test_child.abstract?
                 end
 
                 it "can be executed in expect_execution context as well" do
                     service = nil
                     expect_execution do
                         plan.add(root_task = @action_m.test_action.as_plan)
-                        service = run_planners(root_task)
+                        service = run_planners([root_task])
                     end.to_run
-                    assert_equal @planned_task, service.to_task
+                    assert_equal @planned_tasks[0], service.first.to_task
                 end
             end
+
             describe "recursive: true" do
                 it "runs the planner of all tasks that require "\
                    "it in the root task's subplan" do
-                    plan.add(root_task = @action_m.test_action.as_plan)
-                    root_task.depends_on(child = @action_m.test_child.as_plan)
+                    plan.add(root_task1 = @action_m.test_action(id: 0).as_plan)
+                    plan.add(root_task2 = @action_m.test_action(id: 1).as_plan)
+                    root_task1.depends_on(child = @action_m.test_child.as_plan)
                     child_planner = child.planning_task
 
-                    run_planners(root_task, recursive: true)
+                    result = run_planners([root_task1, root_task2], recursive: true)
+                    assert_equal @planned_tasks, result
                     assert child_planner.finished?
                 end
                 it "re-runs planners that have been added in the first pass" do
