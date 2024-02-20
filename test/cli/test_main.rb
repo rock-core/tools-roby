7f5dc0f2d9343fba062a1f86ada18ac301472109
Sylvain <sylvain.joyeux@tidewise.io>
2024-02-19 21:15:00 -0300

    fix: port collision on the main CLI tests

--- test/cli/test_main.rb
+++ test/cli/test_main.rb
@@ -35,46 +35,39 @@ module Roby
                 end
 
                 describe "the REST API" do
-                    it "starts the API on the default port if --rest is given" do
-                        run_roby "run --rest"
-                        assert_eventually do
-                            Interface::REST::Server.server_alive?(
-                                "localhost", Interface::DEFAULT_REST_PORT
-                            )
-                        end
-                        run_roby_and_stop "quit --retry"
+                    before do
+                        @interface_port = roby_allocate_port
+                        @rest_port = roby_allocate_port
                     end
-                    it "starts the API on a custom port if an integer argument is given to --rest" do
+
+                    it "starts the API" do
                         # Guess an available port ... not optimal, but hopefully good enough
-                        tcp_server = TCPServer.new(0)
-                        port = tcp_server.local_address.ip_port
-                        tcp_server.close
-                        run_roby "run --rest=#{port}"
+                        run_roby "run --rest=#{@rest_port} --port=#{@interface_port}"
                         assert_eventually do
                             Interface::REST::Server.server_alive?(
-                                "localhost", port
+                                "localhost", @rest_port
                             )
                         end
-                        run_roby_and_stop "quit --retry"
+                        run_roby_and_stop "quit --retry --host=localhost:#{@interface_port}"
                     end
                     it "properly shuts down the server" do
                         # We check whether the server gets shut down by
                         # starting two apps one after the other. If the socket
                         # is not closed, we won't be able to create the new
                         # server
-                        @run_cmd = run_roby "run --rest"
-                        run_roby_and_stop "quit --retry"
+                        @run_cmd = run_roby "run --rest=#{@rest_port} --port=#{@interface_port}"
+                        run_roby_and_stop "quit --retry --host=localhost:#{@interface_port}"
                         assert_command_stops @run_cmd
-                        @run_cmd = run_roby "run --rest"
-                        run_roby_and_stop "quit --retry"
+                        @run_cmd = run_roby "run --rest=#{@rest_port} --port=#{@interface_port}"
+                        run_roby_and_stop "quit --retry --host=localhost:#{@interface_port}"
                         assert_command_stops @run_cmd
                     end
                     it "starts the API on a Unix socket if one is given" do
                         dir = make_tmpdir
                         socket = File.join(dir, "rest-socket")
-                        run_roby "run --rest=#{socket}"
+                        run_roby "run --rest=#{socket} --port=#{@interface_port}"
                         assert_eventually { File.exist?(socket) }
-                        run_roby_and_stop "quit --retry"
+                        run_roby_and_stop "quit --retry --host=localhost:#{@interface_port}"
                     end
                 end
             end
@@ -82,32 +75,27 @@ module Roby
             describe "quit" do
                 before do
                     run_roby_and_stop "gen app"
+                    @interface_port = roby_allocate_port
                 end
                 it "exits with a nonzero status if there is no app to quit" do
-                    cmd = run_roby "quit"
+                    cmd = run_roby "quit --host=localhost:#{@interface_port}"
                     cmd.stop
                     assert_equal 1, cmd.exit_status
                 end
                 it "stops a running app" do
-                    run_cmd = run_roby "run"
-                    run_roby_and_stop "wait"
-                    run_roby_and_stop "quit"
-                    assert_command_stops run_cmd
-                end
-                it "stops a running app on another port" do
-                    run_cmd = run_roby "run --port 9999"
-                    run_roby_and_stop "wait --host localhost:9999"
-                    run_roby_and_stop "quit --host localhost:9999"
+                    run_cmd = run_roby "run --port=#{@interface_port}"
+                    run_roby_and_stop "wait --host=localhost:#{@interface_port}"
+                    run_roby_and_stop "quit --host=localhost:#{@interface_port}"
                     assert_command_stops run_cmd
                 end
                 it "waits forever for the app to be available if --retry is given" do
-                    run_cmd = run_roby "run"
-                    run_roby_and_stop "quit --retry"
+                    run_cmd = run_roby "run --port=#{@interface_port}"
+                    run_roby_and_stop "quit --retry --host=localhost:#{@interface_port}"
                     assert_command_stops run_cmd
                 end
                 it "times out on retrying if --retry is given a timeout" do
                     before = Time.now
-                    cmd = run_roby "quit --retry=1"
+                    cmd = run_roby "quit --retry=1 --host=localhost:#{@interface_port}"
                     cmd.stop
                     assert_equal 1, cmd.exit_status
                     assert (Time.now - before) > 1
@@ -117,10 +105,11 @@ module Roby
             describe "shell" do
                 before do
                     run_roby_and_stop "gen app"
+                    @interface_port = roby_allocate_port
                 end
                 it "connects and sends commands" do
-                    run_cmd = run_roby "run --port 9999"
-                    shell_cmd = run_roby "shell --host localhost:9999"
+                    run_cmd = run_roby "run --port #{@interface_port}"
+                    shell_cmd = run_roby "shell --host localhost:#{@interface_port}"
                     shell_cmd.write "quit\n"
                     shell_cmd.write "exit\n"
                     assert_command_stops shell_cmd
