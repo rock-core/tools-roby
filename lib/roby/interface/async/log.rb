63bad3c36a45180c8b18e937a5d785451287ce05
Sylvain <sylvain.joyeux@tidewise.io>
2024-03-25 21:09:34 -0300

    fix: add missing interface/async/log backward compatible file

---
+++ lib/roby/interface/async/log.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "roby/interface/async"
+require "roby/interface/v1/async/log"
+
+Roby.warn_deprecated(
+    "require \"roby/interface/async/log\" is deprecated, use the versioned interface "\
+    "API instead, that is roby/interface/v1/async/log and Roby::Interface::V1::Async::Log"
+)
