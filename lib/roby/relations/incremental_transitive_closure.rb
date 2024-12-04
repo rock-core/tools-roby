e5b3b390dcce88fca61f42729b4a511886409c3a
kapeps <rodrigo.kappes@tidewise.io>
2024-12-04 15:14:39 -0300

    chore: more testing and comments in methods

--- lib/roby/relations/incremental_transitive_closure.rb
+++ lib/roby/relations/incremental_transitive_closure.rb
@@ -5,24 +5,50 @@ require 'rgl/transitivity'
 
 module Roby
     module Relations
+        # This class represents an incremental transitive closure graph, 
+        # where edges and vertices can be added or removed incrementally, 
+        # while keeping track of reachability information (i.e., transitive closure).
         class IncrementalTransitiveClosure
             attr_reader :graph
 
+            # Initializes an empty directed graph using RGL's DirectedAdjacencyGraph
             def initialize
                 @graph = RGL::DirectedAdjacencyGraph.new
             end
         
+            # Adds a new vertex to the graph if it doesn't already exist
+            #
+            # @param vertex [Object] The vertex to be added
             def added_vertex(vertex)
                 return if @graph.has_vertex?(vertex)
+
                 @graph.add_vertex(vertex)
             end
             
-            def added_edge(from, to)
-                @graph.add_edge(from, to)
-                add_edges_to_parents(from, to)
+            # Adds an edge from 'source' to 'target' to the graph
+            # It also ensures that transitive reachability is updated by adding edges
+            # from the 'source's parents vertices to 'target'. As well as adding edges
+            # from the from 'source' to 'target's children vertices 
+            #
+            # @param source [Object] The source vertex
+            # @param target [Object] The target vertex
+            def added_edge(source, target)
+                return if @graph.has_edge?(source, target)
+
+                @graph.add_edge(source, target)
+                add_edges_to_parents(source, target)
+                add_edges_to_children(source, target)
             end
 
+            # Removes a vertex from the graph if it has no adjacent vertices 
+            # (i.e., no outgoing edges)
+            # If there are adjacent vertices, the entire graph is 
+            # reset (rebuilds the graph)
+            #
+            # @param vertex [Object] The vertex to be removed
             def removed_vertex(vertex)
+                return unless @graph.has_vertex?(vertex)
+
                 if @graph.adjacent_vertices(vertex).empty?
                     @graph.remove_vertex(vertex)
                 else
@@ -30,22 +56,67 @@ module Roby
                 end
             end
 
-            def removed_edge(from, to)
-                if @graph.adjacent_vertices(to).empty?
-                    @graph.remove_edge(from, to)
+            # Removes an edge from the graph. If the target vertex has adjacent vertices,
+            # or the source vertex has parent vertices
+            # the entire graph is reset. Otherwise, it simply removes the edge.
+            #
+            # @param source [Object] The source vertex
+            # @param target [Object] The target vertex
+            def removed_edge(source, target)
+                return unless @graph.has_edge?(source, target)
+
+                if @graph.adjacent_vertices(target).empty? && parent_vertices(source).empty?
+                    @graph.remove_edge(source, target)
                 else
                     @graph = RGL::DirectedAdjacencyGraph.new
                 end
             end
             
-            def reachable?(from, to)
-                @graph.has_edge?(from, to)
+            # Checks if there is a directed edge from 'source' to 'target' 
+            # (i.e., if 'target' is reachable from 'source')
+            #
+            # @param source [Object] The source vertex
+            # @param target [Object] The target vertex
+            #
+            # @return [Boolean] Returns true if there is a direct edge from 'source' 
+            # to 'target', false otherwise
+            def reachable?(source, target)
+                @graph.has_edge?(source, target)
             end
 
-            def add_edges_to_parents(from, to)
-                parents = @graph.edges.select { |edge| edge.target == from }.map(&:source)
+            # Updates the transitive closure by adding edges from the 'source's parents 
+            # vertices to 'target', thus ensuring that the graph's 
+            # reachability is consistent.
+            #
+            # @param source [Object] The source vertex
+            # @param target [Object] The target vertex
+            def add_edges_to_parents(source, target)
+                parents = parent_vertices(source)
                 parents.each do |parent|
-                    @graph.add_edge(parent, to) unless @graph.has_edge?(parent, to)
+                    @graph.add_edge(parent, target) unless @graph.has_edge?(parent, target)
+                end
+            end
+
+            # Return the parent vertices of the provided vertex
+            #
+            # @param from [Object] The source vertex
+            #
+            # @return [Array<Object>] An array of parent vertices (those that point to 
+            # the given vertex).
+            def parent_vertices(vertex)
+                return @graph.edges.select { |edge| edge.target == vertex }.map(&:source)
+            end
+
+            # Updates the transitive closure by adding edges from 'source' to
+            # the 'target's children vertices, thus ensuring that the graph's 
+            # reachability is consistent.
+            #
+            # @param source [Object] The source vertex
+            # @param target [Object] The target vertex
+            def add_edges_to_children(source, target)
+                children = @graph.adjacent_vertices(target)
+                children.each do |child|
+                    @graph.add_edge(source, child) unless @graph.has_edge?(source, child)
                 end
             end
         end
--- test/relations/test_incremental_transitive_closure.rb
+++ test/relations/test_incremental_transitive_closure.rb
@@ -24,6 +24,7 @@ module Roby
                     incremental_transitive_closure.added_vertex(1)
                     incremental_transitive_closure.added_edge(0,1)
                     assert incremental_transitive_closure.graph.has_edge?(0,1)
+                    refute incremental_transitive_closure.graph.has_edge?(1,0)
                   end
 
                   it "adds indirect edges between vertex" do
@@ -33,6 +34,13 @@ module Roby
                     incremental_transitive_closure.added_edge(0,1)
                     incremental_transitive_closure.added_edge(1,2)
                     assert incremental_transitive_closure.graph.has_edge?(0,2)
+                    refute incremental_transitive_closure.graph.has_edge?(2,0)
+
+                    incremental_transitive_closure.added_vertex(3)
+                    incremental_transitive_closure.added_edge(3,1)
+                    assert incremental_transitive_closure.graph.has_edge?(3,1)
+                    assert incremental_transitive_closure.graph.has_edge?(3,2)
+                    refute incremental_transitive_closure.graph.has_edge?(3,0)
                   end
 
                   it "tests reachability from direct and indirect vertex" do
@@ -41,10 +49,15 @@ module Roby
                     incremental_transitive_closure.added_vertex(2)
                     incremental_transitive_closure.added_edge(0,1)
                     incremental_transitive_closure.added_edge(1,2)
+                    incremental_transitive_closure.added_vertex(3)
+                    incremental_transitive_closure.added_edge(3,1)
                     assert incremental_transitive_closure.reachable?(0,2)
                     assert incremental_transitive_closure.reachable?(1,2)
+                    assert incremental_transitive_closure.reachable?(3,2)
                     refute incremental_transitive_closure.reachable?(2,1)
                     refute incremental_transitive_closure.reachable?(2,0)
+                    refute incremental_transitive_closure.reachable?(2,0)
+                    refute incremental_transitive_closure.reachable?(0,3)
                   end
                 end
 
@@ -53,21 +66,50 @@ module Roby
                     incremental_transitive_closure.added_vertex(0)
                     incremental_transitive_closure.added_vertex(1)
                     incremental_transitive_closure.added_vertex(2)
+                    incremental_transitive_closure.added_vertex(3)
                     incremental_transitive_closure.added_edge(0,1)
                     incremental_transitive_closure.added_edge(1,2)
-                    incremental_transitive_closure.removed_edge(1,2)
-                    assert_equal(incremental_transitive_closure.graph.num_vertices, 3) 
-                    refute incremental_transitive_closure.graph.has_edge?(1,2)
+                    incremental_transitive_closure.added_edge(3,2)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 4)
+                    assert incremental_transitive_closure.graph.has_edge?(3,2)
+                    incremental_transitive_closure.removed_edge(3,2)
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 4)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 3) 
+                    refute incremental_transitive_closure.graph.has_edge?(3,2)
                   end
 
-                  it "resets representation if removed edge of node containing children" do
+                  it "does nothing if it removes non-existent edge" do
+                    incremental_transitive_closure.added_vertex(0)
+                    incremental_transitive_closure.added_vertex(1)
+                    incremental_transitive_closure.added_vertex(2)
+                    incremental_transitive_closure.added_edge(0,1)
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 3) 
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 1)
+                    incremental_transitive_closure.removed_edge(1,2)
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 3) 
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 1)
+                  end
+
+                  it "resets representation if removed edge of target vertex containing children" do
                     incremental_transitive_closure.added_vertex(0)
                     incremental_transitive_closure.added_vertex(1)
                     incremental_transitive_closure.added_vertex(2)
                     incremental_transitive_closure.added_edge(0,1)
                     incremental_transitive_closure.added_edge(1,2)
                     incremental_transitive_closure.removed_edge(0,1)
-                    assert_equal(incremental_transitive_closure.graph.num_vertices, 0) 
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 0)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 0)
+                  end
+
+                  it "resets representation if removed edge of source vertex containing parents" do
+                    incremental_transitive_closure.added_vertex(0)
+                    incremental_transitive_closure.added_vertex(1)
+                    incremental_transitive_closure.added_vertex(2)
+                    incremental_transitive_closure.added_edge(0,1)
+                    incremental_transitive_closure.added_edge(1,2)
+                    incremental_transitive_closure.removed_edge(1,2)
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 0)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 0)
                   end
 
                   it "removes vertex" do
@@ -76,8 +118,22 @@ module Roby
                     incremental_transitive_closure.added_vertex(2)
                     incremental_transitive_closure.added_edge(0,1)
                     incremental_transitive_closure.added_edge(1,2)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 3) 
                     incremental_transitive_closure.removed_vertex(2)
-                    assert_equal(incremental_transitive_closure.graph.num_vertices, 2) 
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 2)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 1)
+                  end
+
+                  it "does nothing if it removes inexistent vertex" do
+                    incremental_transitive_closure.added_vertex(0)
+                    incremental_transitive_closure.added_vertex(1)
+                    incremental_transitive_closure.added_vertex(2)
+                    incremental_transitive_closure.added_edge(0,1)
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 3) 
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 1)
+                    incremental_transitive_closure.removed_vertex(3)
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 3) 
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 1)
                   end
 
                   it "resets representation if removed vertex containing children" do
@@ -86,8 +142,10 @@ module Roby
                     incremental_transitive_closure.added_vertex(2)
                     incremental_transitive_closure.added_edge(0,1)
                     incremental_transitive_closure.added_edge(1,2)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 3) 
                     incremental_transitive_closure.removed_vertex(1)
-                    assert_equal(incremental_transitive_closure.graph.num_vertices, 0) 
+                    assert_equal(incremental_transitive_closure.graph.num_vertices, 0)
+                    assert_equal(incremental_transitive_closure.graph.num_edges, 0)
                   end
                 end
             end
